190fcd4c8af8e61f8a7867fa5f765a5a5e23cdcc
YazoonDinalt <kykyky228kykyk@gmail.com>
2024-10-16 18:02:57 +0300

    feat: Start develop parser
    Signed-off-by: YazoonDinalt <kykyky228kykyk@gmail.com>

--- MEML/lib/ast.ml
+++ MEML/lib/ast.ml
@@ -1,4 +1,4 @@
-(** Copyright 2024-2025, Efim Perevalov, Vetaly Dyachkov *)
+(** Copyright 2024-2025, Perevalov Efim, Dyachkov Vitaliy *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
---
+++ MEML/tests/pars_tests.ml
@@ -0,0 +1,36 @@
+(** Copyright 2023-2024, Perevalov Efim, Dyachkov Vitaliy *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+open MEML_lib
+open Ast
+open Parser
+
+(* TESTS  PARSER*)
+
+let start_test parser show input =
+  let res = start_parsing parser input in
+  match res with
+  | Ok res -> Format.printf "%s" (show res)
+  | Error err -> Format.printf "%s" err
+;;
+
+(* Test const parser *)
+
+let%expect_test _ =
+  let test = "true" in
+  start_test parse_const show_const test;
+  [%expect {| (CBool true) |}]
+;;
+
+let%expect_test _ =
+  let test = "\"itsastring\"" in
+  start_test parse_const show_const test;
+  [%expect {| (CString "itsastring") |}]
+;;
+
+let%expect_test _ =
+  let test = "951753" in
+  start_test parse_const show_const test;
+  [%expect {| (CInt 951753) |}]
+;;
