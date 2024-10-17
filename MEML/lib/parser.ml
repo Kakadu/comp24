47e34de934a2233ccf2336ce49a85ee355c681a8
Urtix <es.perevalov@mail.ru>
2024-10-17 03:40:10 +0300

    feat: Add parse econst, evar, ebinaryop, eifelse
    Signed-off-by: Urtix <es.perevalov@mail.ru>

--- MEML/lib/parser.ml
+++ MEML/lib/parser.ml
@@ -38,6 +38,7 @@ let is_keyword = function
   | "false"
   | "match"
   | "with"
+  | "val"
   | "in" -> true
   | _ -> false
 ;;
@@ -71,7 +72,7 @@ let chainl1 e op =
   e >>= fun init -> go init
 ;;
 
-(* Const parsers *)
+(** Const parsers *)
 
 let parse_bool =
   parse_white_space
@@ -92,7 +93,7 @@ let parse_str =
   >>| fun a -> CString a
 ;;
 
-(* Var parsers*)
+(* Var parsers *)
 
 let parse_type =
   parse_white_space
@@ -108,7 +109,7 @@ let check_var cond =
   parse_white_space *> take_while1 cond
   >>= fun v ->
   if is_keyword v
-  then fail ("You can not use" ^ v ^ "keywords as vars")
+  then fail ("You can not use \"" ^ v ^ "\" keywords as vars")
   else if Char.is_digit @@ String.get v 0
   then fail "Identifier first symbol is letter, not digit"
   else return v
@@ -121,12 +122,80 @@ let parse_var =
   check_var is_entry
 ;;
 
+(** Pattern parsers *)
+
 let parse_pattern_var =
   (fun a -> PVar (a, TUnknown))
   <$> parse_var
   <|> parens @@ lift2 (fun a b -> PVar (a, b)) parse_var parse_type
+  <|> lift2 (fun a b -> PVar (a, b)) parse_var parse_type
 ;;
 
-let parse_const = (fun v -> PConst v) <$> choice [ parse_int; parse_bool; parse_str ]
+let parse_pconst = (fun v -> PConst v) <$> choice [ parse_int; parse_bool; parse_str ]
 let parse_wild = (fun _ -> PWild) <$> pstrtoken "_"
-let parse_pattern = choice [ parse_wild; parse_const; parse_pattern_var ]
+let parse_pattern = parse_wild <|> parse_pconst <|> parse_pattern_var
+
+(** Expression type *)
+
+(* EConst *)
+
+let parse_econst = (fun v -> EConst v) <$> choice [ parse_int; parse_bool; parse_str ]
+
+(* EVar *)
+
+let parse_evar =
+  (fun a -> EVar (a, TUnknown))
+  <$> parse_var
+  <|> parens @@ lift2 (fun a b -> EVar (a, b)) parse_var parse_type
+  <|> lift2 (fun a b -> EVar (a, b)) parse_var parse_type
+;;
+
+(* EBinaryOp *)
+
+let parse_op char_op op =
+  pstrtoken char_op *> return (fun e1 e2 -> EBinaryOp (op, e1, e2))
+;;
+
+let pmulti = parse_op "*" Mul <|> parse_op "/" Div <|> parse_op "%" Mod
+let padd = parse_op "+" Add <|> parse_op "-" Sub
+
+let pcomp =
+  parse_op ">=" Greq <|> parse_op ">" Gre <|> parse_op "<=" Leq <|> parse_op "<" Less
+;;
+
+let peq = parse_op "=" Eq <|> parse_op "<>" Neq
+let pconj = parse_op "&&" And
+let pdisj = parse_op "||" Or
+
+let parse_binop x =
+  let multi = chainl1 x pmulti in
+  let add = chainl1 multi padd in
+  let comp = chainl1 add pcomp in
+  let eq = chainl1 comp peq in
+  let conj = chainl1 eq pconj in
+  chainl1 conj pdisj <* parse_white_space
+;;
+
+let parse_ebinop = parse_binop @@ (parse_econst <|> parse_evar)
+
+(* EApp *)
+
+(* EIfElse *)
+
+let parse_eifelse =
+  fix
+  @@ fun _ ->
+  lift3
+    (fun e1 e2 e3 -> EIfElse (e1, e2, e3))
+    (pstrtoken "if" *> parse_ebinop)
+    (pstrtoken "then" *> parse_ebinop)
+    (pstrtoken "else" *> parse_ebinop)
+;;
+
+(* ELetIn *)
+
+(* EFun *)
+
+(* Expression parsers *)
+
+(** Binding type *)
---
+++ MEML/tests/parser_tests.ml
@@ -0,0 +1,152 @@
+(** Copyright 2023-2024, Perevalov Efim, Dyachkov Vitaliy *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+open MEML_lib
+open Ast
+open Parser
+
+(* TESTS  PARSER*)
+
+let start_test parser show input =
+  let res = start_parsing parser input in
+  match res with
+  | Ok res -> Format.printf "%s" (show res)
+  | Error err -> Format.printf "%s" err
+;;
+
+(* Test pattern parser *)
+
+let%expect_test _ =
+  let test = "true" in
+  start_test parse_pattern show_pattern test;
+  [%expect {| (PConst (CBool true)) |}]
+;;
+
+let%expect_test _ =
+  let test = "\"itsastring\"" in
+  start_test parse_pattern show_pattern test;
+  [%expect {| (PConst (CString "itsastring")) |}]
+;;
+
+let%expect_test _ =
+  let test = "951753" in
+  start_test parse_pattern show_pattern test;
+  [%expect {| (PConst (CInt 951753)) |}]
+;;
+
+let%expect_test _ =
+  let test = "var" in
+  start_test parse_pattern show_pattern test;
+  [%expect {| (PVar ("var", TUnknown)) |}]
+;;
+
+let%expect_test _ =
+  let test = "(var : string)" in
+  start_test parse_pattern show_pattern test;
+  [%expect {| (PVar ("var", TString)) |}]
+;;
+
+let%expect_test _ =
+  let test = "let" in
+  start_test parse_pattern show_pattern test;
+  [%expect {| : You can not use "let" keywords as vars |}]
+;;
+
+(* Test expression *)
+
+(* EConst *)
+
+let%expect_test _ =
+  let test = "1" in
+  start_test parse_econst show_expression test;
+  [%expect {| (EConst (CInt 1)) |}]
+;;
+
+let%expect_test _ =
+  let test = "false" in
+  start_test parse_econst show_expression test;
+  [%expect {| (EConst (CBool false)) |}]
+;;
+
+let%expect_test _ =
+  let test = "false" in
+  start_test parse_econst show_expression test;
+  [%expect {| (EConst (CBool false)) |}]
+;;
+
+let%expect_test _ =
+  let test = "\"popka\"" in
+  start_test parse_econst show_expression test;
+  [%expect {| (EConst (CString "popka")) |}]
+;;
+
+(* EVar *)
+
+let%expect_test _ =
+  let test = "papa" in
+  start_test parse_evar show_expression test;
+  [%expect {| (EVar ("papa", TUnknown)) |}]
+;;
+
+let%expect_test _ =
+  let test = "(papa : int)" in
+  start_test parse_evar show_expression test;
+  [%expect {| (EVar ("papa", TInt)) |}]
+;;
+
+(* EBinaryOp *)
+
+let%expect_test _ =
+  let test = "1 + 2" in
+  start_test parse_ebinop show_expression test;
+  [%expect {| (EBinaryOp (Add, (EConst (CInt 1)), (EConst (CInt 2)))) |}]
+;;
+
+let%expect_test _ =
+  let test = "3 + 2 * 4 - 1" in
+  start_test parse_ebinop show_expression test;
+  [%expect
+    {|
+    (EBinaryOp (Sub,
+       (EBinaryOp (Add, (EConst (CInt 3)),
+          (EBinaryOp (Mul, (EConst (CInt 2)), (EConst (CInt 4)))))),
+       (EConst (CInt 1)))) |}]
+;;
+
+let%expect_test _ =
+  let test = "1 + a / 3" in
+  start_test parse_ebinop show_expression test;
+  [%expect
+    {|
+    (EBinaryOp (Add, (EConst (CInt 1)),
+       (EBinaryOp (Div, (EVar ("a", TUnknown)), (EConst (CInt 3)))))) |}]
+;;
+
+let%expect_test _ =
+  let test = "true || false" in
+  start_test parse_ebinop show_expression test;
+  [%expect {| (EBinaryOp (Or, (EConst (CBool true)), (EConst (CBool false)))) |}]
+;;
+
+(* EIfElse *)
+
+let%expect_test _ =
+  let test = "if n = 1 then 1 else 3" in
+  start_test parse_eifelse show_expression test;
+  [%expect
+    {|
+    (EIfElse ((EBinaryOp (Eq, (EVar ("n", TUnknown)), (EConst (CInt 1)))),
+       (EConst (CInt 1)), (EConst (CInt 3)))) |}]
+;;
+
+(* Run time test*)
+
+(* let%expect_test _ =
+  let test = "if n = 1 then 1 else 3" in
+  start_test parse_eifelse show_expression test;
+  [%expect
+    {|
+    (EBinaryOp (Add, (EConst (CInt 1)),
+       (EBinaryOp (Div, (EVar ("a", TUnknown)), (EConst (CInt 3)))))) |}]
+;; *)
