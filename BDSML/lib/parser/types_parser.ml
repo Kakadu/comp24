9cac848379fe720a51c0007e98e3fa039f5ae9be
Kuarni <kuarni22@gmail.com>
2024-10-19 01:08:06 +0300

    feat: add parse_params for types and refactor type_parser code

--- BDSML/lib/parser/types_parser.ml
+++ BDSML/lib/parser/types_parser.ml
@@ -7,25 +7,34 @@ open Ast
 open Angstrom
 open Utils
 
-let parse_single = parse_ident_name >>| fun id -> Type_single id
+let parse_single =
+  let+ id = parse_ident_name in
+  Type_single id
+;;
 
 let parse_tuple s =
-  sep_by1 (char '*' <* ws) s
-  >>= fun t_list ->
+  let* t_list = sep_by1 (char '*' <* ws) s in
   if List.length t_list >= 2
   then return @@ Type_tuple t_list
   else fail "tuple of types length must be >= 2"
 ;;
 
 let parse_fun s =
-  sep_by1 (char '*' <* ws) s
-  >>= fun t_list ->
+  let* t_list = sep_by1 (char '*' <* ws) s in
   if List.length t_list >= 2
   then return @@ Type_tuple t_list
   else fail "types of fun length must be >= 2"
 ;;
 
-let parse_type = fix (fun s -> parse_single <|> parse_tuple s <|> parse_fun s)
+let parse_params s =
+  let* type_param = s <* ws1 in
+  let+ main_type = parse_ident_name in
+  Type_params (type_param, main_type)
+;;
+
+let parse_type =
+  fix (fun s -> rec_remove_parents @@ parse_single <|> parse_tuple s <|> parse_fun s)
+;;
 
 let parse_ident =
   let* name = parse_ident_name in
