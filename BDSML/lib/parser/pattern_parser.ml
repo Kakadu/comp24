55816652e7fa3c9a71b10db3dc90b34dd20eed90
LeonidElkin <yolkinleonwork@gmail.com>
2024-10-19 01:49:06 +0300

    fix: patterns parse

--- BDSML/lib/parser/pattern_parser.ml
+++ BDSML/lib/parser/pattern_parser.ml
@@ -3,11 +3,11 @@ open Angstrom
 open Const_parser
 open Ast
 
-let parse_any = char '_' *> return Pat_any
+let parse_any = check_char '_' *> return Pat_any
 let parse_pconst p = rec_remove_parents p <|> (parse_const >>| fun c -> Pat_constant c)
 
 let parse_ptuple p =
-  sep_by (char ',') p
+  sep_by (check_char ',') p
   >>= function
   | [] -> p
   | [ h ] -> return h
@@ -15,22 +15,17 @@ let parse_ptuple p =
 ;;
 
 let parse_pcons p =
-  let helper = string "::" *> return (fun ptr1 ptr2 -> Pat_cons (ptr1, ptr2)) in
+  let helper = check_string "::" *> return (fun ptr1 ptr2 -> Pat_cons (ptr1, ptr2)) in
   chainr1 p helper
 ;;
 
 let parse_por p =
-  let helper = char '|' *> return (fun ptr1 ptr2 -> Pat_or (ptr1, ptr2)) in
+  let helper = check_char '|' *> return (fun ptr1 ptr2 -> Pat_or (ptr1, ptr2)) in
   chainl1 p helper
 ;;
 
-let parse_plist p = char '[' *> (sep_by (char ';') p >>| fun l -> Pat_list l) <* char ']'
-
-let parse_pconstruct p =
-  choice
-    [ string "None" *> return (Pat_construct ("None", None))
-    ; string "Some" *> return (Pat_construct ("Some", p))
-    ]
+let parse_plist p =
+  remove_square_brackets (sep_by (check_char ';') p >>| fun l -> Pat_list l)
 ;;
 
 let parse_pattern =
@@ -38,6 +33,8 @@ let parse_pattern =
   @@ fun p ->
   let pattern = parse_pconst p in
   let pattern = parse_any <|> pattern in
+  let pattern = parse_por pattern <|> pattern in
+  let pattern = parse_plist pattern <|> pattern in
   let pattern = parse_pcons pattern <|> pattern in
   let pattern = parse_ptuple pattern <|> pattern in
   pattern
