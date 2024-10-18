50aad6d2098345a70a89b88bc213f44db8290491
Kuarni <kuarni22@gmail.com>
2024-10-18 23:24:05 +0300

    feat: add some parsers for type

--- BDSML/lib/parser/types_parser.ml
+++ BDSML/lib/parser/types_parser.ml
@@ -7,10 +7,28 @@ open Ast
 open Angstrom
 open Utils
 
-let rec_remove_parents m = fix (fun t -> remove_parents t <|> m)
+let parse_single = parse_ident_name >>| fun id -> Type_single id
 
+let parse_tuple s =
+  sep_by1 (char '*' <* ws) s
+  >>= fun t_list ->
+  if List.length t_list >= 2
+  then return @@ Type_tuple t_list
+  else fail "tuple of types length must be >= 2"
+;;
+
+let parse_fun s =
+  sep_by1 (char '*' <* ws) s
+  >>= fun t_list ->
+  if List.length t_list >= 2
+  then return @@ Type_tuple t_list
+  else fail "types of fun length must be >= 2"
+;;
+
+let parse_type = fix (fun s -> parse_single <|> parse_tuple s <|> parse_fun s)
 
 let parse_ident =
   let* name = parse_ident_name in
-  
+  let+ id_type = char ':' *> ws *> parse_type >>| Option.some <|> return None in
+  { name; id_type }
 ;;
