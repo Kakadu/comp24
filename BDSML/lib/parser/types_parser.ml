d7df0d824de056497a6ebb52f29ab29aee5b29a3
Kuarni <kuarni22@gmail.com>
2024-10-19 04:42:12 +0300

    fix: make types parser work

--- BDSML/lib/parser/types_parser.ml
+++ BDSML/lib/parser/types_parser.ml
@@ -7,37 +7,48 @@ open Ast
 open Angstrom
 open Utils
 
-let parse_single =
-  let+ id = parse_ident_name in
-  Type_single id
+(** [int] *)
+let parse_single s =
+  (let+ id = parse_ident_name in
+   Type_single id)
+  <|> s
 ;;
 
-let parse_tuple s =
-  let* t_list = sep_by1 (char '*' <* ws) s in
-  if List.length t_list >= 2
-  then return @@ Type_tuple t_list
-  else fail "tuple of types length must be >= 2"
-;;
-
-let parse_fun s =
-  let* t_list = sep_by1 (char '*' <* ws) s in
-  if List.length t_list >= 2
-  then return @@ Type_tuple t_list
-  else fail "types of fun length must be >= 2"
-;;
-
+(** [int t list] *)
 let parse_params s =
-  let* type_param = s <* ws1 in
-  let+ main_type = parse_ident_name in
-  Type_params (type_param, main_type)
+  let rec go acc =
+    (let+ main_type = ws1 *> parse_ident_name in
+     Type_params (acc, main_type))
+    >>= go
+    <|> return acc
+  in
+  let* init = parse_single s in
+  go init
 ;;
 
-let parse_type =
-  fix (fun s -> rec_remove_parents @@ parse_single <|> parse_tuple s <|> parse_fun s)
+(** [int * string * int] *)
+let parse_tuple s =
+  sep_by1 (check_char '*') @@ parse_params s
+  >>= function
+  | _ :: _ :: _ as t_list -> return @@ Type_tuple t_list
+  | [ t ] -> return t
+  | _ -> fail "invalid state in tuple type"
 ;;
 
+(** [int -> int] *)
+let parse_fun s =
+  sep_by1 (check_string "->") @@ parse_tuple s
+  >>= function
+  | _ :: _ :: _ as t_list -> return @@ Type_fun t_list
+  | [ t ] -> return t
+  | _ -> fail "invalid state in function type"
+;;
+
+let parse_type = fix (fun s -> remove_parents @@ parse_fun s <|> parse_fun s)
+
+(** [a:int] *)
 let parse_ident =
   let* name = parse_ident_name in
-  let+ id_type = char ':' *> ws *> parse_type >>| Option.some <|> return None in
+  let+ id_type = char ':' *> parse_type >>| Option.some <|> return None in
   { name; id_type }
 ;;
