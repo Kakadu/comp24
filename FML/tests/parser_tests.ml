1c5e59f7d5ebe3cc1cd7f83c308bed3260b6a5f6
Dmitry Pilyuk <dmitriipiliuk@gmail.com>
2024-10-19 17:30:38 +0300

    Add tests

---
+++ FML/tests/parser_tests.ml
@@ -0,0 +1,24 @@
+(** Copyright 2024-2025, Dmitry Pilyuk, Aleksandr Rozhkov *)
+
+(** SPDX-License-Identifier: LGPL-2.1 *)
+
+open Fml_lib.Parser
+open Fml_lib.Ast
+
+let parse_with_print input =
+  match parse input with
+  | Ok ast -> Format.printf "%a\n" pp_program ast
+  | Error message -> Format.printf "Error: %s\n" message
+;;
+
+let%expect_test _ = parse_with_print {| let n = 5|};
+  [%expect {| [(DDeclaration (NoRec, (PIdentifier "n"), (EConst (CInt 5))))] |}]
+let%expect_test _ = parse_with_print {| let flag = true|};
+  [%expect {| [(DDeclaration (NoRec, (PIdentifier "flag"), (EConst (CBool true))))] |}]
+let%expect_test _ = parse_with_print {| let flag = false|};
+  [%expect {| Error: : end_of_input |}]
+let%expect_test _ = parse_with_print {| let h::tl = lst|};
+  [%expect {|
+    [(DDeclaration (NoRec, (PCons ((PIdentifier "h"), (PIdentifier "tl"))),
+        (EIdentifier "lst")))
+      ] |}]
