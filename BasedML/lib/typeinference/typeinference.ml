affe00425a1d8b616cdd2b30868b758c242df988
Osogi <dragonmail135159@gmail.com>
2024-10-19 11:07:56 +0300

    feat: add expression to infer except letin

--- BasedML/lib/typeinference/typeinference.ml
+++ BasedML/lib/typeinference/typeinference.ml
@@ -10,8 +10,12 @@ let const2type : Ast.constant -> Ast.typeName = function
   | Ast.CInt _ -> Ast.TInt
 ;;
 
-let infer_add_pattern : Ast.pattern -> (state, Ast.typeName) t =
-  fun cpat ->
+type patern_mode =
+  | PMAdd
+  | PMCheck
+
+let infer_pattern : patern_mode -> Ast.pattern -> (state, Ast.typeName) t =
+  fun pm cpat ->
   let get_pat_and_type = function
     | Ast.PNConstraint pat -> fresh_tv >>= fun t -> return (pat, t)
     | Ast.PConstraint (pat, tp) -> return (pat, tp)
@@ -54,24 +58,51 @@ let infer_add_pattern : Ast.pattern -> (state, Ast.typeName) t =
   write_env MapString.empty
   *> let* tp = help cpat in
      let* loc_env = read_env in
-     let new_env =
-       MapString.merge
-         (fun _ old_el new_el ->
-           match new_el with
-           | Some x -> Some x
-           | None -> old_el)
-         glob_env
-         loc_env
-     in
-     write_env new_env *> return tp
+     match pm with
+     | PMAdd ->
+       let new_env =
+         MapString.merge
+           (fun _ old_el new_el ->
+             match new_el with
+             | Some x -> Some x
+             | None -> old_el)
+           glob_env
+           loc_env
+       in
+       write_env new_env *> return tp
+     | PMCheck ->
+       let not_found =
+         MapString.fold
+           (fun name _ acc ->
+             match acc with
+             | None when not (MapString.mem name glob_env) -> Some name
+             | x -> x)
+           loc_env
+           None
+       in
+       (match not_found with
+        | None -> write_env glob_env *> return tp
+        | Some x -> fail (Format.sprintf "Unbound value %s" x))
 ;;
 
 let rec infer_expr : Ast.expr -> (state, Ast.typeName) t = function
   | Ast.EConstant c -> return (const2type c)
   | Ast.EIdentifier name -> infer_ident name
+  | Ast.ENil ->
+    let* tv = fresh_tv in
+    return (Ast.TList tv)
   | Ast.EFunction (arg, body) -> infer_func arg body
+  | Ast.EApplication (fun_exp, arg_exp) -> infer_app fun_exp arg_exp
   | Ast.EIfThenElse (e1, e2, e3) -> infer_ifthenelse e1 e2 e3
-  | _ -> fail "infer_expr: aboba"
+  | Ast.ELetIn (_rec_flag, _pattern, _expr_val, _expr_in) ->
+    fail "boba should be implemented"
+  | Ast.ETuple exp_lst ->
+    let* t_lst = map_list infer_expr exp_lst in
+    return (Ast.TTuple t_lst)
+  | Ast.EMatch (scrutin, pat_exp_lst) -> infer_match scrutin pat_exp_lst
+  | Ast.EConstraint (exp, tp) ->
+    let* exp_tp = infer_expr exp in
+    write_constr (create_constr exp_tp tp) *> return exp_tp
 
 and infer_ident : string -> (state, Ast.typeName) t =
   fun name ->
@@ -79,6 +110,20 @@ and infer_ident : string -> (state, Ast.typeName) t =
   match vt_opt with
   | None -> fail (Format.sprintf "Unbound value: %s" name)
   | Some tp -> return tp
+
+and infer_func : Ast.pattern -> Ast.expr -> (state, Ast.typeName) t =
+  fun pat exp ->
+  let* arg_tp = infer_pattern PMAdd pat in
+  let* exp_tp = infer_expr exp in
+  let fcn_tp = Ast.TFunction (arg_tp, exp_tp) in
+  return fcn_tp
+
+and infer_app : Ast.expr -> Ast.expr -> (state, Ast.typeName) t =
+  fun func_exp args_exp ->
+  let* self_tp = fresh_tv in
+  let* func_tp = infer_expr func_exp in
+  let* arg_tp = infer_expr args_exp in
+  write_constr (create_constr func_tp (Ast.TFunction (arg_tp, self_tp))) *> return self_tp
 
 and infer_ifthenelse : Ast.expr -> Ast.expr -> Ast.expr -> (state, Ast.typeName) t =
   fun e1 e2 e3 ->
@@ -92,12 +137,20 @@ and infer_ifthenelse : Ast.expr -> Ast.expr -> Ast.expr -> (state, Ast.typeName)
   *> write_constr (create_constr tp t3)
   *> return tp
 
-and infer_func : Ast.pattern -> Ast.expr -> (state, Ast.typeName) t =
-  fun pat exp ->
-  let* arg_tp = infer_add_pattern pat in
-  let* exp_tp = infer_expr exp in
-  let fcn_tp = Ast.TFunction (arg_tp, exp_tp) in
-  return fcn_tp
+and infer_match : Ast.pattern -> (Ast.pattern * Ast.expr) list -> (state, Ast.typeName) t =
+  fun scrutin pat_exp_lst ->
+  let* tp = fresh_tv in
+  let* scr_tp = infer_pattern PMCheck scrutin in
+  let* cur_env = read_env in
+  let help (pat, exp) =
+    let* pat_tp = infer_pattern PMAdd pat in
+    let* exp_tp = infer_expr exp in
+    write_constr (create_constr pat_tp scr_tp)
+    *> write_constr (create_constr exp_tp tp)
+    *> return ()
+    <* write_env cur_env
+  in
+  map_list help pat_exp_lst *> return tp
 ;;
 
 let test_infer_exp string_exp =
@@ -120,7 +173,8 @@ let test_infer_exp string_exp =
 
 let%expect_test _ =
   test_infer_exp "fun ((x, y): (int*bool)) -> y";
-  [%expect {|
+  [%expect
+    {|
     res: (TFunction ((TTuple [(TPoly "_p0"); (TPoly "_p1")]), (TPoly "_p1")))
     env: [""x"": (TFFlat (TPoly "_p0")),
      ""y"": (TFFlat (TPoly "_p1")),
@@ -131,7 +185,8 @@ let%expect_test _ =
 
 let%expect_test _ =
   test_infer_exp "fun ((x::y): (int list)) -> y";
-  [%expect {|
+  [%expect
+    {|
     res: (TFunction ((TPoly "_p1"), (TPoly "_p1")))
     env: [""x"": (TFFlat (TPoly "_p0")),
      ""y"": (TFFlat (TPoly "_p1")),
@@ -140,3 +195,30 @@ let%expect_test _ =
      ((TPoly "_p1"), (TList (TPoly "_p0")))
      ] |}]
 ;;
+
+let%expect_test _ =
+  test_infer_exp {|fun f list -> match list with
+  | [] -> list
+  | h :: tl -> h|};
+  [%expect {|
+    res: (TFunction ((TPoly "_p0"), (TFunction ((TPoly "_p1"), (TPoly "_p2")))))
+    env: [""f"": (TFFlat (TPoly "_p0")),
+     ""list"": (TFFlat (TPoly "_p1")),
+     ]
+     constrs: [((TPoly "_p1"), (TPoly "_p2"))
+     ((TPoly "_p2"), (TPoly "_p7"))
+     ((TPoly "_p3"), (TPoly "_p8"))
+     ((TPoly "_p3"), (TList (TPoly "_p5")))
+     ((TPoly "_p4"), (TList (TPoly "_p5")))
+     ((TPoly "_p6"), (TPoly "_p8"))
+     ((TPoly "_p8"), (TList (TPoly "_p7")))
+     ] |}]
+;;
+
+let%expect_test _ =
+  test_infer_exp {|fun f list -> match nolist with
+  | [] -> list
+  | h :: tl -> h|};
+  [%expect {| Infer error: Unbound value nolist |}]
+;;
+
