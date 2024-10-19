e847d12f4b0c2bfef771a2c4726022589dbb9132
RozhkovAleksandr <rozhkov_a@outlook.com>
2024-10-19 16:22:19 +0300

    add expression parsers

--- FML/lib/parser/parser.ml
+++ FML/lib/parser/parser.ml
@@ -43,6 +43,7 @@ let is_keyword = function
 let skip_wspace = skip_while is_whitespace
 let parens p = skip_wspace *> char '(' *> skip_wspace *> p <* skip_wspace <* char ')'
 let sqr_br p = skip_wspace *> char '[' *> skip_wspace *> p <* skip_wspace <* char ']'
+let token s = skip_wspace *> string s
 
 let chainl1 e op =
   let rec go acc = lift2 (fun f x -> f acc x) op e >>= go <|> return acc in
@@ -63,12 +64,20 @@ let parse_identifier constr =
   else fail "Syntax error: invalid identifier name"
 ;;
 
+let identifier =
+  parse_name
+  >>= fun name ->
+  if ((not @@ is_keyword name) && is_lower name.[0]) || name.[0] = '_'
+  then return name
+  else fail "Syntax error: invalid identifier name"
+;;
+
 let parse_int =
   skip_wspace *> take_while1 is_digit <* skip_wspace >>| int_of_string >>| cint
 ;;
 
 let parse_bool =
-  skip_wspace *> string "true" <|> string "false" >>| bool_of_string >>| cbool
+  token "true" <|> string "false" >>| bool_of_string >>| cbool
 ;;
 
 let parse_const constr = choice [ parse_int; parse_bool ] >>| constr
@@ -91,12 +100,12 @@ let parse_tuple_type p_type =
   lift2
     (fun h tl -> ATuple (h :: tl))
     p_type
-    (many1 (skip_wspace *> string "*" *> p_type))
+    (many1 (token "*" *> p_type))
 ;;
 
 let parse_function_type p_type =
   let fun_helper =
-    skip_wspace *> string "->" *> return (fun arg ret -> AFunction (arg, ret))
+    token "->" *> return (fun arg ret -> AFunction (arg, ret))
   in
   chainr1 p_type fun_helper
 ;;
@@ -118,7 +127,7 @@ let parse_pconst = parse_const pconst
 let parse_pnill = sqr_br skip_wspace >>| pnill
 
 let parse_pcons p_pattern =
-  let cons_helper = skip_wspace *> string "::" *> return pcons in
+  let cons_helper = token "::" *> return pcons in
   chainl1 p_pattern cons_helper
 ;;
 
@@ -127,7 +136,7 @@ let parse_ptuple p_pattern =
   @@ lift2
        (fun h tl -> ptuple (h :: tl))
        p_pattern
-       (many1 (skip_wspace *> string "," *> p_pattern))
+       (many1 (token "," *> p_pattern))
 ;;
 
 let parse_pattern_wout_type =
@@ -167,15 +176,56 @@ let parse_etuple p_expr =
   @@ lift2
        (fun h tl -> etuple (h :: tl))
        p_expr
-       (many1 (skip_wspace *> string "," *> p_expr))
+       (many1 (token "," *> p_expr))
 ;;
 
 let parse_efun p_expr =
-  let* args = skip_wspace *> string "fun" *> many1 parse_pattern in
-  let* expr = skip_wspace *> string "->" *> p_expr in
+  let* args = token "fun" *> many1 parse_pattern in
+  let* expr = token "->" *> p_expr in
   match List.rev args with
   | h :: tl -> return @@ List.fold_left (fun acc x -> efun x acc) (efun h expr) tl
   | _ -> fail "Syntax errror"
+;;
+
+let rec parse_bundle pexpr =
+  let pattern_expr = identifier >>| fun var -> PIdentifier var in
+  let expr_with_pattern =
+    pattern_expr
+    >>= fun pat -> parse_bundle pexpr <|> token "=" *> pexpr >>| fun e -> EFun (pat, e)
+  in
+  expr_with_pattern <|> token "fun" *> parse_efun pexpr
+;;
+
+let parse_ebinop chain1 e parse_binop = chain1 e (parse_binop >>| fun op e1 e2 -> EBinaryOperation (op, e1, e2))
+let parse_lbinop = parse_ebinop chainl1
+
+let parse_eif arg =
+  skip_while is_whitespace *>
+    (lift3
+       (fun i t e -> EIf (i, t, e))
+       (token "if" *> arg)
+       (token "then" *> arg)
+       (token "else" *> arg))
+;;
+
+let parse_match pexpr =
+  let pcase patterns pexpr =
+    lift2 (fun p e -> p, e) (token "|" *> patterns) (token "->" *> pexpr)
+  in
+  lift2
+    (fun expr cases -> EMatch (expr, cases))
+    (token "match" *> pexpr <* token "with")
+    (many1 (pcase parse_pattern pexpr))
+;;
+
+let parse_let pexpr =
+  token "let"
+  *> lift4
+       (fun r id e1 e2 -> ELetIn (r, id, e1, e2))
+       (token "rec" *> return Rec <|> return NoRec)
+       (skip_while is_whitespace *> (token "()") <|> identifier >>| fun var -> PIdentifier var)
+       (skip_while is_whitespace *> (token "=") *> pexpr <|> parse_bundle pexpr)
+       (token "in" *> pexpr)
 ;;
 
 let parse_expr =
@@ -183,15 +233,6 @@ let parse_expr =
   @@ fun self ->
   choice
     [ parens self; parse_econst; parse_identifier; parse_etuple self; parse_efun self ]
-;;
-
-let parse_eif arg =
-  skip_while is_whitespace
-  *> lift3
-       (fun i t e -> EIf (i, t, e))
-       (skip_wspace *> string "if" *> arg)
-       (skip_wspace *> string "then" *> arg)
-       (skip_wspace *> string "else" *> arg)
 ;;
 
 (* ------------------------- *)
