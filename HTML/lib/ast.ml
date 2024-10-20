ab3e1833249ade922d1aac4e3715f8c37d3d434a
Parzival-05 <parzivalwasd@gmail.com>
2024-10-21 00:43:36 +0300

    feat: Uprade AST

--- HTML/lib/ast.ml
+++ HTML/lib/ast.ml
@@ -6,76 +6,76 @@ type const =
   | CInt of int (** int *)
   | CBool of bool (** bool *)
   | CNil (** [] *)
-  | CUnit (** () *)
-[@@deriving eq, show { with_path = false }]
-
-type bin_op =
-  | Add (** + *)
-  | Sub (** - *)
-  | Mul (** * *)
-  | Div (** / *)
-  | Leq (** <= *)
-  | Less (** < *)
-  | Geq (** >= *)
-  | Gre (** > *)
-  | Eq (** == *)
-  | Neq (** != *)
-  | And (** && *)
-  | Or (** || *)
-[@@deriving eq, show { with_path = false }]
-
-type unary_op =
-  | Plus (** ~+ *)
-  | Minus (** ~- *)
-  | Not (** ~not *)
+  | CUnit (*** () *)
 [@@deriving eq, show { with_path = false }]
 
 type ident = string [@@deriving eq, show { with_path = false }]
-
-type capitalized_ident = Capitalized_ident of string (** Capitalized idents *)
-[@@deriving eq, show { with_path = false }]
 
 type rec_flag =
   | Recursive (** Recursive *)
   | Not_recursive (** Not recursive *)
 [@@deriving eq, show { with_path = false }]
 
+type ground =
+  | GInt (** Int *)
+  | GBool (** Bool *)
+  | GUnit (** Unit — () *)
+[@@deriving eq, show { with_path = false }]
+
+type typ =
+  | TVar of string (** 'a, 'b, ... *)
+  | TArr of typ * typ (** 'a -> 'b *)
+  | TTuple of typ list (** 'a * 'b *)
+  | TList of typ (** 'a list *)
+  | TGround of ground (** ground *)
+[@@deriving eq, show { with_path = false }]
+
 type pattern =
   | PId of ident (** x *)
-  | PTuple of pattern list (** (x, y) *)
-  | PList of pattern * pattern (** x :: xs *)
+  | PTuple of pattern_typed list (** (x, y) *)
+  | PList of pattern_typed * pattern_typed (** x :: xs *)
   | PConst of const (** 3 *)
 [@@deriving eq, show { with_path = false }]
 
+and pattern_typed = pattern * typ option [@@deriving eq, show { with_path = false }]
+
 type expr =
   | EConst of const (** Const. Examples: 100; true *)
-  | EBinop of expr * bin_op * expr
+  | EBinop of expr * ident * expr
   (** Binary operation. Examples: 2 + 2; (234 * 234) + 234 *)
-  | EUnop of unary_op * expr (** Unary operation. Examples: -(1); (+b) *)
+  | EUnop of ident * expr (** Unary operation. Examples: -(1); (+b) *)
   | EId of ident (** Identifier. Examples: a, b, c *)
-  | EFun of pattern * expr (** Function. Examples: fun x -> x + 1 *)
+  | EFun of pattern_typed * expr (** Function. Examples: fun x -> x + 1 *)
   | EApp of expr * expr (** Application. Examples: f (x - 1) *)
   | EIf of expr * expr * expr
   (** If-then-else. Examples: if x >= y then x - y else y - x *)
   | EList of expr * expr (** Lists. Examples: [1; 2; 3] *)
   | ETuple of expr list (** Tuple. Examples: (1, 2, 3) *)
   | EClsr of decl * expr (** Closure. Examples: let inc x = x + 1 in inc 5*)
-  | EMatch of expr * (pattern * expr) list
+  | EMatch of expr * (pattern_typed * expr) list
   (** Matching. Examples: match l with | hd::tl -> hd | _ -> [] *)
 [@@deriving eq, show { with_path = false }]
 
-and decl = DLet of rec_flag * ident * expr (** Let declarations *)
+and decl =
+  | DLet of rec_flag * ident * expr * typ option (** Let declarations *)
+  | DLetMut of rec_flag * (ident * expr * typ option) list
 [@@deriving eq, show { with_path = false }]
 
 type prog = decl list [@@deriving eq, show { with_path = false }]
 
+let tint = TGround GInt
+let tbool = TGround GBool
+let tunit = TGround GUnit
+let tarrow left_type right_type = TArr (left_type, right_type)
+let ttuple type_list = TTuple type_list
+let tlist typ = TList typ
+let tvar n = TVar n
 let pid id = PId id
 let ptuple p_list = PTuple p_list
 let plist hd tl = PList (hd, tl)
+let p_typed ?(typ = None) (p : pattern) : pattern_typed = p, typ
 let pconst c = PConst c
 let econst c = EConst c
-let ebinop e1 op e2 = EBinop (e1, op, e2)
-let eunop op e = EUnop (op, e)
 let eid i = EId i
 let efun pat e = EFun (pat, e)
 let eapp f args = EApp (f, args)
@@ -84,5 +84,6 @@ let elist hd tl = EList (hd, tl)
 let etuple l = ETuple l
 let eclsr d e = EClsr (d, e)
 let ematch e cl = EMatch (e, cl)
-let dlet rf i e_let = DLet (rf, i, e_let)
+let dlet rf i e_let typ = DLet (rf, i, e_let, typ)
+let dletmut rec_flag let_list = DLetMut (rec_flag, let_list)
 let prog (d_l : decl list) : prog = d_l
--- HTML/lib/ast.mli
+++ HTML/lib/ast.mli
@@ -6,50 +6,17 @@ type const =
   | CInt of int (** int *)
   | CBool of bool (** bool *)
   | CNil (** [] *)
-  | CUnit (** () *)
+  | CUnit (*** () *)
 
 val equal_const : const -> const -> bool
 val pp_const : Format.formatter -> const -> unit
 val show_const : const -> string
-
-type bin_op =
-  | Add (** + *)
-  | Sub (** - *)
-  | Mul (** * *)
-  | Div (** / *)
-  | Leq (** <= *)
-  | Less (** < *)
-  | Geq (** >= *)
-  | Gre (** > *)
-  | Eq (** == *)
-  | Neq (** != *)
-  | And (** && *)
-  | Or (** || *)
-
-val equal_bin_op : bin_op -> bin_op -> bool
-val pp_bin_op : Format.formatter -> bin_op -> unit
-val show_bin_op : bin_op -> string
-
-type unary_op =
-  | Plus (** ~+ *)
-  | Minus (** ~- *)
-  | Not (** ~not *)
-
-val equal_unary_op : unary_op -> unary_op -> bool
-val pp_unary_op : Format.formatter -> unary_op -> unit
-val show_unary_op : unary_op -> string
 
 type ident = string
 
 val equal_ident : ident -> ident -> bool
 val pp_ident : Format.formatter -> ident -> unit
 val show_ident : ident -> string
-
-type capitalized_ident = Capitalized_ident of ident (** Capitalized idents *)
-
-val equal_capitalized_ident : capitalized_ident -> capitalized_ident -> bool
-val pp_capitalized_ident : Format.formatter -> capitalized_ident -> unit
-val show_capitalized_ident : capitalized_ident -> string
 
 type rec_flag =
   | Recursive (** Recursive *)
@@ -59,33 +26,60 @@ val equal_rec_flag : rec_flag -> rec_flag -> bool
 val pp_rec_flag : Format.formatter -> rec_flag -> unit
 val show_rec_flag : rec_flag -> string
 
+type ground =
+  | GInt (** Int *)
+  | GBool (** Bool *)
+  | GUnit (** Unit — () *)
+
+val equal_ground : ground -> ground -> bool
+val pp_ground : Format.formatter -> ground -> unit
+val show_ground : ground -> string
+
+type typ =
+  | TVar of string (** 'a, 'b, ... *)
+  | TArr of typ * typ (** 'a -> 'b *)
+  | TTuple of typ list (** 'a * 'b *)
+  | TList of typ (** 'a list *)
+  | TGround of ground (** ground *)
+
+val equal_typ : typ -> typ -> bool
+val pp_typ : Format.formatter -> typ -> unit
+val show_typ : typ -> string
+
 type pattern =
   | PId of ident (** x *)
-  | PTuple of pattern list (** (x, y) *)
-  | PList of pattern * pattern (** x :: xs *)
+  | PTuple of pattern_typed list (** (x, y) *)
+  | PList of pattern_typed * pattern_typed (** x :: xs *)
   | PConst of const (** 3 *)
 
+and pattern_typed = pattern * typ option
+
 val equal_pattern : pattern -> pattern -> bool
+val equal_pattern_typed : pattern_typed -> pattern_typed -> bool
 val pp_pattern : Format.formatter -> pattern -> unit
 val show_pattern : pattern -> string
+val pp_pattern_typed : Format.formatter -> pattern_typed -> unit
+val show_pattern_typed : pattern_typed -> string
 
 type expr =
   | EConst of const (** Const. Examples: 100; true *)
-  | EBinop of expr * bin_op * expr
+  | EBinop of expr * ident * expr
   (** Binary operation. Examples: 2 + 2; (234 * 234) + 234 *)
-  | EUnop of unary_op * expr (** Unary operation. Examples: -(1); (+b) *)
+  | EUnop of ident * expr (** Unary operation. Examples: -(1); (+b) *)
   | EId of ident (** Identifier. Examples: a, b, c *)
-  | EFun of pattern * expr (** Function. Examples: fun x -> x + 1 *)
+  | EFun of pattern_typed * expr (** Function. Examples: fun x -> x + 1 *)
   | EApp of expr * expr (** Application. Examples: f (x - 1) *)
   | EIf of expr * expr * expr
   (** If-then-else. Examples: if x >= y then x - y else y - x *)
   | EList of expr * expr (** Lists. Examples: [1; 2; 3] *)
   | ETuple of expr list (** Tuple. Examples: (1, 2, 3) *)
   | EClsr of decl * expr (** Closure. Examples: let inc x = x + 1 in inc 5*)
-  | EMatch of expr * (pattern * expr) list
+  | EMatch of expr * (pattern_typed * expr) list
   (** Matching. Examples: match l with | hd::tl -> hd | _ -> [] *)
 
-and decl = DLet of rec_flag * ident * expr (** Let declarations *)
+and decl =
+  | DLet of rec_flag * ident * expr * typ option (** Let declarations *)
+  | DLetMut of rec_flag * (ident * expr * typ option) list
 
 val equal_expr : expr -> expr -> bool
 val equal_decl : decl -> decl -> bool
@@ -99,20 +93,27 @@ type prog = decl list
 val equal_prog : prog -> prog -> bool
 val pp_prog : Format.formatter -> prog -> unit
 val show_prog : prog -> string
+val tint : typ
+val tbool : typ
+val tunit : typ
+val tarrow : typ -> typ -> typ
+val ttuple : typ list -> typ
+val tlist : typ -> typ
+val tvar : string -> typ
 val pid : ident -> pattern
-val ptuple : pattern list -> pattern
-val plist : pattern -> pattern -> pattern
+val ptuple : pattern_typed list -> pattern
+val plist : pattern_typed -> pattern_typed -> pattern
+val p_typed : ?typ:typ option -> pattern -> pattern_typed
 val pconst : const -> pattern
 val econst : const -> expr
-val ebinop : expr -> bin_op -> expr -> expr
-val eunop : unary_op -> expr -> expr
 val eid : ident -> expr
-val efun : pattern -> expr -> expr
+val efun : pattern_typed -> expr -> expr
 val eapp : expr -> expr -> expr
 val eif : expr -> expr -> expr -> expr
 val elist : expr -> expr -> expr
 val etuple : expr list -> expr
 val eclsr : decl -> expr -> expr
-val ematch : expr -> (pattern * expr) list -> expr
-val dlet : rec_flag -> ident -> expr -> decl
+val ematch : expr -> (pattern_typed * expr) list -> expr
+val dlet : rec_flag -> ident -> expr -> typ option -> decl
+val dletmut : rec_flag -> (ident * expr * typ option) list -> decl
 val prog : decl list -> prog
