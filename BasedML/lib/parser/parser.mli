5c9ff651f1887e121f72b03bf402a75e79779f72
Pavel Averin <pasha2281337alfa@gmail.com>
2024-10-20 22:25:24 +0300

    feat: make p_exp and parse public

--- BasedML/lib/parser/parser.mli
+++ BasedML/lib/parser/parser.mli
@@ -1,6 +1,8 @@
 (** Copyright 2024-2025, Pavel Averin, Alexey Efremov *)
 
 (** SPDX-License-Identifier: LGPL-2.1 *)
-val parse_program : string -> (Ast.let_declaration list, string) result
 
+val p_exp : Ast.expr Angstrom.t
+val parse : 'a Angstrom.t -> string -> ('a, string) result
+val parse_program : string -> (Ast.let_declaration list, string) result
 val test_parse : string -> unit
