90f7c97703dd89da2aa48cf994aaee6d4d4d073a
Osogi <dragonmail135159@gmail.com>
2024-10-20 19:30:10 +0300

    feat: add binops to start state

--- BasedML/lib/typeinference/startState.ml
+++ BasedML/lib/typeinference/startState.ml
@@ -6,11 +6,40 @@
 open StatementInfer
 open Help
 
-let add_bin_op : string -> Ast.typeName -> Ast.typeName -> Ast.typeName -> (state, unit) t
-  =
-  fun name _arg1 arg2 _ret ->
-  let tp = Ast.TFunction (arg2, arg2) in
-  write_flat_var_type name tp *> fail "boba"
+type start_bin_op = string * Ast.typeName * Ast.typeName * Ast.typeName
+
+let add_bin_op : start_bin_op -> (state, unit) t =
+  fun (name, arg1, arg2, ret) ->
+  let tvs = List.fold_left get_tv_from_tp SetString.empty [ arg1; arg2; ret ] in
+  let tp = Ast.TFunction (arg1, Ast.TFunction (arg2, ret)) in
+  if SetString.is_empty tvs
+  then write_flat_var_type name tp
+  else write_var_type name (TFSchem (tvs, tp))
 ;;
 
-let start_state = MapString.empty, [], 0
+let bin_op_list : start_bin_op list =
+  Ast.
+    [ "( - )", TInt, TInt, TInt
+    ; "( + )", TInt, TInt, TInt
+    ; "( / )", TInt, TInt, TInt
+    ; "( * )", TInt, TInt, TInt
+    ; "( < )", TPoly "'_s", TPoly "'_s", TBool
+    ; "( <= )", TPoly "'_s", TPoly "'_s", TBool
+    ; "( >= )", TPoly "'_s", TPoly "'_s", TBool
+    ; "( <> )", TPoly "'_s", TPoly "'_s", TBool
+    ; "( > )", TPoly "'_s", TPoly "'_s", TBool
+    ; "( = )", TPoly "'_s", TPoly "'_s", TBool
+    ; "( :: )", TPoly "'_s", TList (TPoly "'_s"), TList (TPoly "'_s")
+    ]
+;;
+
+let empty_state : state = MapString.empty, [], 0
+let (start_state : state), res = run (map_list add_bin_op bin_op_list) empty_state
+
+let _check =
+  match res with
+  | Result.Error x ->
+    let _ = Format.printf "Error during init start state for infer:\n\t%s\n" x in
+    exit (-1)
+  | _ -> ()
+;;
