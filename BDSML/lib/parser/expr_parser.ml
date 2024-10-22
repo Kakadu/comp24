6f7929fcf58d3f4503c5be4f7293160a0c4c6cba
Kuarni <kuarni22@gmail.com>
2024-10-23 01:21:45 +0300

    feat: rewise operators concept

--- BDSML/lib/parser/expr_parser.ml
+++ BDSML/lib/parser/expr_parser.ml
@@ -2,134 +2,80 @@
 
 (** SPDX-License-Identifier: LGPL-2.1-or-later *)
 
-open Base
 open Angstrom
 open Ast
 open Utils
 
-type associativity = Left | Right | Unspec
-type args = Unary | Binary
+(** https://ocaml.org/manual/5.2/lex.html#sss:lex-ops-symbols *)
+let is_core_operator_char = function
+  | '$' | '&' | '*' | '+' | '-' | '/' | '=' | '>' | '@' | '^' | '|' -> true
+  | _ -> false
+;;
 
-type op_data =  {
-  args : args;
-  assoc : associativity;
-  priority : int;
-}
+let is_operator_char a =
+  is_core_operator_char a
+  ||
+  match a with
+  | '~' | '!' | '?' | '%' | '<' | ':' | '.' -> true
+  | _ -> false
+;;
 
-(**https://ocaml.org/manual/5.2/api/Ocaml_operators.html*)
-let get_prefix_symbol_op_data_next_need = function
-| '~'-> Some({args = Unary; assoc = Unspec priority = 20})
-| '#'-> Some({args = Binary; assoc = Left priority = 18})
-| _ -> None
-
-let get_prefix_symbol_op_data_next_unneed = function
-| '!'-> Some({args = Unary; assoc = Unspec; priority = 20})
-| '*'-> Some({args = Binary; assoc = Left, priority =14})
-| '/'-> Some({args = Binary; assoc = Left, priority =14})
-| '%'-> Some({args = Binary; assoc = Left, priority =14})
-| '+'-> Some({args = Binary; assoc = Left, priority =13})
-| '-'-> Some({args = Binary; assoc = Left, priority =13})
-| '@'-> Some({args = Binary; assoc = Right priority =11})
-| '^'-> Some({args = Binary; assoc = Right priority =11})
-| '='-> Some({args = Binary; assoc = Left, priority =10})
-| '<'-> Some({args = Binary; assoc = Left, priority =10})
-| '>'-> Some({args = Binary; assoc = Left, priority =10})
-| '|'-> Some({args = Binary; assoc = Left, priority =10})
-| '&'-> Some({args = Binary; assoc = Left, priority =10})
-| '$'-> Some({args = Binary; assoc = Left, priority =10})
-| _ -> None
-
-let get_prefix_two_symbols_op_data = function
-| "**" -> Some({args = Binary; assoc = Right, priority =15})
-| ':' -> Some({args = Binary; assoc = Right, priority =12})
-| _ -> None
-
-let get_single_op_data = function
-| "-" -> Some({args = Binary; assoc = Unspec; priority = 16})
-| "-." -> Some({args = Binary; assoc = Unspec; priority = 16})
-| "::" -> Some({args = Binary; assoc = Right, priority =12})
-| "!=" -> Some({args = Binary; assoc = Left, priority =10})
-| "&" -> Some({args = Binary; assoc = Right, priority =9})
-| "&&" -> Some({args = Binary; assoc = Right, priority =9})
-| "or" -> Some({args = Binary; assoc = Right, priority =8})
-| "||" -> Some({args = Binary; assoc = Right, priority =8})
-| "," -> Some({args = Binary; assoc = Unspec, priority =7})
-| "<-" -> Some({args = Binary; assoc = Right, priority =6})
-| ":=" -> Some({args = Binary; assoc = Right, priority =6})
-| ";" -> Some({args = Binary; assoc = Right, priority =4})
-| _ -> None
-
-let get_operator_data = 
-let prefix_op =
-  let is_first_char_need_following = function
-    | '?' | '~' -> true
-    | _ -> false
-  in
-  let is_first_char_unneed_following = function
-    | '!' -> true
-    | _ -> false
-  in
-  let is_next_char = function
-    | '$' | '&' | '*' | '+' | '-' | '/' | '=' | '>' | '@' | '^' | '|' | '%' | '<' -> true
-    | _ -> false
-  in
+let parse_prefix_op =
   let case1 =
-    let* first_el = satisfy is_first_char_need_following in
-    let+ other_el = take_while1 is_next_char in
+    let* first_el = char '?' <|> char '~' in
+    let+ other_el = take_while1 is_operator_char in
     Char.escaped first_el ^ other_el
   in
   let case2 =
-    let* first_el = satisfy is_first_char_unneed_following in
-    let+ other_el = take_while is_next_char in
+    let* first_el = char '!' in
+    let+ other_el = take_while is_operator_char in
     Char.escaped first_el ^ other_el
   in
   case1 <|> case2
 ;;
 
-let binary_op =
-  let is_first_char_need_following = function
-    | '#' -> true
-    | _ -> false
-  in
-  let is_first_char_unneed_following = function
-    | '$' | '&' | '*' | '+' | '-' | '/' | '=' | '>' | '@' | '^' | '|' | '%' | '<' -> true
-    | _ -> false
-  in
-  let is_next_char = function
-    | '$'
-    | '&'
-    | '*'
-    | '+'
-    | '-'
-    | '/'
-    | '='
-    | '>'
-    | '@'
-    | '^'
-    | '|'
-    | '%'
-    | '<'
-    | '!'
-    | '.'
-    | ':'
-    | '?'
-    | '~' -> true
-    | _ -> false
-  in
+let parse_infix_op prefix =
   let case1 =
-    let* first_el = satisfy is_first_char_need_following in
-    let+ other_el = take_while1 is_next_char in
+    let+ first_el = char '#'
+    and+ other_el = take_while1 is_operator_char in
     Char.escaped first_el ^ other_el
   in
   let case2 =
-    let* first_el = satisfy is_first_char_unneed_following in
-    let+ other_el = take_while is_next_char in
+    let+ first_el = satisfy is_core_operator_char <|> char '%' <|> char '<'
+    and+ other_el = take_while is_operator_char in
     Char.escaped first_el ^ other_el
   in
-  case1 <|> case2
+  let* check_prefix = peek_string @@ String.length prefix in
+  if String.equal check_prefix prefix then case1 <|> case2 else fail ""
 ;;
 
-let a = and+ char 'a' in char 'b'
+type associativity =
+  | Left
+  | Right
+
+type op_type =
+  | Prefix
+  | Infix of associativity
+
+(** https://ocaml.org/manual/5.2/expr.html#ss%3Aprecedence-and-associativity
+    by priority from higher to lower*)
+let operators =
+  let parse_infix_with_prefixes prefixes = choice (List.map parse_infix_op prefixes) in
+  [ Prefix, parse_prefix_op
+  ; Infix Left, parse_infix_op "#"
+  ; Prefix, string "-." <|> string "-"
+  ; Infix Right, parse_infix_op "**"
+  ; Infix Left, parse_infix_with_prefixes [ "*"; "/"; "%" ]
+  ; Infix Left, parse_infix_with_prefixes [ "+"; "-" ]
+  ; Infix Right, string "::"
+  ; Infix Right, parse_infix_with_prefixes [ "@"; "^" ]
+  ; Infix Left, parse_infix_with_prefixes [ "="; "<"; ">"; "|"; "&"; "$" ] <|> string "!="
+  ; Infix Right, string "&" <|> string "&&"
+  ; Infix Right, string "or" <|> string "||"
+  ; Infix Right, string "<-" <|> string ":="
+  ; Infix Right, string ";"
+  ]
+;;
 
 let parse_ident =
   let+ ident = parse_lowercase_ident in
