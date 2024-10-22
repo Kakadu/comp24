af19e34923ef36a051a78a7e0997fbc252246025
Ilya Syresenkov <ilya.syresenkov.04@gmail.com>
2024-10-22 19:33:06 +0300

    tests: add typechecker tests

--- RoflanML/lib/tests.ml
+++ RoflanML/lib/tests.ml
@@ -173,3 +173,161 @@ module ParserTests = struct
        )) |}]
   ;;
 end
+
+module TypecheckerTests = struct
+  open Typechecker
+  open Typing
+
+  let pp_infer e =
+    match run_infer e with
+    | Ok ty -> Stdlib.Format.printf "%a" pp_ty ty
+    | Error err -> Stdlib.Format.printf "%a" pp_error err
+  ;;
+
+  let pp_parse_and_infer input =
+    match Parser.parse_expr input with
+    | Result.Ok e -> pp_infer e
+    | _ -> Stdlib.print_endline "Failed to parse"
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer "let x = (42, false, fun x -> x)";
+    [%expect {| int * bool * ('0 -> '0) |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer "let f x y = [x; y; x = y]";
+    [%expect {| bool -> bool -> bool list |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer "let f x y = [x; y] in f 42";
+    [%expect {| int -> int list |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer
+      "let rec fact x useless_var = if x = 1 then x else x * fact (x - 1) useless_var";
+    [%expect {| int -> '2 -> int |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer "let rec fact x = if x = 1 then x else x * fact (x - 1) in fact 42";
+    [%expect {| int |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer "let f x = match x with | (h :: tl1) :: tl2 -> true | _ -> false";
+    [%expect {| '2 list list -> bool |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer
+      {|
+      let rec fold_left op acc xs = match xs with
+      | []   -> acc
+      | h :: t -> fold_left op (op acc h) t
+      |};
+    [%expect {| ('12 -> '6 -> '12) -> '12 -> '6 list -> '12 |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer
+      {|
+    let split xs = match xs with 
+      | a :: b :: tl -> a, b, tl
+    |};
+    [%expect {| '3 list -> '3 * '3 * '3 list |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer
+      {|
+    let rec even_length xs = match xs with 
+    | h :: h :: tl -> even_length tl
+    | h :: [] -> false
+    | _ -> true
+    |};
+    [%expect {| '7 list -> bool |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer "[(fun x y -> x = y); (fun x y -> x <> y)]";
+    [%expect {| ('3 -> '3 -> bool) list |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer "fun x -> x, fun x y -> x, y";
+    [%expect {| '0 -> '0 * ('1 -> '2 -> '1 * '2) |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer "(fun x -> x), (fun x y -> x, y)";
+    [%expect {| ('0 -> '0) * ('1 -> '2 -> '1 * '2) |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer
+      {| let permutation_of_123 l =
+        match l with
+        | 1 :: 2 :: 3 :: []
+        | 1 :: 3 :: 2 :: []
+        | 2 :: 1 :: 3 :: []
+        | 2 :: 3 :: 1 :: []
+        | 3 :: 1 :: 2 :: []
+        | 3 :: 2 :: 1 :: [] -> true
+        | _ -> false |};
+    [%expect {| int list -> bool |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer
+      "let f (x: int) (y: bool) (z: int) = if y then x, y, x + 1 else x + 1, y, x";
+    [%expect {| int -> bool -> int -> int * bool * int |}]
+  ;;
+
+  (* Errors *)
+
+  let%expect_test _ =
+    pp_parse_and_infer "let f x = x + y";
+    [%expect {| Unbound value y |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer "let rec f x = f";
+    [%expect {| The type variable '0 occurs inside '1 -> '0 |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer "[1; 1, 2]";
+    [%expect {| Failed to unify types int and int * int |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer "let f x = [fun x -> x + x; fun x -> x >= x]";
+    [%expect {| Failed to unify types int and bool |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer "let () = if true then 1";
+    [%expect {| Failed to unify types int and unit |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer
+      "let f x = match x with | a :: b -> a | ((a :: true) :: c) :: tl -> c ";
+    [%expect {| Failed to unify types '4 list and bool |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer
+      "let f x = match x with | a :: b :: y :: tl | a :: b :: z :: tl -> tl";
+    [%expect {| Variable z doesn't occure in some 'or' patterns |}]
+  ;;
+
+  let%expect_test _ =
+    pp_parse_and_infer "let f x = match x with | a :: 1 :: _ | a :: false :: _ -> a";
+    [%expect
+      {| Variable a has different types in 'or' patterns: int and bool are not equal |}]
+  ;;
+end
