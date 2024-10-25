1122c1a9b5c55238a2fbe3b248a2e5f8ad61a37b
Lesh79 <lesh79.zaebal@gmail.com>
2024-10-25 16:47:54 +0300

    Refactor: added name for every tests

--- RoflanML/lib/tests.ml
+++ RoflanML/lib/tests.ml
@@ -14,18 +14,18 @@ module ParserTests = struct
     | _ -> Stdlib.print_endline "Failed to parse"
   ;;
 
-  let%expect_test _ =
+  let%expect_test "simple function definition" =
     pp pp_expr parse_expr "let f x = x";
     [%expect {| (ELet (NonRec, "f", (EFun (("x", None), (EVar "x"))), None)) |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "if-then-else branch" =
     pp pp_expr parse_expr "if f x then x else 0";
     [%expect
       {| (EBranch ((EApp ((EVar "f"), (EVar "x"))), (EVar "x"), (EConst (CInt 0)))) |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "list with arithmetic operations" =
     pp pp_expr parse_expr "[1 + 2; 1 * 2; 1 - 2; 1 / 2]";
     [%expect
       {|
@@ -37,7 +37,7 @@ module ParserTests = struct
       |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "match expression with pattern matching" =
     pp
       pp_expr
       parse_expr
@@ -54,7 +54,7 @@ module ParserTests = struct
       |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "tuple of lists" =
     pp pp_expr parse_expr "[1; 2], [3; 4]";
     [%expect
       {|
@@ -63,7 +63,7 @@ module ParserTests = struct
     |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "nested tuples in a list" =
     pp pp_expr parse_expr "[1, 2, 3; 4, 5, 6]";
     [%expect
       {|
@@ -73,12 +73,12 @@ module ParserTests = struct
     |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "unit let binding" =
     pp pp_expr parse_expr "let () = () in ()";
     [%expect {| (ELet (NonRec, "()", (EConst CUnit), (Some (EConst CUnit)))) |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "function with type annotation" =
     pp pp_expr parse_expr "let f (x : bool) (y: int)= x * y";
     [%expect
       {|
@@ -91,7 +91,7 @@ module ParserTests = struct
       |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "operator function definition" =
     pp pp_expr parse_expr "let (+) = fun (x: int) (y: int) -> x * y";
     [%expect
       {|
@@ -104,7 +104,7 @@ module ParserTests = struct
       |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "nested let and operations in function" =
     pp pp_expr parse_expr "let ( + ) (x: int) y = let ( - ) x (y: int) = x + y in x - y";
     [%expect
       {|
@@ -123,7 +123,7 @@ module ParserTests = struct
       |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "recursive function with nested application" =
     pp pp_expr parse_expr "let fix f = f (fix f) x in fix g";
     [%expect
       {|
@@ -136,7 +136,7 @@ module ParserTests = struct
       |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "higher order function with tuple and list" =
     pp pp_expr parse_expr "let f = fun (g: int -> int -> int) (x: int) (y: int) -> g x y";
     [%expect
       {|
@@ -151,7 +151,7 @@ module ParserTests = struct
       |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "tuple argument type in function" =
     pp pp_expr parse_expr "let f (x: int * bool) = x";
     [%expect
       {|
@@ -160,7 +160,7 @@ module ParserTests = struct
       |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "function with tuple argument to unit type" =
     pp pp_expr parse_expr "let f (x: (int * bool) -> unit) = x";
     [%expect
       {|
@@ -171,13 +171,13 @@ module ParserTests = struct
       |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "function with list type argument" =
     pp pp_expr parse_expr "let f (x: int list) = x";
     [%expect
       {| (ELet (NonRec, "f", (EFun (("x", (Some (TList TInt))), (EVar "x"))), None)) |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "function with complex tuple and list types" =
     pp pp_expr parse_expr "let f (x: int * bool list * bool) = x";
     [%expect
       {|
@@ -187,7 +187,7 @@ module ParserTests = struct
       |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "application of operator within function body" =
     pp pp_expr parse_expr "let f x y = ( > ) x y";
     [%expect
       {|
@@ -200,7 +200,7 @@ module ParserTests = struct
       |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "nested let with function and operator application" =
     pp pp_expr parse_expr "let x = let f g x y = g x y in f ( + ) 7 8";
     [%expect
       {|
@@ -220,7 +220,7 @@ module ParserTests = struct
       |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "chained equality in custom operator function" =
     pp pp_expr parse_expr "let ( ^-^ ) x y z = x = y = z";
     [%expect
       {|
@@ -256,38 +256,38 @@ module TypecheckerTests = struct
     | _ -> Stdlib.print_endline "Failed to parse"
   ;;
 
-  let%expect_test _ =
+  let%expect_test "type inference for tuple with function" =
     pp_parse_and_infer "let x = (42, false, fun x -> x)";
     [%expect {| int * bool * ('a -> 'a) |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "inference for list of booleans with equality" =
     pp_parse_and_infer "let f x y = [x; y; x = y]";
     [%expect {| bool -> bool -> bool list |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "inference for function returning a list" =
     pp_parse_and_infer "let f x y = [x; y] in f 42";
     [%expect {| int -> int list |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "inference for recursive factorial function" =
     pp_parse_and_infer
       "let rec fact x useless_var = if x = 1 then x else x * fact (x - 1) useless_var";
     [%expect {| int -> 'a -> int |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "type of recursive factorial function application" =
     pp_parse_and_infer "let rec fact x = if x = 1 then x else x * fact (x - 1) in fact 42";
     [%expect {| int |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "nested list matching in function" =
     pp_parse_and_infer "let f x = match x with | (h :: tl1) :: tl2 -> true | _ -> false";
     [%expect {| 'a list list -> bool |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "type inference for fold_left implementation" =
     pp_parse_and_infer
       {|
       let rec fold_left op acc xs = match xs with
@@ -297,7 +297,7 @@ module TypecheckerTests = struct
     [%expect {| ('b -> 'a -> 'b) -> 'b -> 'a list -> 'b |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "inference for pattern matching in split function" =
     pp_parse_and_infer
       {|
     let split xs = match xs with 
@@ -306,7 +306,7 @@ module TypecheckerTests = struct
     [%expect {| 'a list -> 'a * 'a * 'a list |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "even length function with list patterns" =
     pp_parse_and_infer
       {|
     let rec even_length xs = match xs with 
@@ -317,22 +317,22 @@ module TypecheckerTests = struct
     [%expect {| 'a list -> bool |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "list of comparison functions" =
     pp_parse_and_infer "[(fun x y -> x = y); (fun x y -> x <> y)]";
     [%expect {| ('a -> 'a -> bool) list |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "inference for nested functions in tuple" =
     pp_parse_and_infer "fun x -> x, fun x y -> x, y";
     [%expect {| 'a -> 'a * ('b -> 'c -> 'b * 'c) |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "inference for nested functions in tuple" =
     pp_parse_and_infer "(fun x -> x), (fun x y -> x, y)";
     [%expect {| ('a -> 'a) * ('b -> 'c -> 'b * 'c) |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "permutation check on list patterns" =
     pp_parse_and_infer
       {| let permutation_of_123 l =
         match l with
@@ -346,79 +346,79 @@ module TypecheckerTests = struct
     [%expect {| int list -> bool |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "inference for conditional with mixed return types" =
     pp_parse_and_infer
       "let f (x: int) (y: bool) (z: int) = if y then x, y, x + 1 else x + 1, y, x";
     [%expect {| int -> bool -> int -> int * bool * int |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "partial application of function with annotation" =
     pp_parse_and_infer "let not_apply (g: int -> int) x = g";
     [%expect {| (int -> int) -> 'a -> int -> int |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "function passing as argument with annotation" =
     pp_parse_and_infer
       "let _ (f: (int -> int) -> unit) (g: int -> int -> int) (x: int) = f (g x)";
     [%expect {| ((int -> int) -> unit) -> (int -> int -> int) -> int -> unit |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "inference for custom operator with multiple arguments" =
     pp_parse_and_infer "let ( + ) x y z = x * y * z in 1 + 2";
     [%expect {| int -> int |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "chained comparison in custom operator" =
     pp_parse_and_infer "let ( ^-^ ) x y z = x = y = z";
     [%expect {| 'a -> 'a -> bool -> bool |}]
   ;;
 
   (* Errors *)
 
-  let%expect_test _ =
+  let%expect_test "unbound value error in expression" =
     pp_parse_and_infer "let f x = x + y";
     [%expect {| Unbound value y |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "type recursion error in self-reference" =
     pp_parse_and_infer "let rec f x = f";
     [%expect {| The type variable 'a occurs inside 'b -> 'a |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "type mismatch error in list of mixed types" =
     pp_parse_and_infer "[1; 1, 2]";
     [%expect {| Failed to unify types int and int * int |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "type mismatch in function list with different return types" =
     pp_parse_and_infer "let f x = [fun x -> x + x; fun x -> x >= x]";
     [%expect {| Failed to unify types int and bool |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "type mismatch in conditional branches" =
     pp_parse_and_infer "let () = if true then 1";
     [%expect {| Failed to unify types int and unit |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "pattern matching type error in nested lists" =
     pp_parse_and_infer
       "let f x = match x with | a :: b -> a | ((a :: true) :: c) :: tl -> c ";
     [%expect {| Failed to unify types 'a list and bool |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "error in 'or' patterns with unmatched variable" =
     pp_parse_and_infer
       "let f x = match x with | a :: b :: y :: tl | a :: b :: z :: tl -> tl";
     [%expect {| Variable z doesn't occure in some 'or' patterns |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "type error in 'or' pattern with conflicting types" =
     pp_parse_and_infer "let f x = match x with | a :: 1 :: _ | a :: false :: _ -> a";
     [%expect
       {| Variable a has different types in 'or' patterns: int and bool are not equal |}]
   ;;
 
-  let%expect_test _ =
+  let%expect_test "argument type mismatch in function application" =
     pp_parse_and_infer "let apply (g: int -> int) (x: bool) = g x";
     [%expect {| Failed to unify types int and bool |}]
   ;;
