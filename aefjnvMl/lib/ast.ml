284d36f2bce79ca111848c5aa253c14a0bf474ba
Artem Rhzankov <Artemr2004@rambler.ru>
2024-10-27 16:03:20 +0300

    feat: update ast

--- aefjnvMl/lib/ast.ml
+++ aefjnvMl/lib/ast.ml
@@ -1,4 +1,4 @@
-(** Copyright 2023, Artem-Rzhankoff, ItIsMrLaG *)
+(** Copyright 2024, Artem-Rzhankoff, ItIsMrLag *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
@@ -9,17 +9,19 @@ type rec_flag =
   | Recursive
 [@@deriving eq, show { with_path = false }]
 
-type type_ident = 
-  | Int
-  | Bool
-  | Polymorfic_id of ident (* TODO: is it actual??? *)
-  | Id of ident 
-[@@deriving show { with_path = false }]
 
 type const =
   | Const_int of int (** Integers constants such as [52] *)
   | Const_bool of bool (** Boolean constant: [true], [false]*)
   | Const_nil (** Represents empty list [[]] *)
+[@@deriving show { with_path = false }]
+
+type core_type = 
+  | Ptyp_int
+  | Ptyp_bool
+  | Ptyp_var of ident
+  | Ptyp_tuple of core_type list
+  | Ptyp_arrow of core_type * core_type
 [@@deriving show { with_path = false }]
 
 type pattern =
@@ -28,7 +30,8 @@ type pattern =
   | Pat_cons of pattern * pattern (** The pattern such as [P1::P2] *)
   | Pat_any (** The pattern [_] *)
   | Pat_tuple of pattern list (** Patterns [(P1, ..., P2)]
-  Invariant : [n >= 2]*)
+                                  Invariant : [n >= 2]*)
+  | Pat_constraint of pattern * core_type
 [@@deriving show { with_path = false }]
 
 type expression =
@@ -36,7 +39,7 @@ type expression =
   | Exp_ident of ident (** Identifiers such as [x] *)
   | Exp_tuple of expression list (** Expressions [(E1, ..., En)]
                                      Invariant: [n >= 2] *)
-  | Exp_function of (pattern * type_ident option) * expression (** [fun P1 -> E] or  *)
+  | Exp_function of pattern * expression (** [fun P1 -> E] *)
   | Exp_let of decl * expression
   (** [Exp_let({d_rec=flag; P; E}, E')] represents:
       - [let P = E in E'] when [flag] is {{!rec_flag.Nonrecursive} [Nonrecursive]}
@@ -44,8 +47,8 @@ type expression =
   | Exp_match of expression * (pattern * expression) list
   (** [match E0 with P1 -> E1 | .. | Pn -> En] *)
   | Exp_ifthenelse of expression * expression * expression (** [if E1 then E2 else E3] *)
-  | Exp_apply of expression * expression list (** [E0 E1 E2 ... En] *)
-  (*  | Exp_list of expression list (** [[E1; ..; En]]*)*)
+  | Exp_apply of expression * expression list (** [E0 E1 ... En]
+  Invariant: n > 1 *)
   | Exp_list of expression * expression
   (** The expression such as [E1::E2]
       This also represents lists [E1; ... En] via [E]*)
@@ -56,7 +59,7 @@ type expression =
     - [let rec P = E] when [d_rec] is {{!rec_flag.Recursive} [Recursive]} *)
 and decl =
   { d_rec : rec_flag
-  ; d_pat : pattern * type_ident option
+  ; d_pat : pattern
   ; d_expr : expression
   }
 
