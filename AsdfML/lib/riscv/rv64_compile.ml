b9b58f8aaeb71f5ef0a88930a5e488a8544d6515
ArtemKhel <ArtemKhel@gmail.com>
2024-12-01 15:59:40 +0300

    ...

--- AsdfML/lib/riscv/rv64_compile.ml
+++ AsdfML/lib/riscv/rv64_compile.ml
@@ -6,25 +6,25 @@ let extern =
   |> String.concat_lines
 ;;
 
-let test =
-  Format.sprintf
-    {|
+let section_data = {|
     .section .data
-%s
-
-_start:
-    call main
-
-    # syscall exit 0
-    li a7, 93
-    li a0, 0
-    ecall
-
-main:
-    li a0, 0
-    ret
 |}
-    extern
+
+let codegen (prog : Cf_ast.program) =
+  let open Cf_ast in
+  let gen_def = function
+    | CFDLet (id, expr) -> failwith "codegen: unsupported definition"
+  in
+  let gen_expr = function
+    | CFConst c -> failwith "codegen: unsupported const"
+    | CFVar v -> failwith "codegen: unsupported var"
+    | CFApp (f, arg) -> failwith "codegen: unsupported app"
+    | CFIfElse (cond, if_expr, else_expr) -> failwith "codegen: unsupported if-else"
+    | CFFun (args, body) -> failwith "codegen: unsupported fun"
+    | CFLetIn (def, exp) -> failwith "codegen: unsupported let-in"
+    | _ -> failwith "codegen: unsupported match"
+  in
+  List.map prog ~f:gen_def
 ;;
 
 let compile () =
@@ -32,7 +32,6 @@ let compile () =
   Stdio.Out_channel.with_file "/tmp/out.s" ~f:(fun out ->
     let fmt = Format.formatter_of_out_channel out in
     let print = Format.fprintf fmt "%s" in
-    print test;
     ());
   Ok ()
 ;;
