96b10a09a8d59a701193b624beef63f15d91dbbc
ArtemKhel <ArtemKhel@gmail.com>
2025-01-01 22:37:22 +0300

    simplified ast

--- AsdfML/lib/transforms/closure_conversion.ml
+++ AsdfML/lib/transforms/closure_conversion.ml
@@ -8,7 +8,7 @@ open Base
 open Utils
 open Vars
 open Types
-
+open Sast
 
 let env_to_str env =
   Map.to_alist env
@@ -16,60 +16,60 @@ let env_to_str env =
   |> String.concat ~sep:", "
 ;;
 
+let pat_list_to_id_list =
+  List.map ~f:(function
+    | PIdent id -> id
+    | PWild -> "_"
+    | _ -> failwith "not id")
+;;
+
 let rec cc_expr globals env ?(apply = true) = function
-  | TEConst _ as c -> c
-  | TEVar (_, id) as var ->
+  | TEConst (_, c) -> s_const c
+  | TEVar (_, id) ->
     (match Map.find env id with
-     | None -> var
+     | None -> s_var id
      | Some fvs ->
        dbg "FV in %s: %s\n" id (set_to_string fvs);
-       fvs
-       |> Set.to_list
-       |> List.map ~f:(te_var dummy_ty)
-       |> List.fold ~init:var ~f:(te_app dummy_ty))
-  | TEApp (ty, l, r) -> te_app ty (cc_expr globals env l) (cc_expr globals env r)
-  | TEIfElse (ty, c, t, e) ->
-    te_if_else ty (cc_expr globals env c) (cc_expr globals env t) (cc_expr globals env e)
-  | TEFun (ty, pat, body) as func ->
+       fvs |> Set.to_list |> List.map ~f:s_var |> List.fold ~init:(s_var id) ~f:s_app)
+  | TEApp (_, l, r) -> s_app (cc_expr globals env l) (cc_expr globals env r)
+  | TEIfElse (_, c, t, e) ->
+    s_if_else (cc_expr globals env c) (cc_expr globals env t) (cc_expr globals env e)
+  | TEFun (_, pat, body) as func ->
     let fvs = Set.diff (free_vars_texpr func) globals |> Set.to_list in
     dbg "FVs %s in fun\n%a\n" (list_to_string fvs) Tast.pp_texpr func;
     let body' = cc_expr globals env body in
     (match fvs with
-     | [] -> te_fun ty pat body'
+     | [] -> s_fun (pat_list_to_id_list pat) body'
      | _ ->
        dbg
          "Creating (apply:%b) closure with FVs %s\nand body %a\n"
          apply
          (list_to_string fvs)
-         Tast.pp_texpr
+         Sast.pp_sexpr
          body';
        let pat = fvs |> List.map ~f:p_ident |> (Fn.flip List.append) pat in
-       let closure_fun = te_fun ty pat body' in
+       let closure_fun = s_fun (pat_list_to_id_list pat) body' in
        if apply
-       then
-         fvs
-         |> List.map ~f:(te_var dummy_ty)
-         |> List.fold ~init:closure_fun ~f:(te_app dummy_ty)
+       then fvs |> List.map ~f:s_var |> List.fold ~init:closure_fun ~f:s_app
        else closure_fun)
-  | TELetIn (ty, def, exp) ->
+  | TELetIn (_, def, exp) ->
     let def', env', globals' = cc_def globals env def ~apply:false in
     let exp' = cc_expr globals' env' exp in
-    te_let_in ty def' exp'
-  | TETuple (ty, xs) -> List.map xs ~f:(cc_expr globals env) |> te_tuple ty
-  | TEList (ty, xs) -> List.map xs ~f:(cc_expr globals env) |> te_list ty
+    s_let_in def' exp'
+  | TETuple (_, xs) -> List.map xs ~f:(cc_expr globals env) |> s_tuple
+  | TEList (_, xs) -> List.map xs ~f:(cc_expr globals env) |> s_list
   | TEMatch _ -> failwith "removed by now"
-  (* | _ -> failwith "cc_expr not implemented" *)
 
 and cc_def globals env ?(apply = true) = function
-  | TDLet ((TArrow _ as ty), (_ as flag), (PIdent id as pat), func) as def ->
+  | TDLet (TArrow _, flag, PIdent id, func) as def ->
     let fvs = Set.diff (free_vars_tdef def) globals in
     let env' = Map.set env ~key:id ~data:fvs in
     let globals' = Set.add globals id in
     let func = cc_expr globals' env' func ~apply in
-    td_let_flag flag ty pat func, env', globals'
-  | TDLet (ty, flag, (PIdent _ as pat), exp) ->
+    s_let_flag flag id func, env', globals'
+  | TDLet (_, flag, PIdent id, exp) ->
     let exp' = cc_expr globals env exp in
-    td_let_flag flag ty pat exp', env, globals
+    s_let_flag flag id exp', env, globals
   | _ -> failwith "cc_def not implemented"
 ;;
 
---
+++ AsdfML/lib/transforms/sast.ml
@@ -0,0 +1,64 @@
+open Ast
+
+(* Simplified AST after pattern and match elimination and closure conversion *)
+
+type sexpr =
+  | SConst of constant
+  | SVar of id
+  | SApp of sexpr * sexpr
+  | SIfElse of sexpr * sexpr * sexpr
+  | SFun of id list * sexpr
+  | SLetIn of sdefinition * sexpr
+  | STuple of sexpr list
+  | SList of sexpr list
+[@@deriving show { with_path = false }]
+
+and sdefinition = SLet of Ast.rec_flag * id * sexpr
+[@@deriving show { with_path = false }]
+
+type sprogram = sdefinition list [@@deriving show { with_path = false }]
+
+let s_const c = SConst c
+let s_var x = SVar x
+let s_app f x = SApp (f, x)
+let s_if_else cond e_true e_false = SIfElse (cond, e_true, e_false)
+let s_fun p e = SFun (p, e)
+let s_let_in def e = SLetIn (def, e)
+let s_tuple exprs = STuple exprs
+let s_list exprs = SList exprs
+let s_let_flag r x e = SLet (r, x, e)
+let s_let x e = s_let_flag NonRec x e
+let s_let_rec x e = s_let_flag Rec x e
+
+open Format
+open Utils
+
+let rec pp_sexpr fmt = function
+  | SConst c -> fprintf fmt "%a" Pp_ast.pp_constant c
+  | SVar v -> fprintf fmt "%s" v
+  | SApp (e1, e2) ->
+    (match e1 with
+     | SApp (_, _) ->
+       let rec pp_rest fmt = function
+         | SApp (e1, e2) -> fprintf fmt "%a %a" pp_rest e1 pp_sexpr e2
+         | e -> fprintf fmt "(%a" pp_sexpr e
+       in
+       fprintf fmt "%a %a)" pp_rest e1 pp_sexpr e2
+     | _ -> fprintf fmt "(%a %a)" pp_sexpr e1 pp_sexpr e2)
+  | SIfElse (c, t, e) -> fprintf fmt "if %a then %a else %a" pp_sexpr c pp_sexpr t pp_sexpr e
+  | SFun (p, e) ->
+    fprintf fmt "(fun ";
+    pp_print_list
+      ~pp_sep:(fun fmt _ -> fprintf fmt " ")
+      (fun fmt x -> fprintf fmt "%s" x)
+      fmt
+      p;
+    fprintf fmt " -> %a)" pp_sexpr e
+  | SLetIn (d, e) -> fprintf fmt "%a in %a" pp_sdef d pp_sexpr e
+  | STuple xs -> pp_list ~sep:", " fmt pp_sexpr xs
+  | SList xs -> pp_list ~op:"[" ~cl:"]" ~sep:"; " fmt pp_sexpr xs
+
+and pp_sdef fmt = function
+  | SLet (NonRec, name, e) -> fprintf fmt "let %s = %a\n" name pp_sexpr e
+  | SLet (Rec, name, e) -> fprintf fmt "let rec %s = %a\n" name pp_sexpr e
+;;
