7d5c7b065284d8feb97eb65e9de5ca7b816453e8
ArtemKhel <ArtemKhel@gmail.com>
2025-01-02 20:55:50 +0300

    wip

--- AsdfML/lib/transforms/anf.ml
+++ AsdfML/lib/transforms/anf.ml
@@ -4,7 +4,14 @@ open Cf_ast
 open State.IntStateM
 open State.IntStateM.Syntax
 
-let rec anf_expr (expr : cf_expr) (cont : imm_expr -> aexpr State.IntStateM.t) =
+let new_var () =
+  let* fresh = fresh in
+  return ("a" ^ string_of_int fresh)
+;;
+
+let rec anf_expr env (expr : cf_expr) (cont : imm_expr -> aexpr State.IntStateM.t)
+  : aexpr State.IntStateM.t
+  =
   match expr with
   | CFConst c ->
     (match c with
@@ -13,22 +20,45 @@ let rec anf_expr (expr : cf_expr) (cont : imm_expr -> aexpr State.IntStateM.t) =
      | _ -> failwith "Not implemented")
   | CFVar x -> cont (ImmId x)
   | CFApp (func, arg) ->
-    let* fresh = fresh in
-    let name = string_of_int fresh in
+    let* name = new_var () in
     let* body = cont (ImmId name) in
-    anf_expr func (fun imm_func ->
-      anf_expr arg (fun imm_arg ->
-        let res = ALet (name, CApplication (imm_func, imm_arg), body) in
-        return res))
+    anf_expr env func (fun imm_func ->
+      anf_expr env arg (fun imm_arg ->
+        return (ALet (name, CApp (imm_func, imm_arg), body))))
   | CFIfElse (i, t, e) ->
-    let* fresh = fresh in
-    let name = string_of_int fresh in
+    let* name = new_var () in
     let* body = cont (ImmId name) in
-    anf_expr i (fun i ->
-      let* t = anf_expr t (fun x -> return @@ ACExpr (CImmExpr x)) in
-      let* e = anf_expr e (fun x -> return @@ ACExpr (CImmExpr x)) in
-      let res = ALet (name, CIfElse (i, t, e), body) in
-      return res)
-  | CFLetIn (def, exp) -> failwith "Not implemented"
-  | _ -> failwith "Not implemented"
+    anf_expr env i (fun i ->
+      let* t = anf_expr env t (fun x -> return @@ ACExpr (CImmExpr x)) in
+      let* e = anf_expr env e (fun x -> return @@ ACExpr (CImmExpr x)) in
+      return (ALet (name, CIfElse (i, t, e), body)))
+  | CFLetIn (id, expr, body) ->
+    let* name = new_var () in
+    let new_env = Map.set env ~key:id ~data:name in
+    anf_expr env expr (fun imm_expr ->
+      let* body = anf_expr new_env body cont in
+      return (ALet (name, CImmExpr imm_expr, body)))
+  | CFTuple _ | CFList _ -> failwith "Not implemented"
+;;
+
+let anf_def env (def : cf_definition) : fn State.IntStateM.t =
+  match def with
+  | CFLet (id, args, expr) ->
+    let* aexpr = anf_expr env expr (fun x -> return @@ ACExpr (CImmExpr x)) in
+    return (Fn (id, args, aexpr))
+;;
+
+type env = string Map.M(String).t
+
+let anf (ast : Cf_ast.program) : Anf_ast.program =
+  let h ast =
+    List.fold
+      ast
+      ~init:(return ([] : fn list))
+      ~f:(fun acc x ->
+        let* fns = acc in
+        let* x = anf_def Map.Poly.empty x in
+        return (x :: fns))
+  in
+  run (h ast)
 ;;
--- AsdfML/lib/transforms/anf_ast.ml
+++ AsdfML/lib/transforms/anf_ast.ml
@@ -6,14 +6,21 @@ type imm_expr =
   | ImmInt of int
   | ImmBool of bool
   | ImmId of id
+  | ImmTuple of imm_expr list
+  | ImmList of imm_expr list
+[@@deriving show { with_path = false }]
 
 type cexpr =
-  | CApplication of imm_expr * imm_expr
+  | CApp of imm_expr * imm_expr
   | CIfElse of imm_expr * aexpr * aexpr
   | CImmExpr of imm_expr
+[@@deriving show { with_path = false }]
 
 and aexpr =
   | ALet of id * cexpr * aexpr
   | ACExpr of cexpr
+[@@deriving show { with_path = false }]
 
-type functions = id * id list * aexpr
+type fn = Fn of id * id list * aexpr [@@deriving show { with_path = false }]
+type program = fn list [@@deriving show { with_path = false }]
+
