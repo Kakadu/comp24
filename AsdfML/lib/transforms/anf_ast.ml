ecaefbc238670e82a47b46e06ed7ee1fae7c212e
ArtemKhel <ArtemKhel@gmail.com>
2025-01-03 17:27:31 +0300

    pp

--- AsdfML/lib/transforms/anf_ast.ml
+++ AsdfML/lib/transforms/anf_ast.ml
@@ -24,3 +24,35 @@ and aexpr =
 type fn = Fn of id * id list * aexpr [@@deriving show { with_path = false }]
 type program = fn list [@@deriving show { with_path = false }]
 
+open Base
+open Format
+open Utils
+
+let rec pp_imm_expr fmt = function
+  | ImmInt n -> fprintf fmt "%d" n
+  | ImmBool b -> fprintf fmt "%b" b
+  | ImmId id -> fprintf fmt "%s" id
+  | ImmTuple xs -> pp_list ~op:"(" ~cl:")" ~sep:", " fmt pp_imm_expr xs
+  | ImmList xs -> pp_list ~op:"[" ~cl:"]" ~sep:"; " fmt pp_imm_expr xs
+;;
+
+let rec pp_cexpr fmt = function
+  | CApp (e1, e2) -> fprintf fmt "%a %a" pp_imm_expr e1 pp_imm_expr e2
+  | CIfElse (c, t, e) ->
+    fprintf fmt "if %a then %a else %a" pp_imm_expr c pp_aexpr t pp_aexpr e
+  | CImmExpr e -> pp_imm_expr fmt e
+
+and pp_aexpr fmt = function
+  | ALet (id, cexpr, aexpr) ->
+    fprintf fmt "let %s = %a in\n%a" id pp_cexpr cexpr pp_aexpr aexpr
+  | ACExpr cexpr -> pp_cexpr fmt cexpr
+;;
+
+let pp_fn fmt = function
+  | Fn (id, args, aexpr) ->
+    (match args with
+     | [] -> fprintf fmt "let %s = %a\n" id pp_aexpr aexpr
+     | _ -> fprintf fmt "let %s %s = %a\n" id (String.concat args ~sep:" ") pp_aexpr aexpr)
+;;
+
+let pp_program fmt = pp_list ~op:"" ~cl:"" ~sep:"\n" fmt pp_fn
