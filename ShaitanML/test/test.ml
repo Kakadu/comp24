ea11e35433767705aafcde2c0c70148f16edff5e
SurfaceYellowDuck <lukonenko.nikitka@mail.ru>
2025-01-03 14:46:37 +0300

    add: cram tests, but not works not works now

---
+++ ShaitanML/test/test.ml
@@ -0,0 +1,7 @@
+open Shaitanml_lib
+let () =
+  let s = Stdio.In_channel.input_all Stdlib.stdin in
+  match Parser.parse s with
+  | Ok ast -> Format.printf "%a\n" Ast.pp_structure ast
+  | Error message -> Format.printf "Error: %s\n" message
+;;
