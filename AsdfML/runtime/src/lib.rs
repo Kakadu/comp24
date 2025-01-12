0ccce53944af99a1ccf7d411c6a9852940ffed6d
ArtemKhel <ArtemKhel@gmail.com>
2025-01-12 23:17:22 +0300

    asm tuples

--- AsdfML/runtime/src/lib.rs
+++ AsdfML/runtime/src/lib.rs
@@ -1,8 +1,12 @@
+#![feature(box_as_ptr)]
+
 use env_logger;
 use log::LevelFilter;
 mod closure;
 mod list;
 mod tuple;
+use std::io::Write;
+
 pub use closure::*;
 pub use list::*;
 pub use tuple::*;
@@ -11,10 +15,12 @@ pub use tuple::*;
 /// This can be overridden by setting the `RUST_LOG` environment variable.
 #[no_mangle]
 pub extern "C" fn runtime_init() {
-    let _ = env_logger::Builder::new()
+    env_logger::Builder::new()
+        .format(|buf, record| writeln!(buf, "[{}]: {}", record.target(), record.args()))
         .filter_level(LevelFilter::Info)
         .parse_default_env()
-        .try_init();
+        .try_init()
+        .unwrap();
 }
 
 macro_rules! make_int_bin_op {
