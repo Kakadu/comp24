86f1ad05ceb062581712ac012d5a2208252b4d3f
Roman Pozharskiy <rnpozharskiy@gmail.com>
2025-01-17 00:21:27 +0300

    cc: extend inner fun closure; ppf

--- KreML/lib/middle_end/Closure_conversion.ml
+++ KreML/lib/middle_end/Closure_conversion.ml
@@ -1,63 +1,17 @@
 open Ast
 open Utils
+open Lambda
 
-(* module Closure_env = struct
-  type t = (ident * expr) list
 
-  let lookup env name =
-    let _, v = List.find (fun (n, _) -> n = name) env in
-    v
-  ;;
-
-  let extend env name value = (name, value) :: env
-end *)
-
-type lambda =
-  | Lconst of const
-  | Lvar of ident
-  | Lclosure of closure
-  | Lapp of lambda * lambda
-  | Lite of lambda * lambda * lambda
-  | Llet of rec_flag * ident * lambda * lambda
-  | Lswitch (* TODO *)
-[@@deriving show]
-
-and closure =
-  { body : lambda
-  ; env : closure_env
-  ; total_args : int
-  ; applied_count : int
-  }
-[@@deriving show]
-
-and closure_env = (int * lambda) list [@@deriving show]
-
-and global_value =
-  | Fun of closure
-  | Var of lambda
-[@@deriving show]
-
-type lprogram = (ident * global_value) list [@@deriving show]
-
-let apply_arg { body; env; total_args; applied_count } arg =
-  Lclosure
-    { body
-    ; total_args
-    ; applied_count = applied_count + 1
-    ; env = (applied_count, arg) :: env
-    }
-;;
-
-let iconst i = Lconst (Const_int i)
-let lclosure body total_args = Lclosure { body; total_args; applied_count = 0; env = [] }
-let lapp f a = Lapp (f, a)
-let lvar v = Lvar v
-let llet id expr scope = Llet (NonRecursive, id, expr, scope)
-let lletrec id expr scope = Llet (Recursive, id, expr, scope)
 
 module CC_state = struct
   type vars = (ident, int, Base.String.comparator_witness) Base.Map.t
+
+  let empty_vars = Base.Map.empty (module Base.String)
+
   type local_env = (ident, lambda, Base.String.comparator_witness) Base.Map.t
+
+  let empty_local_env = empty_vars
 
   type ctx =
     { global_env : lprogram
@@ -67,15 +21,22 @@ module CC_state = struct
     ; names_indexer : int
     }
 
+  let empty_ctx =
+    { global_env = []
+    ; local_env : local_env = empty_vars
+    ; iv = empty_local_env
+    ; indexer = 0
+    ; names_indexer = 0
+    }
+  ;;
+
   module Monad = State (struct
       type t = ctx
     end)
 
   let fresh_idx =
     let open Monad in
-    get
-    >>= fun { indexer; iv; global_env; local_env; names_indexer } ->
-    put { indexer = indexer + 1; iv; global_env; local_env; names_indexer }
+    get >>= fun ({ indexer; _ } as state) -> put { state with indexer = indexer + 1 }
   ;;
 
   let fresh_name =
@@ -104,36 +65,85 @@ module CC_state = struct
   ;;
 end
 
-(* let pattern_Indexed_vars p =
-  let rec helper acc = function
-    | Pat_wildcard | Pat_nil | Pat_const _ -> acc
-    | Pat_var x -> Indexed_vars.add x acc
-    | Pat_cons (x, xs) -> helper (helper acc x) xs
-    | Pat_constrained (p, _) -> helper acc p
-    | Pat_tuple (fst, snd, rest) -> List.fold_left helper acc (fst :: snd :: rest)
-  in
-  helper Indexed_vars.empty p
-;; *)
+module Freevars = struct
+  include Stdlib.Set.Make(String)
 
-(* let rec expr_free_Indexed_vars = function
-  | Expr_const _ | Expr_nil -> Indexed_vars.empty
-  | Expr_fun (p, e) -> Indexed_vars.diff (expr_free_Indexed_vars e) (pattern_Indexed_vars p)
-  | Expr_app (x, y) | Expr_cons (x, y) -> Indexed_vars.union (expr_free_Indexed_vars x) (expr_free_Indexed_vars y)
-  | Expr_constrained (e, _) -> expr_free_Indexed_vars e
-  | Expr_match (e, cases) -> list_free_Indexed_vars (e :: List.map snd cases)
-  | Expr_tuple (fst, snd, rest) -> list_free_Indexed_vars (fst :: snd :: rest)
-  | Expr_var v -> Indexed_vars.singleton v
-  | Expr_ite (c, t, e) -> list_free_Indexed_vars [ c; t; e ]
-  | Expr_let (_, (p, e), scope) ->
-    let scope_fv = expr_free_Indexed_vars scope in
-    let pfv = pattern_Indexed_vars p in
-    Indexed_vars.union (expr_free_Indexed_vars e) (Indexed_vars.diff scope_fv pfv)
+  let rec collect_pat = function
+    | Pat_wildcard | Pat_nil | Pat_const _ -> empty
+    | Pat_var x -> singleton x
+    | Pat_cons (x, xs) -> union (collect_pat x) (collect_pat xs)
+    | Pat_constrained (p, _) -> collect_pat p
+    | Pat_tuple (fst, snd, rest) ->
+      List.fold_left (fun acc p -> union acc (collect_pat p)) empty (fst :: snd :: rest)
+  ;;
 
-and list_free_Indexed_vars l =
-  List.fold_left (fun acc e -> Indexed_vars.union acc (expr_free_Indexed_vars e)) Indexed_vars.empty l
-;; *)
+  let rec collect_expr = function
+    | Expr_const _ | Expr_nil -> empty
+    | Expr_fun (p, e) -> diff (collect_expr e) (collect_pat p)
+    | Expr_app (x, y) | Expr_cons (x, y) -> union (collect_expr x) (collect_expr y)
+    | Expr_constrained (e, _) -> collect_expr e
+    | Expr_match (e, cases) -> collect_list (e :: List.map snd cases)
+    | Expr_tuple (fst, snd, rest) -> collect_list (fst :: snd :: rest)
+    | Expr_var v -> singleton v
+    | Expr_ite (c, t, e) -> collect_list [ c; t; e ]
+    | Expr_let (_, (p, e), scope) ->
+      let scope_fv = collect_expr scope in
+      let pfv = collect_pat p in
+      union (collect_expr e) (diff scope_fv pfv)
+
+  and collect_list l = List.fold_left (fun acc e -> union acc (collect_expr e)) empty l
+end
 
 open CC_state.Monad
+
+(* passes env variables from outer function to inner function *)
+let extend_inner_fun_env call =
+  let* lambda = call in
+  match lambda with
+  | Lclosure { body; env; total_args; applied_count; freevars; _ }
+    when applied_count = total_args ->
+    let rec extend = function
+      | Lclosure
+          ({ body = inner_body; env = innner_env; freevars = inner_free_vars; _ } as
+           inner_fun) ->
+        (* extend inner *)
+        let* extended_body = extend inner_body in
+        let* { iv; local_env; _ } : CC_state.ctx = get in
+        (* find var either in closure or local environment *)
+        let find_var id =
+          match Base.Map.find local_env id with
+          | Some v -> v
+          | None ->
+            let id = CC_state.lookup_iv_exn iv id in
+            List.find (fun (closure_arg_id, _) -> closure_arg_id = id) env |> snd
+        in
+        let intersection = Freevars.inter freevars inner_free_vars in
+        let extended_env =
+          Freevars.fold
+            (fun id acc ->
+              let idx = Base.Map.find_exn iv id in
+              let value = find_var id in
+              (idx, value) :: acc)
+            intersection
+            innner_env
+        in
+        Lclosure { inner_fun with env = extended_env; body = extended_body } |> return
+      | (Lvar _ | Lconst _) as atom -> return atom
+      | Lite (c, t, e) ->
+        let* c = extend c in
+        let* t = extend t in
+        let* e = extend e in
+        Lite (c, t, e) |> return
+      | LApp _ as a -> return a (* for now it is used only for runtime calls *)
+      | Llet (rf, id, e, scope) ->
+        let* e = extend e in
+        let* scope = extend scope in
+        Llet (rf, id, e, scope) |> return
+      | Lswitch -> internalfail "TODO"
+    in
+    extend body
+  | _ -> return lambda
+;;
 
 let rec lexpr e =
   let is_global_var v =
@@ -146,7 +156,9 @@ let rec lexpr e =
   | Expr_fun (p, e) ->
     let* _ = CC_state.index get p in
     let* body, total_args = extract_fun_body (return e) in
-    lclosure body total_args |> return
+    let open Freevars in
+    let free_vars = diff (collect_expr e) (collect_pat p) in
+    lclosure body total_args free_vars |> return
   | Expr_var x ->
     let* is_global = is_global_var x in
     if is_global
@@ -196,11 +208,11 @@ let rec lexpr e =
     (match lf with
      | Lvar id ->
        (match List.find_opt (fun (x, _) -> x = id) global_env with
-        | Some (_, Fun f) -> apply_arg f la |> return
+        | Some (_, Fun f) -> apply_arg f la |> return |> extend_inner_fun_env
         | Some _ ->
           unreachable () (* program is type checked, can not apply arg to non function *)
         | None -> internalfail @@ Format.sprintf "Unknown identifier %s" id)
-     | Lclosure f -> apply_arg f la |> return
+     | Lclosure f -> apply_arg f la |> return |> extend_inner_fun_env
      | f -> internalfail @@ Format.asprintf "Expected function, got %a" pp_lambda f)
   | Expr_constrained (e, _) -> lexpr e
   | Expr_cons (x, xs) ->
@@ -214,7 +226,7 @@ let rec lexpr e =
     Lite (c', t', e') |> return
   | Expr_let (_, (Pat_var id, (Expr_fun _ as f)), scope) ->
     (* lift anonmous fun to top level *)
-    (* TODO: maybe need second pass on Llet for dumb constructions like let a, f = 5, (fun x -> x) *)
+    (* TODO: maybe need second pass on Llet for dumb constructions like let a, f = 5, (fun x -> x) in... *)
     let* f' = lexpr f in
     let* ({ global_env; _ } as state) = get in
     (match f' with
@@ -257,6 +269,9 @@ and split_let pat expr k =
   | Pat_var id, expr ->
     let* expr = lexpr expr in
     let* scope = k (lvar id) in
+    let* ({ local_env; _ } as state) = get in
+    let local_env = Base.Map.set local_env ~key:id ~data:expr in
+    let* _ = put { state with local_env } in
     llet id expr scope |> return
   | Pat_cons (px, pxs), Expr_cons (ex, exs) ->
     split_let px ex (fun _ -> split_let pxs exs k)
@@ -290,4 +305,16 @@ and split_list ps es k =
   helper [] ps es
 ;;
 
-let cc _ = unreachable ()
+let cc structure =
+  let init = CC_state.empty_ctx in
+  let item_to_lambda acc_state (Str_value (_, bindings)) =
+    let split_binding acc_state (p, e) =
+      let l = split_let p e return in
+      let state, _ = run l acc_state in
+      { state with indexer = 0; names_indexer = 0 }
+    in
+    List.fold_left split_binding acc_state bindings
+  in
+  let { global_env; _ } : CC_state.ctx = List.fold_left item_to_lambda init structure in
+  global_env
+;;
