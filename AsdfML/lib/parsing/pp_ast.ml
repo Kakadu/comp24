2b1733bf83c8ca18c2c1cb6986aba9a9a4676944
ArtemKhel <ArtemKhel@gmail.com>
2025-01-22 22:04:26 +0300

    fix parser

--- AsdfML/lib/parsing/pp_ast.ml
+++ AsdfML/lib/parsing/pp_ast.ml
@@ -19,7 +19,12 @@ and pp_type_ann fmt = function
   | TAInt -> fprintf fmt "int"
   | TABool -> fprintf fmt "bool"
   | TAUnit -> fprintf fmt "()"
-  | TAFun (a, b) -> fprintf fmt "%a -> %a" pp_type_ann a pp_type_ann b
+  | TAFun (e1, e2) ->
+    let rec pp_rest fmt = function
+      | TAFun (e1, e2) -> fprintf fmt "%a -> %a" pp_type_ann e1 pp_rest e2
+      | e -> fprintf fmt "%a)" pp_type_ann e
+    in
+    fprintf fmt "(%a -> %a" pp_type_ann e1 pp_rest e2
   | TATuple xs -> pp_list ~sep:" * " fmt pp_type_ann xs
   | TAList x -> fprintf fmt "%a list" pp_type_ann x
 
@@ -34,18 +39,20 @@ and pp_pattern fmt = function
 
 and pp_pattern_list fmt p = pp_list ~op:"" ~cl:"" ~sep:" " fmt pp_pattern p
 
-and pp_expr fmt = function
+and pp_expr fmt =
+  let parenthesize fmt = function
+    | (EIfElse _ | EMatch _ | ELetIn _) as e -> fprintf fmt "(%a)" pp_expr e
+    | e -> fprintf fmt "%a" pp_expr e
+  in
+  function
   | EConst c -> fprintf fmt "%a" pp_constant c
   | EVar v -> fprintf fmt "%s" v
   | EApp (e1, e2) ->
-    (match e1 with
-     | EApp (_, _) ->
-       let rec pp_rest fmt = function
-         | EApp (e1, e2) -> fprintf fmt "%a %a" pp_rest e1 pp_expr e2
-         | e -> fprintf fmt "(%a" pp_expr e
-       in
-       fprintf fmt "%a %a)" pp_rest e1 pp_expr e2
-     | _ -> fprintf fmt "(%a %a)" pp_expr e1 pp_expr e2)
+    let rec pp_rest fmt = function
+      | EApp (e1, e2) -> fprintf fmt "%a %a" pp_rest e1 parenthesize e2
+      | e -> fprintf fmt "(%a" parenthesize e
+    in
+    fprintf fmt "%a %a)" pp_rest e1 parenthesize e2
   | EIfElse (c, t, e) -> fprintf fmt "if %a then %a else %a" pp_expr c pp_expr t pp_expr e
   | EFun (p, e) ->
     fprintf fmt "(fun ";
@@ -66,3 +73,5 @@ and pp_definition fmt = function
   | DLet (NonRec, pat, e) -> fprintf fmt "let %a = %a\n" pp_pattern pat pp_expr e
   | DLet (Rec, pat, e) -> fprintf fmt "let rec %a = %a\n" pp_pattern pat pp_expr e
 ;;
+
+let pp_program fmt p = pp_list ~op:"" ~cl:"" ~sep:"\n" fmt pp_definition p
