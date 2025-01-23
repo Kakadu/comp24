76501b54b8a66efa4f102e67dd60541c5daea759
Roman Pozharskiy <rnpozharskiy@gmail.com>
2025-01-23 05:03:03 +0300

    codegen flambda

--- KreML/lib/runtime/Runtime.ml
+++ KreML/lib/runtime/Runtime.ml
@@ -4,8 +4,9 @@
 
 open Ast
 
-let access_closure = "access_closure"
-let access_tuple = "access_tuple"
+
+let alloc_tuple = "alloc_tuple"
+let alloc_closure = "alloc_closure"
 let list_cons = "list_cons"
 let list_head = "list_head"
 let list_tail = "list_tail"
@@ -15,7 +16,7 @@ let runtime_error s =
   Expr_app (Expr_var msg, Expr_const Const_unit)
 ;;
 
-let runtime_funs = [ access_closure; access_tuple; list_cons; list_head; list_tail ]
+let runtime_funs = [ alloc_closure; alloc_tuple; list_cons; list_head; list_tail ]
 
 let is_runtime_fun f =
   match List.find_opt (( = ) f) runtime_funs with
