ffa8ed990f665d72ee724c5553ff73262bff019c
raf-nr <nurmuhametovrafik04@gmail.com>
2025-01-29 18:04:48 +0500

    feat(parser): implement helper functions and parsers for parsing

---
+++ arML/lib/parser/common.ml
@@ -0,0 +1,77 @@
+(** Copyright 2024-2025, raf-nr and ksenmel *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+open Angstrom
+
+(* Helper functions *)
+
+let is_whitespace = function
+  | ' ' | '\n' -> true
+  | _ -> false
+;;
+
+let is_upper_char = function
+  | 'A' .. 'Z' -> true
+  | _ -> false
+;;
+
+let is_lower_char = function
+  | 'a' .. 'z' -> true
+  | _ -> false
+;;
+
+let is_digit_char = function
+  | '0' .. '9' -> true
+  | _ -> false
+;;
+
+let is_acceptable_service_char = function
+  | '_' | '\'' -> true
+  | _ -> false
+;; 
+
+let is_keyword = function
+  | "let"
+  | "if"
+  | "then"
+  | "else"
+  | "int"
+  | "string"
+  | "bool"
+  | "rec"
+  | "fun"
+  | "char"
+  | "function"
+  | "false"
+  | "true"
+  | "match"
+  | "with"
+  | "and"
+  | "in" -> true
+  | _ -> false
+;;
+
+let chainl1 e op =
+  let rec go acc = lift2 (fun f x -> f acc x) op e >>= go <|> return acc in
+  e >>= go
+;;
+
+let rec chainr1 e op = e >>= fun a -> op >>= (fun f -> chainr1 e op >>| f a) <|> return a
+;;
+
+(* ---------------- *)
+
+(* Helper parsers *)
+
+let skip_wspace = skip_while is_whitespace
+let skip_wspace1 = take_while1 is_whitespace
+
+let parens p = skip_wspace *> char '(' *> p <* skip_wspace <* char ')'
+let brackets p = skip_wspace *> char '[' *> p <* skip_wspace <* char ']'
+
+let arrow = skip_wspace *> string "->"
+let trait = skip_wspace *> string "|"
+let tying = skip_wspace *> string "="
+
+(* ---------------- *)
