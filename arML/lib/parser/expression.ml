bfd1da8ae33f690e3a7c633ae1a596d9a1c2ab88
raf-nr <nurmuhametovrafik04@gmail.com>
2025-01-29 18:10:50 +0500

    feat(parser): implement constant expression parser

---
+++ arML/lib/parser/expression.ml
@@ -0,0 +1,14 @@
+(** Copyright 2024-2025, raf-nr and ksenmel *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+open Ast
+open Common
+
+(* Constant expression parsers *)
+
+let parse_constant_expr =
+  let* constant = parse_constant in
+  return @@ EConstant constant
+
+(* ---------------- *)
