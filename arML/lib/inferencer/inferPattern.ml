4f772effca0f67be7ebc9000dc8dc3242faf52ce
raf-nr <nurmuhametovrafik04@gmail.com>
2025-01-30 19:45:27 +0500

    feat(type): add type inference for wildcard, il and tuple patterns

--- arML/lib/inferencer/inferPattern.ml
+++ arML/lib/inferencer/inferPattern.ml
@@ -5,17 +5,37 @@
 open StateResultMonad
 open StateResultMonad.Syntax
 open CommonFunctions
-(* open TypeUtils *)
+open UniquePatternVarsChecker
 
 let infer_pattern =
-  (* let rec helper env = function *)
-  let helper env = function
+  let rec helper env = function
     | Ast.PConst c -> return (infer_const c, env)
     | Ast.PVar (Id v) ->
       let* fv = fresh_var in
       let schema = Schema.Schema (TypeTree.TypeVarSet.empty, fv) in
       let env = TypeEnv.extend env v schema in
       return (fv, env)
+    | Ast.PAny ->
+      let* fv = fresh_var in
+      return (fv, env)
+    | Ast.PNill ->
+      let* fv = fresh_var in
+      let ty = TypeTree.TList fv in
+      return (ty, env)
+    | Ast.PTuple (first_pattern, second_pattern, patterns) as tuple_p ->
+      let* _ = check_unique_vars tuple_p in
+      (* Check several bounds *)
+      let* ty, env =
+        (* Here is the list of types in reverse order. *)
+        RList.fold_left
+          (first_pattern :: second_pattern :: patterns)
+          ~init:(return ([], env))
+          ~f:(fun (acc, env) (pattern) ->
+            let* ty1, env1 = helper env pattern in
+            return (ty1 :: acc, env1))
+      in
+      let ty = TypeTree.TTuple (List.rev ty) in
+      return (ty, env)
     | _ ->
       (* !!! *)
       let* fv = fresh_var in
