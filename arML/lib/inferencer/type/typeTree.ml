15aceb5a53974d6d950a9a235a4c663d798909bc
raf-nr <nurmuhametovrafik04@gmail.com>
2025-01-30 18:48:01 +0500

    feat(type): add type tree implementation

---
+++ arML/lib/inferencer/type/typeTree.ml
@@ -0,0 +1,30 @@
+(** Copyright 2024-2025, raf-nr and ksenmel *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+type type_var = int
+
+module TypeVar = struct
+  type t = type_var
+  let compare = Int.compare
+end
+
+type ground_type =
+  | GTInt
+  | GTBool
+  | GTUnit
+  | GTChar
+  | GTString
+[@@deriving show { with_path = false }]
+
+type typ =
+  | TGround of ground_type (* int, bool, unit, char, string *)
+  | TVar of type_var (* 'a, 'b, etc. *)
+  | TArr of typ * typ (* 'a -> 'b *)
+  | TTuple of typ list (* 'a * int * char *)
+  | TList of typ (* 'a list *)
+(* [@@deriving show { with_path = false }] *)
+
+module TypeVarSet = Stdlib.Set.Make (TypeVar) (* Set, that storing type variables. *)
+
+module VarSet = Stdlib.Set.Make (String) (* Set, that storing variables (let-binds and effect declarations). *)
