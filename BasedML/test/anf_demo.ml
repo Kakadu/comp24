d740d63ef2fb7f3d1f29c392d45f3e77822fee7c
Pavel Averin <pasha2281337alfa@gmail.com>
2025-01-31 03:51:54 +0900

    refacotr: format code

--- BasedML/test/anf_demo.ml
+++ BasedML/test/anf_demo.ml
@@ -6,10 +6,15 @@ let () =
   let s = Stdio.In_channel.input_all Stdlib.stdin in
   match Parser.parse_program s with
   | Ok ast ->
-    let final = ast |> Middleend.Closure_conversion.convert_ast |> Middleend.Lambda_lifting.lift_ast |> Middleend.Anf.transform in
+    let final =
+      ast
+      |> Middleend.Closure_conversion.convert_ast
+      |> Middleend.Lambda_lifting.lift_ast
+      |> Middleend.Anf.transform
+    in
     List.iter
       (fun binding ->
          Stdlib.Format.printf "%a\n" Middleend.Restore_anf_ast.restore_anf_decl binding)
-         final
+      final
   | Error message -> Format.printf "Error: %s\n" message
-;; 
+;;
--- BasedML/test/lifting_demo.ml
+++ BasedML/test/lifting_demo.ml
@@ -6,10 +6,12 @@ let () =
   let s = Stdio.In_channel.input_all Stdlib.stdin in
   match Parser.parse_program s with
   | Ok ast ->
-    let lifted = ast |> Middleend.Closure_conversion.convert_ast |> Middleend.Lambda_lifting.lift_ast  in
+    let lifted =
+      ast |> Middleend.Closure_conversion.convert_ast |> Middleend.Lambda_lifting.lift_ast
+    in
     List.iter
       (fun binding ->
          Stdlib.Format.printf "%a\n" Middleend.Restore_llast.pp_lllet_declaration binding)
       lifted
   | Error message -> Format.printf "Error: %s\n" message
-;; 
+;;
