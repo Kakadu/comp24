1d3b8dda82126229ea0a16d25a8916cd9ac658c9
raf-nr <nurmuhametovrafik04@gmail.com>
2025-01-30 19:14:44 +0500

    feat(type): implement type inference for const and var expression

---
+++ arML/lib/inferencer/inferExpression.ml
@@ -0,0 +1,22 @@
+(** Copyright 2024-2025, raf-nr and ksenmel *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+open TypeTree
+open StateResultMonad
+open StateResultMonad.Syntax
+open TypeErrors
+open InferPattern
+open TypeUtils
+open CommonFunctions
+
+let infer_expr =
+
+  let rec helper env = function
+  | Ast.EConstant c -> return @@ (Substitution.empty, infer_const c)
+  | Ast.EIdentifier (Id name) -> infer_id env name
+  | _ -> fail Occurs_check (* !!! *)
+  in
+
+  helper
+;;
