edcf93aa0493c629e0bbd5647c2ccc087b89ead1
raf-nr <nurmuhametovrafik04@gmail.com>
2025-01-30 19:18:38 +0500

    feat(type): add type inference runner

---
+++ arML/lib/inferencer/runner.ml
@@ -0,0 +1,12 @@
+(** Copyright 2024-2025, raf-nr and ksenmel *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+open StateResultMonad
+open TypeUtils
+open InferExpression
+open InferProgram
+
+let run_expr_inferencer expr = Result.map snd (run (infer_expr TypeEnv.empty expr))
+
+let run_program_inferencer program = run (infer_program TypeEnv.empty program)
