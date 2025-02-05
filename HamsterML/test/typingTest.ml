36ee13b3ca93120415d75ccb7235d5bc47b65c4a
AlexShmak <lexnder9@gmail.com>
2025-02-05 22:53:30 +0300

    refactor existing typingTests

--- HamsterML/test/typingTest.ml
+++ HamsterML/test/typingTest.ml
@@ -7,18 +7,23 @@ let typecheck expr =
 ;;
 
 (* data types *)
-let%test _ = typecheck (Value (Const (Int 228))) = TInt
-let%test _ = typecheck (Value (Const (Float 228.337))) = TFloat
-let%test _ = typecheck (Value (Const (String "kfc boss"))) = TString
-let%test _ = typecheck (Value (Const (Bool true))) = TBool
-let%test _ = typecheck (Value (Const (Char '1'))) = TChar
+let%test _ = typecheck (Pattern (Const (Int 228))) = TInt
+let%test _ = typecheck (Pattern (Const (Float 228.337))) = TFloat
+let%test _ = typecheck (Pattern (Const (String "kfc boss"))) = TString
+let%test _ = typecheck (Pattern (Const (Bool true))) = TBool
+let%test _ = typecheck (Pattern (Const (Char '1'))) = TChar
 
 (* binary operations *)
-let%test _ = typecheck (BinOp (ADD, Value (Const (Int 1)), Value (Const (Int 1)))) = TInt
+let%test _ =
+  typecheck (BinOp (ADD, Pattern (Const (Int 1)), Pattern (Const (Int 1)))) = TInt
+;;
 
 let%test _ =
-  typecheck (BinOp (CONCAT, Value (Const (String "nike")), Value (Const (String "pro"))))
+  typecheck
+    (BinOp (CONCAT, Pattern (Const (String "nike")), Pattern (Const (String "pro"))))
   = TString
 ;;
 
-let%test _ = typecheck (BinOp (EQ, Value (Const (Int 1)), Value (Const (Int 20)))) = TBool
+let%test _ =
+  typecheck (BinOp (EQ, Pattern (Const (Int 1)), Pattern (Const (Int 20)))) = TBool
+;;
