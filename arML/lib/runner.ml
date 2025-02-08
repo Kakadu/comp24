357a3ef80039cb3b1e6e16962446cdb273f3847c
raf-nr <nurmuhametovrafik04@gmail.com>
2025-02-08 21:31:10 +0500

    feat: add arML inferencer runner

--- arML/lib/runner.ml
+++ arML/lib/runner.ml
@@ -13,3 +13,27 @@ let parse_program_with_print program =
 ;;
 
 (* -------------- *)
+
+(* Inferencer runner *)
+
+let inference_expr ast =
+  match Inferencer.Runner.run_expr_inferencer ast with
+  | Ok typ -> Inferencer.PpType.print_expr_type typ
+  | Error e -> Inferencer.PpTypeErrors.print_inferencer_error e
+;;
+
+let inference_program ast =
+  match Inferencer.Runner.run_program_inferencer ast with
+  | Ok (env, names_list) -> Inferencer.PpType.print_program_type env names_list
+  | Error e -> Inferencer.PpTypeErrors.print_inferencer_error e
+;;
+
+let inference program =
+  let ast = parse_program program in
+  match ast with
+  | Ok [SExpression expr] -> inference_expr expr
+  | Ok ast -> inference_program ast
+  | Error _ -> Parser.Pprint.print_parser_error Parser.Error.Syntax_error
+;;
+
+(* -------------- *)
