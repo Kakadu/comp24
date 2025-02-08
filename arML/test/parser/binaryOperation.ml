d682cce0711ee2864163bef8de3c963a84c1b0d3
ksenmel <ksemelashenko@gmail.com>
2025-02-08 18:47:28 +0300

    feat(parser): add binary operations tests

--- arML/test/parser/binaryOperation.ml
+++ arML/test/parser/binaryOperation.ml
@@ -29,30 +29,100 @@ let%expect_test _ =
 
 let%expect_test _ =
   parse_with_print {| 1 + 2 / 3 |};
-  [%expect]
+  [%expect{|
+    [(SExpression
+        (EApplication ((EIdentifier (Id "( + )")),
+           [(EConstant (CInt 1));
+             (EApplication ((EIdentifier (Id "( / )")),
+                [(EConstant (CInt 2)); (EConstant (CInt 3))]))
+             ]
+           )))
+      ] |}]
 ;;
 
 let%expect_test _ =
   parse_with_print {| (1 + 2) * (3 + 4) / (1 + (2 * 3)) |};
-  [%expect]
+  [%expect{|
+    [(SExpression
+        (EApplication ((EIdentifier (Id "( / )")),
+           [(EApplication ((EIdentifier (Id "( * )")),
+               [(EApplication ((EIdentifier (Id "( + )")),
+                   [(EConstant (CInt 1)); (EConstant (CInt 2))]));
+                 (EApplication ((EIdentifier (Id "( + )")),
+                    [(EConstant (CInt 3)); (EConstant (CInt 4))]))
+                 ]
+               ));
+             (EApplication ((EIdentifier (Id "( + )")),
+                [(EConstant (CInt 1));
+                  (EApplication ((EIdentifier (Id "( * )")),
+                     [(EConstant (CInt 2)); (EConstant (CInt 3))]))
+                  ]
+                ))
+             ]
+           )))
+      ] |}]
 ;;
 
 let%expect_test _ =
   parse_with_print {| 1 < 2 && 2 > 1 |};
-  [%expect]
+  [%expect{|
+    [(SExpression
+        (EApplication ((EIdentifier (Id "( && )")),
+           [(EApplication ((EIdentifier (Id "( < )")),
+               [(EConstant (CInt 1)); (EConstant (CInt 2))]));
+             (EApplication ((EIdentifier (Id "( > )")),
+                [(EConstant (CInt 2)); (EConstant (CInt 1))]))
+             ]
+           )))
+      ] |}]
 ;;
 
 let%expect_test _ =
   parse_with_print {| 1 <= 2 || 3 >= 2 |};
-  [%expect]
+  [%expect{|
+    [(SExpression
+        (EApplication ((EIdentifier (Id "( || )")),
+           [(EApplication ((EIdentifier (Id "( <= )")),
+               [(EConstant (CInt 1)); (EConstant (CInt 2))]));
+             (EApplication ((EIdentifier (Id "( >= )")),
+                [(EConstant (CInt 3)); (EConstant (CInt 2))]))
+             ]
+           )))
+      ] |}]
 ;;
 
 let%expect_test _ =
   parse_with_print {| 1 <> 2 && 3 != 4 || 1 =|};
-  [%expect]
+  [%expect{| Syntax error. |}]
 ;;
 
 let%expect_test _ =
   parse_with_print {| (1 * 2 + 3 / 3) >= (3 / 3 / 3) || (1 != 9 / 3) |};
-  [%expect]
+  [%expect{|
+    [(SExpression
+        (EApplication ((EIdentifier (Id "( || )")),
+           [(EApplication ((EIdentifier (Id "( >= )")),
+               [(EApplication ((EIdentifier (Id "( + )")),
+                   [(EApplication ((EIdentifier (Id "( * )")),
+                       [(EConstant (CInt 1)); (EConstant (CInt 2))]));
+                     (EApplication ((EIdentifier (Id "( / )")),
+                        [(EConstant (CInt 3)); (EConstant (CInt 3))]))
+                     ]
+                   ));
+                 (EApplication ((EIdentifier (Id "( / )")),
+                    [(EApplication ((EIdentifier (Id "( / )")),
+                        [(EConstant (CInt 3)); (EConstant (CInt 3))]));
+                      (EConstant (CInt 3))]
+                    ))
+                 ]
+               ));
+             (EApplication ((EIdentifier (Id "( != )")),
+                [(EConstant (CInt 1));
+                  (EApplication ((EIdentifier (Id "( / )")),
+                     [(EConstant (CInt 9)); (EConstant (CInt 3))]))
+                  ]
+                ))
+             ]
+           )))
+      ] |}]
 ;;
