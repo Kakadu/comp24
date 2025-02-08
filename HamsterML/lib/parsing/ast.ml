ef3f44c62a349915a86d9e6637f54ca887192eb9
AlexShmak <lexnder9@gmail.com>
2025-02-08 17:59:44 +0300

    small refactor on ast.ml

--- HamsterML/lib/parsing/ast.ml
+++ HamsterML/lib/parsing/ast.ml
@@ -1,6 +1,6 @@
 type id = string [@@deriving show]
 
-(* default types *)
+(** default types *)
 type dataType =
   | Int of int
   | Float of float
@@ -10,7 +10,7 @@ type dataType =
   | Unit
 [@@deriving show]
 
-(* let f (a: Int) (b: Int) = ...*)
+(** let f (a: Int) (b: Int) = ...*)
 type paramType =
   | PInt
   | PFloat
@@ -20,25 +20,25 @@ type paramType =
 [@@deriving show]
 
 type bop =
-  | ADD (** + *)
-  | SUB (** - *)
-  | MUL (** * *)
-  | DIV (** / *)
-  | EQ (** = *)
-  | ID_EQ
-  | NEQ (** != *)
-  | GT (** > *)
-  | GTE (** >= *)
-  | LT (** < *)
-  | LTE (** <= *)
-  | AND (** && *)
-  | OR (** || *)
-  | CONCAT (** ^ *)
+  | ADD (* + *)
+  | SUB (* - *)
+  | MUL (* * *)
+  | DIV (* / *)
+  | EQ (* = *)
+  | ID_EQ (* == *)
+  | NEQ (* != *)
+  | GT (* > *)
+  | GTE (* >= *)
+  | LT (* < *)
+  | LTE (* <= *)
+  | AND (* && *)
+  | OR (* || *)
+  | CONCAT (* ^ *)
 [@@deriving show]
 
 type uop = NOT (** not true *) [@@deriving show]
 
-(* Pattern is a value (dataType -> dataStructure) *)
+(** pattern is a value (dataType -> dataStructure) *)
 type pattern =
   | Const of dataType
   | VarId of id
@@ -48,14 +48,14 @@ type pattern =
   | List of expr list (* [1; 2; 3] *)
   | ListConcat of expr * pattern (* a :: [b;c]*)
 [@@deriving show]
-
+  
 and expr =
   | BinOp of bop * expr * expr
   | UnOp of uop * expr
   | Application of expr * expr
   | Pattern of pattern
-  (* let id a = a | the first `pattern` is the pattern that can be used instead of function's name *)
   | Let of funType * pattern * pattern list * expr
+    (* let id a = a | the first `pattern` is the pattern that can be used instead of function's name *)
   | LetAndIn of expr list * expr option (* let a = 1 and b = 2 in a + b *)
   | Fun of pattern list * expr (* (fun a -> a + 1) *)
   | If of expr * expr * expr option (* if a = b then c (else d) *)
@@ -67,5 +67,4 @@ and funType =
   | Nonrecursive
 [@@deriving show]
 
-type prog = expr list
-[@@deriving show]
+type prog = expr list [@@deriving show]
