b84c44eeeb58bbb546fcae7ab17f1f42b33c1e3c
SurfaceYellowDuck <lukonenko.nikitka@mail.ru>
2025-02-11 20:27:50 +0300

    add: parsing of type annotations for expressions

--- ShaitanML/lib/parser.mli
+++
@@ -1,7 +0,0 @@
-(** Copyright 2023-2024, Nikita Lukonenko and Nikita Nemakin *)
-
-(** SPDX-License-Identifier: LGPL-3.0-or-later *)
-
-val parse : string -> (Ast.structure, string) result
-val test_parse : string -> unit
-val is_keyword : string -> bool
