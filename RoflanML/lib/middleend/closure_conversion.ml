b19cecaeed608f6710d9e43a70f2ca8c7a981fab
Ilya Syresenkov <ilya.syresenkov.04@gmail.com>
2025-02-12 18:11:26 +0300

    fix CC and add some tests

--- RoflanML/lib/middleend/closure_conversion.ml
+++ RoflanML/lib/middleend/closure_conversion.ml
@@ -5,7 +5,7 @@
 open Ast
 open Base
 
-let find_free_vars global_env =
+let find_free_vars =
   let rec find_free_vars_pattern pat =
     match pat with
     | PWild | PEmpty | PConst _ -> Set.empty (module String)
@@ -23,33 +23,46 @@ let find_free_vars global_env =
       (* According to OCaml rules, all patterns in POr must have same variables set *)
     | POr (p1, _, _) -> find_free_vars_pattern p1
   in
-  let rec find_free_vars_case env (pat, e) =
+  let rec find_free_vars_case env free_vars (pat, e) =
     let env = Set.union env (find_free_vars_pattern pat) in
-    helper env e
-  and helper env = function
+    helper env free_vars e
+  and helper env free_vars = function
     | EConst _ -> Set.empty (module String)
     | EVar x ->
-      if Set.mem env x then Set.empty (module String) else Set.singleton (module String) x
+      let fvs1 =
+        match Map.find free_vars x with
+        | Some lst -> Set.of_list (module String) lst
+        | None -> Set.empty (module String)
+      in
+      let fvs2 =
+        if Set.mem env x
+        then Set.empty (module String)
+        else Set.singleton (module String) x
+      in
+      Set.union fvs1 fvs2
     | ETuple (e1, e2, es) ->
       Set.union_list
         (module String)
-        [ helper env e1
-        ; helper env e2
+        [ helper env free_vars e1
+        ; helper env free_vars e2
         ; List.fold
             es
             ~init:(Set.empty (module String))
-            ~f:(fun acc e -> Set.union acc (helper env e))
+            ~f:(fun acc e -> Set.union acc (helper env free_vars e))
         ]
-    | EList es -> Set.union_list (module String) (List.map es ~f:(fun e -> helper env e))
+    | EList es ->
+      Set.union_list (module String) (List.map es ~f:(fun e -> helper env free_vars e))
     | EBranch (cond, t, f) ->
-      Set.union_list (module String) [ helper env cond; helper env t; helper env f ]
+      Set.union_list
+        (module String)
+        [ helper env free_vars cond; helper env free_vars t; helper env free_vars f ]
     | EMatch (e, cases) ->
-      let vs1 = helper env e in
+      let vs1 = helper env free_vars e in
       let vs2 =
         List.fold
           cases
           ~init:(Set.empty (module String))
-          ~f:(fun acc case -> Set.union acc (find_free_vars_case env case))
+          ~f:(fun acc case -> Set.union acc (find_free_vars_case env free_vars case))
       in
       Set.union vs1 vs2
     | ELet (is_rec, id, e1, e2) ->
@@ -58,20 +71,20 @@ let find_free_vars global_env =
         | Rec -> Set.add env id
         | NonRec -> env
       in
-      let vs1 = helper env e1 in
+      let vs1 = helper env free_vars e1 in
       let env = Set.add env id in
       let vs2 =
         match e2 with
-        | Some e2 -> helper env e2
+        | Some e2 -> helper env free_vars e2
         | None -> Set.empty (module String)
       in
       Set.union vs1 vs2
     | EFun ((id, _), e) ->
       let env = Set.add env id in
-      helper env e
-    | EApp (e1, e2) -> Set.union (helper env e1) (helper env e2)
+      helper env free_vars e
+    | EApp (e1, e2) -> Set.union (helper env free_vars e1) (helper env free_vars e2)
   in
-  helper global_env
+  helper
 ;;
 
 let closure_app e args =
@@ -114,7 +127,7 @@ let close_expr e env =
       let cases = List.map cases ~f:(fun (pat, e) -> pat, helper e free_vars env) in
       EMatch (e, cases)
     | ELet (is_rec, id, (EFun (_, _) as e1), e2) ->
-      let free = Set.to_list (find_free_vars env e) in
+      let free = Set.to_list (find_free_vars env free_vars e1) in
       let free_vars = Map.update free_vars id ~f:(fun _ -> free) in
       let args, e = uncurry e1 in
       let e = helper e free_vars env in
@@ -135,7 +148,7 @@ let close_expr e env =
       in
       ELet (is_rec, id, e1, e2)
     | EFun (_, _) as f ->
-      let free = Set.to_list (find_free_vars env f) in
+      let free = Set.to_list (find_free_vars env free_vars f) in
       let args, e = uncurry f in
       let e = helper e free_vars env in
       let f = curry args e in
