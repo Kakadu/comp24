c39850d937f5576775d3d4de0af79294c5330b48
Nikita Nemakin <nikita.nemakin@yandex.ru>
2025-02-16 16:06:14 +0300

    some parser&infer fixes

--- ShaitanML/lib/typedtree.ml
+++ ShaitanML/lib/typedtree.ml
@@ -1,4 +1,4 @@
-(** Copyright 2023-2024, Nikita Lukonenko and Nikita Nemakin *)
+(** Copyright 2024-2025, Nikita Lukonenko and Nikita Nemakin *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
