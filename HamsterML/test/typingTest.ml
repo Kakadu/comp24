fce2c9bfabd557e38116e3aff6776f40857c6b29
d-zaytsev <dzaytsev308@gmail.com>
2025-02-16 12:38:12 +0300

    Add let tests

--- HamsterML/test/typingTest.ml
+++ HamsterML/test/typingTest.ml
@@ -150,60 +150,18 @@ let%test _ =
   = TArrow (TArrow (TInt, TBool), TArrow (TList TInt, TList TBool))
 ;;
 
-(* let%test _ = typecheck_expr (parse_expr "let x = true") = TBool
-let%test _ = typecheck_expr (parse_expr "let (a, b) = (1, 2)") = TTuple [ TInt; TInt ]
+(* Let *)
+
+let%test _ = infer_expr "let x = true" = TBool
+let%test _ = infer_expr "let (a, b) = (1, 2)" = TTuple [ TInt; TInt ]
+let%test _ = infer_expr "let a, b = 1, 2" = TTuple [ TInt; TInt ]
+let%test _ = infer_expr "let f x y = x + y" = TArrow (TInt, TArrow (TInt, TInt))
+let%test _ = infer_expr "let f = fun x y -> x + y" = infer_expr "let f x y = x + y"
 
 let%test _ =
-  typecheck_expr (parse_expr "let f x y = x + y") = TArrow (TInt, TArrow (TInt, TInt))
-;;
-
-let%test _ =
-  typecheck_expr (parse_expr "let f = fun x y -> x + y")
-  = typecheck_expr (parse_expr "let f x y = x + y")
-;;
-
-let%test _ =
-  typecheck_expr (parse_expr "let f x y = if x = y && x = 10 then 1 else 2")
+  infer_expr "let f x y = if x = y && x = 10 then 1 else 2"
   = TArrow (TInt, TArrow (TInt, TInt))
 ;;
 
-let%test _ =
-  typecheck_expr (parse_expr "let f x = if x = 10 then true else false")
-  = TArrow (TInt, TBool)
-;;
-
-let%test _ =
-  typecheck_expr (parse_expr "let f x = if x = 10 then true else false")
-  = TArrow (TInt, TBool)
-;;
-
-let%test _ = typecheck_expr (parse_expr "let x () () = 10")
- = TArrow (TUnit, TArrow (TUnit, TInt))
-
-(* --- Data types *)
-let%test _ = typecheck_expr (Pattern (Const (Int 228))) = TInt
-let%test _ = typecheck_expr (Pattern (Const (Float 228.337))) = TFloat
-let%test _ = typecheck_expr (Pattern (Const (String "kfc boss"))) = TString
-let%test _ = typecheck_expr (Pattern (Const (Bool true))) = TBool
-let%test _ = typecheck_expr (Pattern (Const (Char '1'))) = TChar
-
-(* --- Binary operations *)
-let%test _ =
-  typecheck_expr (BinOp (ADD, Pattern (Const (Int 1)), Pattern (Const (Int 1)))) = TInt
-;;
-
-let%test _ =
-  typecheck_expr
-    (BinOp (CONCAT, Pattern (Const (String "nike")), Pattern (Const (String "pro"))))
-  = TString
-;;
-
-let%test _ =
-  typecheck_expr (BinOp (EQ, Pattern (Const (Int 1)), Pattern (Const (Int 20)))) = TBool
-;;
-
-(* fun [10; 10] = () => int list -> unit *)
-let%test _ =
-  typecheck_expr (Fun ([ List [ Const (Int 10); Const (Int 20) ] ], Pattern (Const Unit)))
-  = TArrow (TList TInt, TUnit)
-;; *)
+let%test _ = infer_expr "let x () () = 10" = TArrow (TUnit, TArrow (TUnit, TInt))
+let%test _ = infer_expr "let x () = ()" = TArrow (TUnit, TUnit)
