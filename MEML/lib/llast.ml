97f3949bd7a8031184d7930aa3ce9ecd3d67234b
Urtix <es.perevalov@mail.ru>
2025-02-17 02:26:06 +0300

    feat: Add lambda lifting
    Signed-off-by: Urtix <es.perevalov@mail.ru>

---
+++ MEML/lib/llast.ml
@@ -0,0 +1,20 @@
+open Ast
+
+type llexpression =
+  | LLConst of const
+  | LLVar of name
+  | LLIfElse of llexpression * llexpression * llexpression
+  | LLEbinOp of binary_op * llexpression * llexpression
+  | LLApp of llexpression * llexpression
+  | LLLetIn of rec_flag * name list * llexpression * llexpression
+  | LLTuple of llexpression list
+  | LLMatch of llexpression * (pattern * llexpression) list
+  | LLList of llexpression * llexpression
+[@@deriving show { with_path = false }]
+
+type llbindings =
+  | LLLet of rec_flag * name list * pattern list * llexpression
+  | LLExpression of llexpression
+[@@deriving show { with_path = false }]
+
+type llstatements = llbindings list [@@deriving show { with_path = false }]
