e8d2a0c412c27d5d43eb978f7acb97a85126e5d4
AlexShmak <lexnder9@gmail.com>
2025-02-18 22:16:30 +0300

    add: search free variables in let expressions

--- HamsterML/lib/middleend/closure_conversion.ml
+++ HamsterML/lib/middleend/closure_conversion.ml
@@ -1,103 +1,103 @@
-(* closure conversion *)
-
 open Base
 open Ast
 
-(* get free variables from an expression *)
-let unbound_variables exp =
-  let rec exclude_bind_pattern pat base_set =
-    let rec exclude_bind_pattern_list acc = function
-      | hd :: tl ->
-        (match hd with
-         | Wildcard | Operation _ -> exclude_bind_pattern_list acc tl
-         | ListConcat (pat1, pat2) ->
-           let new_acc = exclude_bind_pattern_list acc (pat1 :: [ pat2 ]) in
-           exclude_bind_pattern_list new_acc tl
-         | Var id -> exclude_bind_pattern_list (Set.remove acc id) tl
-         | Tuple pat_list | List pat_list ->
-           let new_acc = exclude_bind_pattern_list acc pat_list in
-           exclude_bind_pattern_list new_acc tl
-         | Const _ -> exclude_bind_pattern_list acc tl
-         | Constraint (pat, _) ->
-           let new_acc = exclude_bind_pattern pat base_set in
-           exclude_bind_pattern_list new_acc tl)
-      | [] -> acc
-    in
-    match pat with
-    | Wildcard | Operation _ | Const _ -> base_set
-    | ListConcat (pat1, pat2) -> exclude_bind_pattern_list base_set (pat1 :: [ pat2 ])
-    | Var id -> Set.remove base_set id
-    | Tuple pat_list | List pat_list -> exclude_bind_pattern_list base_set pat_list
-    | Constraint (pat, _) -> exclude_bind_pattern_list base_set [ pat ]
-  in
-  let rec helper_expr exp =
-    let rec helper_pattern pat =
-      match pat with
-      | Const _ | Operation _ | Wildcard -> Set.empty (module String)
-      | Var id -> Set.add (Set.empty (module String)) id
-      | Tuple pats | List pats ->
-        List.fold
-          pats
-          ~init:(Set.empty (module String))
-          ~f:(fun acc h -> Set.union acc (helper_pattern h))
-      | ListConcat (pat1, pat2) -> Set.union (helper_pattern pat1) (helper_pattern pat2)
-      | Constraint (pat, _) -> helper_pattern pat
-    in
-    match exp with
-    | EConst _ | EOperation _ -> Set.empty (module String)
-    | EVar id -> Set.add (Set.empty (module String)) id
-    | ETuple exps | EList exps ->
-      List.fold
-        exps
-        ~init:(Set.empty (module String))
-        ~f:(fun acc e -> Set.union acc (helper_expr e))
-    | EListConcat (expr1, expr2) | Application (expr1, expr2) ->
-      Set.union (helper_expr expr1) (helper_expr expr2)
-    | If (e1, e2, e3) ->
-      let if_then = Set.union (helper_expr e1) (helper_expr e2) in
-      (match e3 with
-       | Some e -> Set.union if_then (helper_expr e)
-       | None -> if_then)
-    | Match (e, cases) ->
-      let unbound_in_cases =
-        List.fold
-          cases
-          ~init:(Set.empty (module String))
-          ~f:(fun acc (pat, expr) ->
-            Set.union acc (exclude_bind_pattern pat (helper_expr expr)))
-      in
-      Set.union unbound_in_cases (helper_expr e)
-    | Let (_, binds, in_expr) ->
-      let binds_set =
-        List.fold
-          binds
-          ~init:(Set.empty (module String))
-          ~f:(fun acc (pat, arg_list, exp) ->
-            let pattern_set = helper_pattern pat in
-            let arg_list_set =
-              List.fold
-                arg_list
-                ~init:(Set.empty (module String))
-                ~f:(fun acc2 h -> Set.union acc2 (helper_pattern h))
-            in
-            let exp_set = helper_expr exp in
-            let bind_item = Set.union pattern_set (Set.union arg_list_set exp_set) in
-            Set.union acc bind_item)
-      in
-      let in_expr_set =
-        match in_expr with
-        | Some e -> helper_expr e
-        | None -> Set.empty (module String)
-      in
-      Set.union binds_set in_expr_set
-    | EConstraint (expr, _) -> helper_expr expr
-    | Fun (args, expr) ->
-      let free_vars = helper_expr expr in
-      List.fold args ~init:free_vars ~f:(fun acc arg -> exclude_bind_pattern arg acc)
-  in
-  helper_expr exp
+(* Remove all the variables that are bound by a single pattern from free variables set *)
+let rec remove_bound_vars_from_pattern free (pat : pattern) =
+  match pat with
+  | Wildcard | Operation _ | Const _ -> free
+  | Var id -> Set.remove free id
+  | ListConcat (p1, p2) -> remove_bound_vars_from_patterns free [ p1; p2 ]
+  | Tuple pats | List pats -> remove_bound_vars_from_patterns free pats
+  | Constraint (p, _) -> remove_bound_vars_from_pattern free p
+
+and remove_bound_vars_from_patterns free (pats : pattern list) =
+  List.fold ~init:free pats ~f:(fun acc pat -> remove_bound_vars_from_pattern acc pat)
 ;;
 
-(* let close_function = failwith "not yet implemented"
-let convert = failwith "not yet implemented"
-let convert_ast = failwith "not yet implemented" *)
+(* Collect the bound variables that a pattern binds, e.g. `match x with ...` *)
+let rec bound_vars_in_pattern (pat : pattern) =
+  match pat with
+  | Wildcard | Operation _ | Const _ -> Set.empty (module String)
+  | Var id -> Set.singleton (module String) id
+  | ListConcat (p1, p2) -> Set.union (bound_vars_in_pattern p1) (bound_vars_in_pattern p2)
+  | Tuple pats | List pats ->
+    List.fold
+      pats
+      ~init:(Set.empty (module String))
+      ~f:(fun acc p -> Set.union acc (bound_vars_in_pattern p))
+  | Constraint (p, _) -> bound_vars_in_pattern p
+;;
+
+(* Find free variables in an expression *)
+let rec free_vars_expr (exp : expr) =
+  match exp with
+  | EConst _ | EOperation _ -> Set.empty (module String)
+  | EVar id -> Set.singleton (module String) id
+  | ETuple exps | EList exps ->
+    List.fold
+      exps
+      ~init:(Set.empty (module String))
+      ~f:(fun acc e -> Set.union acc (free_vars_expr e))
+  | EListConcat (e1, e2) | Application (e1, e2) ->
+    Set.union (free_vars_expr e1) (free_vars_expr e2)
+  | If (e1, e2, e3_opt) ->
+    let base = Set.union (free_vars_expr e1) (free_vars_expr e2) in
+    (match e3_opt with
+     | Some e -> Set.union base (free_vars_expr e)
+     | None -> base)
+  | Match (e, cases) ->
+    let free_in_cases =
+      List.fold
+        cases
+        ~init:(Set.empty (module String))
+        ~f:(fun acc (pat, body) ->
+          let body_free = free_vars_expr body in
+          let body_free = remove_bound_vars_from_pattern body_free pat in
+          Set.union acc body_free)
+    in
+    Set.union (free_vars_expr e) free_in_cases
+  | Let (fun_type, binds, in_expr_opt) ->
+    (* Determine whether the let is recursive *)
+    let is_rec =
+      match fun_type with
+      | Recursive -> true
+      | Nonrecursive -> false
+    in
+    (* The names bound by the let (from the patterns of each binding) *)
+    let bound_names =
+      List.fold
+        binds
+        ~init:(Set.empty (module String))
+        ~f:(fun acc (pat, _args, _body) -> Set.union acc (bound_vars_in_pattern pat))
+    in
+    (* Compute free variables from each binding's definition *)
+    let free_in_binds =
+      List.fold
+        binds
+        ~init:(Set.empty (module String))
+        ~f:(fun acc (pat, args, body) ->
+          let fvs = free_vars_expr body in
+          (* Remove the variables bound by the function arguments *)
+          let fvs =
+            List.fold args ~init:fvs ~f:(fun acc arg ->
+              remove_bound_vars_from_pattern acc arg)
+          in
+          (* For recursive lets, remove the binding pattern from the binding's body *)
+          let fvs = if is_rec then remove_bound_vars_from_pattern fvs pat else fvs in
+          (* In recursive lets, also remove all let-bound names *)
+          let fvs = if is_rec then Set.diff fvs bound_names else fvs in
+          Set.union acc fvs)
+    in
+    let free_in_in_expr =
+      match in_expr_opt with
+      | Some e -> Set.diff (free_vars_expr e) bound_names
+      | None -> Set.empty (module String)
+    in
+    Set.union free_in_binds free_in_in_expr
+  | EConstraint (e, _) -> free_vars_expr e
+  | Fun (args, body) ->
+    let free = free_vars_expr body in
+    List.fold args ~init:free ~f:(fun acc arg -> remove_bound_vars_from_pattern acc arg)
+;;
+
+let unbound_variables (exp : expr) = free_vars_expr exp
