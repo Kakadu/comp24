4c8a9534a46d6c09f89d1ad00148d36b973c12ee
Osogi <dragonmail135159@gmail.com>
2025-02-21 16:10:01 +0300

    feat: add stdlib funs + integrate it to anf_types_demo

--- BasedML/lib/stdlib-funs/stdlib_funs.ml
+++ BasedML/lib/stdlib-funs/stdlib_funs.ml
@@ -11,9 +11,10 @@ type fun_type =
   | UserFun
   | SystemFun
 
-type std_fun = llvm_name * name * fun_type * type_name
+type std_fun = name * llvm_name * fun_type * type_name
 
 let bin_op arg1 arg2 ret = TFunction (arg1, TFunction (arg2, ret))
+let universal_function = TFunction (TPoly "'_a", TPoly "'_b")
 
 let stdlib_funs : std_fun list =
   [ "( + )", "plus_mlint", UserFun, bin_op TInt TInt TInt
@@ -27,12 +28,25 @@ let stdlib_funs : std_fun list =
   ; "( = )", "eq_ml", UserFun, bin_op (TPoly "'_a") (TPoly "'_a") TBool
   ; "( == )", "peq_ml", UserFun, bin_op (TPoly "'_a") (TPoly "'_a") TBool
   ; "( <> )", "neq_ml", UserFun, bin_op (TPoly "'_a") (TPoly "'_a") TBool
+  ; "( != )", "pneq_ml", UserFun, bin_op (TPoly "'_a") (TPoly "'_a") TBool
+  ; "print_int", "print_int", UserFun, TFunction (TInt, TUnit)
+  ; "( && )", "", UserFun, bin_op TBool TBool TBool
+  ; "( || )", "", UserFun, bin_op TBool TBool TBool
   ; ( "( :: )"
-    , "cons_ml"
+    , "mlrt_create_cons"
     , UserFun
     , bin_op (TPoly "'_a") (TList (TPoly "'_a")) (TList (TPoly "'_a")) )
-  ; "print_int", "print_int", UserFun, TFunction (TInt, TUnit)
-  ; "get_field", "get_box_field", SystemFun, TFunction (TPoly "'_a", TFunction (TInt, TPoly "'_b"))
-  ; "check_tag", "check_box_tag", SystemFun, TFunction (TPoly "'_a", TFunction (TInt, TBool))
+  ; ( "get_field"
+    , "mlrt_get_box_field"
+    , SystemFun
+    , TFunction (TPoly "'_a", TFunction (TInt, TPoly "'_b")) )
+  ; ( "check_tag"
+    , "mlrt_check_tag"
+    , SystemFun
+    , TFunction (TPoly "'_a", TFunction (TInt, TBool)) )
+  ; "match_error", "mltr_match_error", SystemFun, TFunction (TUnit, TPoly "_a")
+  ; "_create_tuple", "mlrt_create_tuple", SystemFun, TFunction (TInt, TPoly "'_vargs")
+  ; "_create_empty_closure", "mlrt_create_empty_closure", SystemFun, universal_function
+  ; "_apply_args_to_closure", "mlrt_apply_args_to_closure", SystemFun, universal_function
   ]
 ;;
