4e4f1a716b399bf4d4c6c14f38c4381f342ebfcb
Ilya Syresenkov <ilya.syresenkov.04@gmail.com>
2025-02-27 23:03:38 +0300

    add anf

--- RoflanML/lib/tests.ml
+++ RoflanML/lib/tests.ml
@@ -628,3 +628,116 @@ module LLTests = struct
       |}]
   ;;
 end
+
+module ANFTests = struct
+  open Closure_conversion
+  open Lambda_lifting
+  open Roflanml_stdlib
+  open Anf_ast
+  open Anf
+
+  let env = RoflanML_Stdlib.default |> Map.keys |> Set.of_list (module String)
+
+  let pp_parse_and_anf input =
+    match Parser.parse input with
+    | Result.Ok prog ->
+      (match lift_program (close_program prog env) with
+       | Result.Ok prog ->
+         (match anf prog with
+          | Result.Ok prog ->
+            List.iter prog ~f:(fun decl ->
+              Stdlib.Format.printf "%s\n" (ast_to_str (anf_to_ast decl)))
+          | Result.Error err -> Stdlib.print_endline err)
+       | Result.Error _ -> Stdlib.print_endline "Failed to LL")
+    | _ -> Stdlib.print_endline "Failed to parse"
+  ;;
+
+  let%expect_test "anf simple" =
+    pp_parse_and_anf "let q = 1 + 2 + 3 * 4 + 5";
+    [%expect
+      {|
+      let q =
+      let ANF_0 = ( + ) 1 2 in
+      let ANF_1 = ( * ) 3 4 in
+      let ANF_2 = ( + ) ANF_0 ANF_1 in
+      let ANF_3 = ( + ) ANF_2 5 in ANF_3
+      |}]
+  ;;
+
+  let%expect_test "anf tuple" =
+    pp_parse_and_anf "let q = let f x y z = x + y + z in (f 1 2 3, 4, 5)";
+    [%expect
+      {|
+      let LL_0 x y z =
+      let ANF_2 = ( + ) x y in
+      let ANF_3 = ( + ) ANF_2 z in ANF_3
+
+      let q =
+      let ANF_0 = LL_0 1 2 3 in
+      let ANF_1 = ANF_0, 4, 5 in ANF_1
+      |}]
+  ;;
+
+  let%expect_test "anf partian app" =
+    pp_parse_and_anf "let q = let f x y z = x + y + z in let g x = f 1 2 in g 3";
+    [%expect
+      {|
+      let LL_0 x y z =
+      let ANF_2 = ( + ) x y in
+      let ANF_3 = ( + ) ANF_2 z in ANF_3
+
+      let LL_1 f x =
+      let ANF_1 = LL_0 1 2 in ANF_1
+
+      let q =
+      let ANF_0 = LL_1 LL_0 3 in ANF_0
+      |}]
+  ;;
+
+  let%expect_test "anf branch" =
+    pp_parse_and_anf
+      "let q = if true && false then let f x = x + 1 in f 1 else let g x = x - 1 in g 1";
+    [%expect
+      {|
+      let LL_0 x =
+      let ANF_5 = ( + ) x 1 in ANF_5
+
+      let LL_1 x =
+      let ANF_4 = ( - ) x 1 in ANF_4
+
+      let q =
+      let ANF_0 = ( && ) true false in
+      let ANF_1 = if ANF_0 then
+      let ANF_2 = LL_0 1 in ANF_2 else
+      let ANF_3 = LL_1 1 in ANF_3 in ANF_1
+      |}]
+  ;;
+
+  let%expect_test "anf closure" =
+    pp_parse_and_anf "let q = let x = 1 in let y = 2 in x / y";
+    [%expect
+      {|
+      let q =
+      let x = 1 in
+      let y = 2 in
+      let ANF_0 = ( / ) x y in ANF_0
+      |}]
+  ;;
+
+  let%expect_test "anf lists" =
+    pp_parse_and_anf "let q = [(fun x -> x * x) 1; (fun x -> x / x) 2]";
+    [%expect
+      {|
+      let LL_0 x =
+      let ANF_4 = ( * ) x x in ANF_4
+
+      let LL_1 x =
+      let ANF_3 = ( / ) x x in ANF_3
+
+      let q =
+      let ANF_0 = LL_0 1 in
+      let ANF_1 = LL_1 2 in
+      let ANF_2 = [ ANF_0; ANF_1 ] in ANF_2
+      |}]
+  ;;
+end
