04a8a48943ee0d898c6c3d86b0be90a715d64aa0
raf-nr <nurmuhametovrafik04@gmail.com>
2025-03-02 22:50:48 +0300

    chore(ll): add mli files

---
+++ arML/lib/middleend/lambdaLifting/llExpression.mli
@@ -0,0 +1,17 @@
+(** Copyright 2024-2025, raf-nr and ksenmel *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+open Common.IdentifierStructs
+open Ast.AbstractSyntaxTree
+open Llast
+
+(** [ll_expression] lift all nested functions and 
+    return tuple of expression without nested functions and 
+    list of new top-level declarations *)
+val ll_expression :
+  IdentifierSet.t ->
+  identifier IdentifierMap.t ->
+  ll_decl list ->
+  expression ->
+  (ll_expr * ll_decl list) Common.StateMonad.t
