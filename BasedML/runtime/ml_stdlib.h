d1fff6032be53e81b6af84f6bdd9af87fcccc14f
Osogi <dragonmail135159@gmail.com>
2025-03-02 17:47:39 +0300

    Revert "refactor: remove simple fucntions"
    This reverts commit 6190ab07b95158bd65a187d011415d2113528fb7.

--- BasedML/runtime/ml_stdlib.h
+++ BasedML/runtime/ml_stdlib.h
@@ -11,9 +11,14 @@ int64_t div_mlint(int64_t x, int64_t y);
 
 int64_t eq_ml(int64_t x, int64_t y);
 int64_t neq_ml(int64_t x, int64_t y);
+int64_t peq_ml(int64_t x, int64_t y);
+int64_t pneq_ml(int64_t x, int64_t y);
 int64_t g_ml(int64_t x, int64_t y);
 int64_t ge_ml(int64_t x, int64_t y);
 int64_t l_ml(int64_t x, int64_t y);
 int64_t le_ml(int64_t x, int64_t y);
 
 void print_int(int64_t a);
+
+int64_t lor_ml(int64_t x, int64_t y);
+int64_t land_ml(int64_t x, int64_t y);
