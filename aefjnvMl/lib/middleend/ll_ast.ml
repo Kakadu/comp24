3a29fb66bc82a6160f0ec999f63b7c9996ea46f6
kogora <it.is.mr.lag@gmail.com>
2025-03-02 23:25:08 +0300

    add(ll_ast): basic version

--- aefjnvMl/lib/middleend/ll_ast.ml
+++ aefjnvMl/lib/middleend/ll_ast.ml
@@ -4,65 +4,32 @@
 
 (** 
 TODO (mutual rec): 
-  - mutaual recursion can be only with [rec] (invariant from parser) 
-     --> [and] can be only with function (at least one function would be) 
+  - mutaual recursion can be only with [rec] (invariant) 
+     --> [and] can be only with functions 
      --> mutaual recursion can be only in ll_decl as global funcs (ll-guarantees)
-  p.s. without rec [let a = 2 and b = 10] == [let a = 2;; let b = 10;;] (mb support it)
-  
-     [
-  let a c = 
-    let rec a' c' = b + 10
-    and b = 11 in
-    (a' 10) + c
-  ] ==> [
-  let rec a' c' = b + 10
-    and b = 11;;
-
-  let a c = (a' 10) + c  
-  ]
-
-TODO (let in):
-  - can't be function -- only  pattern * ll_expr
-  [
-  let f e = e;;
-  
-  let a =
-    let b = 10 in
-    let c = f 1 in
-    b + c
-  ;;
-  ]
 *)
 
-open Common.Ast
+open Match_elimination.Me_ast
 
 type ll_expr =
-  | LL_Id of ident
-  | LL_Const of const
-  | LL_App of ll_expr * ll_expr
-  | LL_List of ll_expr * ll_expr
-  | LL_LetIn of ll_bind * ll_expr
-  | LL_If of ll_expr * ll_expr * ll_expr
-  | LL_Tuple of ll_expr list
+  | LL_const of Common.Ast.const
+  | LL_ident of Common.Ast.ident
+  | LL_tuple of ll_expr list
+  | LL_apply of ll_expr * ll_expr
+  | LL_list of ll_expr * ll_expr
+  | LL_ifthenelse of ll_expr * ll_expr * ll_expr
+  | LL_let of ll_bind * ll_expr
 
-and ll_bind = pattern * ll_expr
+and ll_bind = me_ident * ll_expr
 
-type ll_vb = pattern * pattern list * ll_expr
-type ll_decl = ll_vb list
+type ll_global_var = me_ident * ll_expr
+type ll_fun = {
+  lldec_rf: Common.Ast.rec_flag;
+  lldec_name: me_ident;
+  lldec_args: me_ident * me_ident list;
+  lldec_body: ll_expr
+}
 
-(* type me_expr =
-   | ME_Id of me_ident
-   | ME_Const of Common.Ast.const
-   | ME_App of me_expr * me_expr
-   | ME_List of me_expr * me_expr
-   | ME_LetIn of ll_bind * me_expr
-   | ME_If of me_expr * me_expr * me_expr
-   | ME_Tuple of me_expr list
+type ll_structure_item = | LLGlobalV of ll_global_var | LLDecl of ll_fun
 
-   and ll_bind = me_ident * me_expr
-
-   type ll_vb = me_ident * me_ident list * me_expr
-
-   type ll_decl = ll_vb list *)
-
-(* type ident = string [@@deriving show { with_path = false }] *)
+type ll_program = ll_structure_item list
