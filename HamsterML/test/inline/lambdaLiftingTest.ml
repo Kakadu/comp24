afc10c9e5b495ec45f9be146f9b262c3aaa76433
d-zaytsev <dzaytsev308@gmail.com>
2025-03-03 15:09:41 +0300

    Add fac LL test

--- HamsterML/test/inline/lambdaLiftingTest.ml
+++ HamsterML/test/inline/lambdaLiftingTest.ml
@@ -2,3 +2,88 @@ open HamsterML.LL
 open ParserTest
 
 let lambda_lift_prog (s : string) = R.run (ll_prog (parse_prog s))
+
+let%test _ =
+  lambda_lift_prog "let a = 1 + 1"
+  = [ LLLet
+        ( Nonrecursive
+        , [ ( Var "a"
+            , []
+            , LLApplication
+                ( LLApplication (LLOperation (Binary ADD), LLConst (Int 1))
+                , LLConst (Int 1) ) )
+          ]
+        , None )
+    ]
+;;
+
+let%test _ =
+  lambda_lift_prog "let rec a = 1 + 1 in a + 2"
+  = [ LLLet
+        ( Recursive
+        , [ ( Var "a"
+            , []
+            , LLApplication
+                ( LLApplication (LLOperation (Binary ADD), LLConst (Int 1))
+                , LLConst (Int 1) ) )
+          ]
+        , Some
+            (LLApplication
+               (LLApplication (LLOperation (Binary ADD), LLVar "a"), LLConst (Int 2))) )
+    ]
+;;
+
+(*
+   let sum x y = let f a b = a + b in f x y
+   ---
+   let ll_fun_1 a b = a + b
+   let ll_fun_0 x y = ll_fun_1 x y
+*)
+
+let%test _ =
+  lambda_lift_prog "let sum x y = let f a b = a + b in f x y"
+  = [ LLLet
+        ( Nonrecursive
+        , [ ( Var "LL_fun_1"
+            , [ Var "a"; Var "b" ]
+            , LLApplication
+                (LLApplication (LLOperation (Binary ADD), LLVar "a"), LLVar "b") )
+          ]
+        , None )
+    ; LLLet
+        ( Nonrecursive
+        , [ ( Var "LL_fun_0"
+            , [ Var "x"; Var "y" ]
+            , LLApplication (LLApplication (LLVar "LL_fun_1", LLVar "x"), LLVar "y") )
+          ]
+        , None )
+    ]
+;;
+
+(* let rec fac n = if n<=1 then 1 else n * fac (n-1)
+   ---
+   let rec ll_fun_0 n = if n<=1 then 1 else n * ll_fun_0 (n-1)
+*)
+
+let%test _ =
+  lambda_lift_prog "let rec fac n = if n<=1 then 1 else n * fac (n-1)"
+  = [ LLLet
+        ( Recursive
+        , [ ( Var "LL_fun_0"
+            , [ Var "n" ]
+            , LLIf
+                ( LLApplication
+                    (LLApplication (LLOperation (Binary LTE), LLVar "n"), LLConst (Int 1))
+                , LLConst (Int 1)
+                , Some
+                    (LLApplication
+                       ( LLApplication (LLOperation (Binary MUL), LLVar "n")
+                       , LLApplication
+                           ( LLVar "LL_fun_0"
+                           , LLApplication
+                               ( LLApplication (LLOperation (Binary SUB), LLVar "n")
+                               , LLConst (Int 1) ) ) )) ) )
+          ]
+        , None )
+    ]
+;;
