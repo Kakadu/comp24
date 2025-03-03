d08b5cf13fa21abd1f3832dbebddd60e85882a6d
Dmitry Pilyuk <91498570+DmitryPilyuk@users.noreply.github.com>
2025-03-03 21:53:43 +0300

    [D] FML: parser and typechecker (#61)ч
    * Create FML folder
    
    * Add ast
    
    * Add pattern parsers
    
    * fix sqr_br
    
    * Add parsers
    
    * Add pattern parser without type
    
    * Add type parsers
    
    * Add bin_op parsers
    
    * Add fun parser
    
    * add Ematch && EIf && parse_Eif
    
    * Add declaration parser
    
    * add expression parsers
    
    * add parse_expr
    
    * Clean code and fix some errors
    
    * Add tests
    
    * Edit tests
    
    * Add tests
    
    * add parse match
    
    * add parse list
    
    * add parse_operators && fix bugs with patterns and whitespace in the end
    
    * delete binop
    
    * Fix let
    
    * delete parse_oper
    
    * fix (+) Identifier
    
    * fix delete EList && create ECons
    
    * Add mutable rec declarations
    
    * Fix let
    
    * Remove useless
    
    * Start inferencer
    
    * Add type module
    
    * Add substitution module
    
    * Add scheme module
    
    * Fix parser error
    
    * Work on typecheck
    
    * Fix ast
    
    * Add pattern infer
    
    * Fix list cons
    
    * Add expr infer
    
    * fix bugs with operators
    
    * Add ext_by_pat
    
    * add infer let
    
    * Add program parser
    
    * Add inference function and tests
    
    * Add recalculate function
    
    * Use manytests
    
    * Fix parser issues
    
    * fix: problem with apply
    
    * Fix match without |
    
    * fix: let in let
    
    * Add unop to AST and fix _ symbol
    
    * Unop as application
    
    * feat: add EUnit type
    
    * Inferencer manytests
    
    * Add parsing expressions with type
    
    * Improve tests
    
    * Add parsing bin ops as application
    
    * work
    
    * Add inferencing of expr with annotation
    
    * Work on inference
    
    * Change ast
    
    * Implement rec infer
    
    * Add parsing names with numbers
    
    * Add parsing unary minus
    
    * Fix issues
    
    * Add mli files
    
    * Edit ocamlformat version
    
    * Fix formating
    
    ---------
    
    Co-authored-by: RozhkovAleksandr <rozhkov_a@outlook.com>

---
+++ FML/tests/inferencer_manytests.t
@@ -0,0 +1,82 @@
+  $ ./inferencer_runner.exe < manytests/typed/001fac.ml
+  val fac : int -> int
+  val main : int
+
+  $ ./inferencer_runner.exe < manytests/typed/002fac.ml
+  val fac_cps : int -> (int -> 'a) -> 'a
+  val main : int
+
+  $ ./inferencer_runner.exe < manytests/typed/003fib.ml
+  val fib_acc : int -> int -> int -> int
+  val fib : int -> int
+  val main : int
+
+  $ ./inferencer_runner.exe < manytests/typed/004manyargs.ml
+  val wrap : 'a -> 'a
+  val test3 : int -> int -> int -> int
+  val test10 : int -> int -> int -> int -> int -> int -> int -> int -> int -> int -> int
+  val main : int
+
+  $ ./inferencer_runner.exe < manytests/typed/005fix.ml
+  val fix : (('a -> 'b) -> 'a -> 'b) -> 'a -> 'b
+  val fac : (int -> int) -> int -> int
+  val main : int
+
+  $ ./inferencer_runner.exe < manytests/typed/006partial.ml
+  val foo : int -> int
+  val main : int
+
+  $ ./inferencer_runner.exe < manytests/typed/006partial2.ml
+  val foo : int -> int -> int -> int
+  val main : int
+
+  $ ./inferencer_runner.exe < manytests/typed/006partial3.ml
+  val foo : int -> int -> int -> unit
+  val main : int
+
+  $ ./inferencer_runner.exe < manytests/typed/007order.ml
+  val _start : unit -> unit -> int -> unit -> int -> int -> unit -> int -> int -> int
+  val main : unit
+
+  $ ./inferencer_runner.exe < manytests/typed/008ascription.ml
+  val addi : ('a -> bool -> int) -> ('a -> bool) -> 'a -> int
+  val main : int
+
+  $ ./inferencer_runner.exe < manytests/typed/009let_poly.ml
+  val temp : int * bool
+
+  $ ./inferencer_runner.exe < manytests/typed/015tuples.ml
+  val fix : (('a -> 'b) -> 'a -> 'b) -> 'a -> 'b
+  val map : ('a -> 'b) -> 'a * 'a -> 'b * 'b
+  val fixpoly : (('a -> 'b) * ('a -> 'b) -> 'a -> 'b) * (('a -> 'b) * ('a -> 'b) -> 'a -> 'b) -> ('a -> 'b) * ('a -> 'b)
+  val feven : 'a * (int -> int) -> int -> int
+  val fodd : (int -> int) * 'a -> int -> int
+  val tie : (int -> int) * (int -> int)
+  val meven : int -> int
+  val modd : int -> int
+  val main : int
+
+  $ ./inferencer_runner.exe < manytests/typed/016lists.ml
+  val length : 'a list -> int
+  val length_tail : 'a list -> int
+  val map : ('a -> 'b) -> 'a list -> 'b list
+  val append : 'a list -> 'a list -> 'a list
+  val concat : 'a list list -> 'a list
+  val iter : ('a -> unit) -> 'a list -> unit
+  val cartesian : 'a list -> 'b list -> 'a * 'b list
+  val main : int
+
+  $ ./inferencer_runner.exe < manytests/do_not_type/001.ml
+  Type error: unbound variable 'fac'
+  $ ./inferencer_runner.exe < manytests/do_not_type/002if.ml
+  Type error: unification failed - type bool does not match expected type int
+  $ ./inferencer_runner.exe < manytests/do_not_type/003occurs.ml
+  Type error: the type variable 'a occurs inside 'a -> 'b
+
+  $ ./inferencer_runner.exe < manytests/do_not_type/004let_poly.ml
+  Type error: unification failed - type int does not match expected type bool
+
+  $ ./inferencer_runner.exe < manytests/do_not_type/015tuples.ml
+  Type error: Only variables are allowed as left-hand side of `let rec'
+
+
---
+++ FML/tests/parser_tests.ml
@@ -0,0 +1,345 @@
+(** Copyright 2024-2025, Dmitry Pilyuk, Aleksandr Rozhkov *)
+
+(** SPDX-License-Identifier: LGPL-2.1 *)
+
+open Fml_lib.Parser
+open Fml_lib.Ast
+
+let parse_with_print input =
+  match parse input with
+  | Ok ast -> Format.printf "%a\n" pp_program ast
+  | Error message -> Format.printf "Error: %s\n" message
+;;
+
+let%expect_test _ =
+  parse_with_print {| let n = 5|};
+  [%expect {| [(NoRecDecl [(DDeclaration ((PIdentifier "n"), (EConst (CInt 5))))])] |}]
+;;
+
+let%expect_test _ =
+  parse_with_print {| let n = 5
+
+  let f x = x|};
+  [%expect
+    {|
+      [(NoRecDecl [(DDeclaration ((PIdentifier "n"), (EConst (CInt 5))))]);
+        (NoRecDecl
+           [(DDeclaration ((PIdentifier "f"),
+               (EFun ((PIdentifier "x"), (EIdentifier "x")))))
+             ])
+        ] |}]
+;;
+
+let%expect_test _ =
+  parse_with_print {| let flag = true|};
+  [%expect
+    {|
+    [(NoRecDecl [(DDeclaration ((PIdentifier "flag"), (EConst (CBool true))))])] |}]
+;;
+
+let%expect_test _ =
+  parse_with_print {| let flag = false|};
+  [%expect
+    {|
+    [(NoRecDecl [(DDeclaration ((PIdentifier "flag"), (EConst (CBool false))))])] |}]
+;;
+
+let%expect_test _ =
+  parse_with_print {| let (h::tl) = lst|};
+  [%expect
+    {|
+    [(NoRecDecl
+        [(DDeclaration ((PCons ((PIdentifier "h"), (PIdentifier "tl"))),
+            (EIdentifier "lst")))
+          ])
+      ] |}]
+;;
+
+let%expect_test _ =
+  parse_with_print
+    {| let mymatch x =
+        match x with
+          | 1 -> true
+          | _ -> false|};
+  [%expect
+    {|
+    [(NoRecDecl
+        [(DDeclaration ((PIdentifier "mymatch"),
+            (EFun ((PIdentifier "x"),
+               (EMatch ((EIdentifier "x"),
+                  [((PConst (CInt 1)), (EConst (CBool true)));
+                    (PAny, (EConst (CBool false)))]
+                  ))
+               ))
+            ))
+          ])
+      ] |}]
+;;
+
+let%expect_test _ =
+  parse_with_print {|
+        let a = 4::[5; 6]|};
+  [%expect
+    {|
+        [(NoRecDecl
+            [(DDeclaration ((PIdentifier "a"),
+                (ECons ((EConst (CInt 4)),
+                   (ECons ((EConst (CInt 5)), (ECons ((EConst (CInt 6)), ENill))))))
+                ))
+              ])
+          ]
+    |}]
+;;
+
+let%expect_test _ =
+  parse_with_print {| let fix f = let rec g x = f (g x) in g|};
+  [%expect
+    {|
+    [(NoRecDecl
+        [(DDeclaration ((PIdentifier "fix"),
+            (EFun ((PIdentifier "f"),
+               (ELetIn (Rec, (PIdentifier "g"),
+                  (EFun ((PIdentifier "x"),
+                     (EApplication ((EIdentifier "f"),
+                        (EApplication ((EIdentifier "g"), (EIdentifier "x")))))
+                     )),
+                  (EIdentifier "g")))
+               ))
+            ))
+          ])
+      ] |}]
+;;
+
+let%expect_test _ =
+  parse_with_print {|
+        let add_one (x: int) = 1 + x|};
+  [%expect
+    {|
+        [(NoRecDecl
+            [(DDeclaration ((PIdentifier "add_one"),
+                (EFun ((PConstraint ((PIdentifier "x"), AInt)),
+                   (EApplication (
+                      (EApplication ((EIdentifier "( + )"), (EConst (CInt 1)))),
+                      (EIdentifier "x")))
+                   ))
+                ))
+              ])
+          ]
+    |}]
+;;
+
+let%expect_test _ =
+  parse_with_print {|
+        let (+) a = 1 + a |};
+  [%expect
+    {|
+        [(NoRecDecl
+            [(DDeclaration ((PIdentifier "( + )"),
+                (EFun ((PIdentifier "a"),
+                   (EApplication (
+                      (EApplication ((EIdentifier "( + )"), (EConst (CInt 1)))),
+                      (EIdentifier "a")))
+                   ))
+                ))
+              ])
+          ]
+    |}]
+;;
+
+let%expect_test _ =
+  parse_with_print {|
+        let f a b = b + a |};
+  [%expect
+    {|
+        [(NoRecDecl
+            [(DDeclaration ((PIdentifier "f"),
+                (EFun ((PIdentifier "a"),
+                   (EFun ((PIdentifier "b"),
+                      (EApplication (
+                         (EApplication ((EIdentifier "( + )"), (EIdentifier "b"))),
+                         (EIdentifier "a")))
+                      ))
+                   ))
+                ))
+              ])
+          ]
+    |}]
+;;
+
+let%expect_test _ =
+  parse_with_print
+    {|let rec first x = match x with
+  | 1 -> 1
+  | x -> second (x + 10)
+and second x = first (x + 1);;
+
+|};
+  [%expect
+    {|
+    [(RecDecl
+        [(DDeclaration ((PIdentifier "first"),
+            (EFun ((PIdentifier "x"),
+               (EMatch ((EIdentifier "x"),
+                  [((PConst (CInt 1)), (EConst (CInt 1)));
+                    ((PIdentifier "x"),
+                     (EApplication ((EIdentifier "second"),
+                        (EApplication (
+                           (EApplication ((EIdentifier "( + )"),
+                              (EIdentifier "x"))),
+                           (EConst (CInt 10))))
+                        )))
+                    ]
+                  ))
+               ))
+            ));
+          (DDeclaration ((PIdentifier "second"),
+             (EFun ((PIdentifier "x"),
+                (EApplication ((EIdentifier "first"),
+                   (EApplication (
+                      (EApplication ((EIdentifier "( + )"), (EIdentifier "x"))),
+                      (EConst (CInt 1))))
+                   ))
+                ))
+             ))
+          ])
+      ] |}]
+;;
+
+let%expect_test _ =
+  parse_with_print {|
+        let h::x::tl = 5::6::4::[]|};
+  [%expect
+    {|
+    [(NoRecDecl
+        [(DDeclaration (
+            (PCons ((PIdentifier "h"),
+               (PCons ((PIdentifier "x"), (PIdentifier "tl"))))),
+            (ECons ((EConst (CInt 5)),
+               (ECons ((EConst (CInt 6)), (ECons ((EConst (CInt 4)), ENill))))))
+            ))
+          ])
+      ] |}]
+;;
+
+let%expect_test _ =
+  parse_with_print
+    {|
+        let rec factorial n = if n < 1 then 1 else n * factorial (n - 1) |};
+  [%expect
+    {|
+        [(RecDecl
+            [(DDeclaration ((PIdentifier "factorial"),
+                (EFun ((PIdentifier "n"),
+                   (EIf (
+                      (EApplication (
+                         (EApplication ((EIdentifier "( < )"), (EIdentifier "n"))),
+                         (EConst (CInt 1)))),
+                      (EConst (CInt 1)),
+                      (EApplication (
+                         (EApplication ((EIdentifier "( * )"), (EIdentifier "n"))),
+                         (EApplication ((EIdentifier "factorial"),
+                            (EApplication (
+                               (EApplication ((EIdentifier "( - )"),
+                                  (EIdentifier "n"))),
+                               (EConst (CInt 1))))
+                            ))
+                         ))
+                      ))
+                   ))
+                ))
+              ])
+          ]
+    |}]
+;;
+
+let%expect_test _ =
+  parse_with_print {|let () = print_int ( fac_cps 4 ( fun print_int -> print_int ) )|};
+  [%expect
+    {|
+      [(NoRecDecl
+          [(DDeclaration (PUnit,
+              (EApplication ((EIdentifier "print_int"),
+                 (EApplication (
+                    (EApplication ((EIdentifier "fac_cps"), (EConst (CInt 4)))),
+                    (EFun ((PIdentifier "print_int"), (EIdentifier "print_int")))))
+                 ))
+              ))
+            ])
+        ]
+  |}]
+;;
+
+let%expect_test _ =
+  parse_with_print
+    {|let rec fac_cps n k =
+   if n=1 then k 1 else
+   fac_cps (n-1) (fun p -> k (p*n))|};
+  [%expect
+    {|
+      [(RecDecl
+          [(DDeclaration ((PIdentifier "fac_cps"),
+              (EFun ((PIdentifier "n"),
+                 (EFun ((PIdentifier "k"),
+                    (EIf (
+                       (EApplication (
+                          (EApplication ((EIdentifier "( = )"), (EIdentifier "n"))),
+                          (EConst (CInt 1)))),
+                       (EApplication ((EIdentifier "k"), (EConst (CInt 1)))),
+                       (EApplication (
+                          (EApplication ((EIdentifier "fac_cps"),
+                             (EApplication (
+                                (EApplication ((EIdentifier "( - )"),
+                                   (EIdentifier "n"))),
+                                (EConst (CInt 1))))
+                             )),
+                          (EFun ((PIdentifier "p"),
+                             (EApplication ((EIdentifier "k"),
+                                (EApplication (
+                                   (EApplication ((EIdentifier "( * )"),
+                                      (EIdentifier "p"))),
+                                   (EIdentifier "n")))
+                                ))
+                             ))
+                          ))
+                       ))
+                    ))
+                 ))
+              ))
+            ])
+        ]
+  |}]
+;;
+
+let%expect_test _ =
+  parse_with_print
+    {| let test3 a b c =
+  let a = print_int a in
+  let b = print_int b in
+  let c = print_int c in
+  0|};
+  [%expect
+    {|
+    [(NoRecDecl
+        [(DDeclaration ((PIdentifier "test3"),
+            (EFun ((PIdentifier "a"),
+               (EFun ((PIdentifier "b"),
+                  (EFun ((PIdentifier "c"),
+                     (ELetIn (NoRec, (PIdentifier "a"),
+                        (EApplication ((EIdentifier "print_int"),
+                           (EIdentifier "a"))),
+                        (ELetIn (NoRec, (PIdentifier "b"),
+                           (EApplication ((EIdentifier "print_int"),
+                              (EIdentifier "b"))),
+                           (ELetIn (NoRec, (PIdentifier "c"),
+                              (EApplication ((EIdentifier "print_int"),
+                                 (EIdentifier "c"))),
+                              (EConst (CInt 0))))
+                           ))
+                        ))
+                     ))
+                  ))
+               ))
+            ))
+          ])
+      ] |}]
+;;
