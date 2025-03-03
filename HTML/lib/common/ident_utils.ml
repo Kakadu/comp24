854153a22d53d48cdded06c1df1b4628cf7da2fe
Parzival-05 <parzivalwasd@gmail.com>
2025-03-03 21:50:39 +0300

    Alpha conv

---
+++ HTML/lib/common/ident_utils.ml
@@ -0,0 +1,11 @@
+open AstLib.Ast
+
+let ident_to_string id =
+  match id with
+  | IdentOfDefinable (IdentLetters s) -> s
+  | IdentOfDefinable (IdentOp s) -> s
+  | IdentOfBaseOp base_op ->
+    (match base_op with
+     | Plus -> "base +"
+     | Minus -> "base -")
+;;
