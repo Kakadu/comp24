801cf72f5bc88be955efd8dcca0b41be46f65a22
kogora <it.is.mr.lag@gmail.com>
2025-03-03 19:05:43 +0300

    refactor(ll_ast): naming

--- aefjnvMl/lib/middleend/ll_ast.ml
+++ aefjnvMl/lib/middleend/ll_ast.ml
@@ -20,17 +20,14 @@ type ll_expr =
 
 and ll_bind = me_ident * ll_expr
 
-type ll_global_var = me_ident * ll_expr
-
 type ll_fun =
-  { lldec_rf : Common.Ast.rec_flag
-  ; lldec_name : me_ident
+  { lldec_name : me_ident
   ; lldec_args : me_ident * me_ident list
   ; lldec_body : ll_expr
   }
 
 type ll_structure_item =
-  | LLGlobalV of ll_global_var
-  | LLDecl of ll_fun * ll_fun list (** if mutual rec *)
+  | LL_GlobalV of me_ident * ll_expr
+  | LL_Decl of Common.Ast.rec_flag * ll_fun * ll_fun list (** if mutual rec *)
 
 type ll_program = ll_structure_item list
