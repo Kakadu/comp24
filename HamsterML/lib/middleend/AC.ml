ef2682fbb1e4e402de57748c605a37aa09d61465
d-zaytsev <dzaytsev308@gmail.com>
2025-03-04 18:18:43 +0300

    Separate ac_pattern and ac_name_pattern

--- HamsterML/lib/middleend/AC.ml
+++ HamsterML/lib/middleend/AC.ml
@@ -3,25 +3,48 @@ open Base
 module Format = Stdlib.Format
 open Utils.R
 
+module NameSet = struct
+  include Utils.NameSet
+end
+
 module NameEnv = struct
   include Utils.NameEnv
 
-  let rec generate_name (env : t) (name : id) (prefix : string) =
+  (* generates name with overlap *)
+  let rec generate_new_name (env : t) (name : id) (prefix : string) =
+    let* fresh_num = fresh in
+    let new_name = prefix ^ Int.to_string fresh_num in
+    match find new_name env with
+    | None -> return (extend (name, new_name) env, new_name)
+    | Some _ -> generate_new_name env name prefix
+  ;;
+
+  (* generates new name or returns old one if it exists *)
+  let resolve_name (env : t) (name : id) (prefix : string) =
     match find name env with
-    | None ->
-      let* fresh_num = fresh in
-      let new_name = prefix ^ Int.to_string fresh_num in
-      (match find new_name env with
-       | None -> return (extend (name, new_name) env, new_name)
-       | Some _ -> generate_name env name prefix)
-    | Some new_name -> return (env, new_name)
+    | None -> generate_new_name env name prefix
+    | Some old_name -> return (env, old_name)
+  ;;
+
+  let sub (env1 : t) (env2 : t) : t =
+    Map.fold env1 ~init:empty ~f:(fun ~key ~data acc ->
+      match Map.find env2 key with
+      | None -> extend (key, data) acc
+      | Some _ -> acc)
   ;;
 end
 
+let arg_prefix = "arg_"
+let let_prefix = "var_"
+
 let rec ac_pattern (env : NameEnv.t) = function
-  | (Const _ | Operation _ | Wildcard) as orig -> return (env, orig)
+  | (Const _ | Wildcard | Operation (Unary _)) as orig -> return (env, orig)
   | Var id ->
-    let* env, new_id = NameEnv.generate_name env id "var_" in
+    let* env, new_id = NameEnv.resolve_name env id arg_prefix in
+    return (env, Var new_id)
+  | Operation (Binary op) ->
+    let op_id = BinOperator.to_string op in
+    let* env, new_id = NameEnv.resolve_name env op_id arg_prefix in
     return (env, Var new_id)
   | Constraint (p, dt) ->
     let* env, ac_p = ac_pattern env p in
@@ -50,16 +73,48 @@ let rec ac_pattern (env : NameEnv.t) = function
     return (env, Tuple (ac_a, ac_b, ac_pts))
 ;;
 
-let rec ac_expr (env : NameEnv.t) = function
-  | (EConst _ | EOperation _) as orig -> return orig
+let ac_name_pattern (env : NameEnv.t) (arg : pattern) (name_prefix : string) =
+  match arg with
+  | Var id ->
+    let* env, new_id = NameEnv.generate_new_name env id name_prefix in
+    return (env, Var new_id)
+  | Operation (Binary op) ->
+    let op_id = BinOperator.to_string op in
+    let* env, new_id = NameEnv.resolve_name env op_id name_prefix in
+    return (env, Var new_id)
+  | arg -> ac_pattern env arg
+;;
+
+let collect_args (env : NameEnv.t) (args : args) =
+  let* env, ac_args =
+    fold_list args ~init:(env, []) ~f:(fun (env, acc) arg ->
+      let* env, ac_arg = ac_name_pattern env arg arg_prefix in
+      return (env, ac_arg :: acc))
+  in
+  return (env, List.rev ac_args)
+;;
+
+let rec ac_bind (env : NameEnv.t) ((name, args, body) : bind) =
+  let* env, new_name = ac_name_pattern env name let_prefix in
+  let* args_env, ac_args = collect_args env args in
+  let* env, ac_body = ac_expr args_env body in
+  let env = NameEnv.sub env args_env in
+  return (env, ((new_name, ac_args, ac_body) : bind))
+
+and ac_expr (env : NameEnv.t) = function
+  | (EConst _ | EOperation _) as orig -> return (env, orig)
   | EVar id ->
     (match NameEnv.find id env with
-     | Some new_id -> return (EVar new_id)
-     | None -> return (EVar id))
-  | ETuple (a, b, tl) ->
-    let* ac_a = ac_expr env a in
-    let* ac_b = ac_expr env b in
-    let* ac_tl = map_list tl ~f:(ac_expr env) in
-    return (ETuple (ac_a, ac_b, ac_tl))
+     | Some new_id -> return (env, EVar new_id)
+     | None -> return (env, EVar id))
+  | Application (l, r) ->
+    let* env, ac_l = ac_expr env l in
+    let* env, ac_r = ac_expr env r in
+    return (env, Application (ac_l, ac_r))
+  | Fun (args, body) ->
+    let* args_env, ac_args = collect_args env args in
+    let* env, ac_body = ac_expr args_env body in
+    let env = NameEnv.sub args_env env in
+    return (env, Fun (ac_args, ac_body))
   | _ -> failwith "Illegal expression was encountered during Alpha Conversion"
 ;;
--- HamsterML/test/inline/alphaConversionTest.ml
+++ HamsterML/test/inline/alphaConversionTest.ml
@@ -7,16 +7,35 @@ let alpha_conv_pattern (s : string) =
   res
 ;;
 
+let alpha_conv_expr (s : string) =
+  let _, res = HamsterML.Utils.R.run (ac_expr NameEnv.empty (parse_expr s)) in
+  res
+;;
+
 let%test _ =
   alpha_conv_pattern "a :: b :: a"
-  = ListConcat (Var "var_0", ListConcat (Var "var_1", Var "var_0"))
+  = ListConcat (Var "arg_0", ListConcat (Var "arg_1", Var "arg_0"))
 ;;
 
 let%test _ =
   alpha_conv_pattern "a, b, c, a"
-  = Tuple (Var "var_0", Var "var_1", [ Var "var_2"; Var "var_0" ])
+  = Tuple (Var "arg_0", Var "arg_1", [ Var "arg_2"; Var "arg_0" ])
 ;;
 
 let%test _ =
-  alpha_conv_pattern "[a; b; a; c]" = List [ Var "var_0"; Var "var_1"; Var "var_0"; Var "var_2" ]
+  alpha_conv_pattern "[a; b; a; c]"
+  = List [ Var "arg_0"; Var "arg_1"; Var "arg_0"; Var "arg_2" ]
+;;
+
+let%test _ =
+  alpha_conv_expr "fun a -> fun b -> fun a -> a + b"
+  = Fun
+      ( [ Var "arg_0" ]
+      , Fun
+          ( [ Var "arg_1" ]
+          , Fun
+              ( [ Var "arg_2" ]
+              , Application
+                  (Application (EOperation (Binary ADD), EVar "arg_2"), EVar "arg_1") ) )
+      )
 ;;
