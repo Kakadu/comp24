80b10ebc398009a4d8b1c4bc09da3f6ad9f49c16
Parzival-05 <parzivalwasd@gmail.com>
2025-03-04 20:47:56 +0300

    Add demos

---
+++ HTML/demos/alpha_demo.ml
@@ -0,0 +1,42 @@
+let ( let+ ) x f =
+  match x with
+  | Error err -> Error err
+  | Ok v -> f v
+;;
+
+let alpha_demo s =
+  let open TypeInference in
+  let open Result in
+  let+ prog = Parser.parse_program s in
+  Format.printf "Type inference before:\n";
+  Inferencer.run_inference prog |> Inferencer.print_env;
+  Format.printf "\n";
+  let+ prog = Patelim.Elim.p_elim_decls prog in
+  let+ prog = Anf.Cc_ll.closure_convert prog in
+  let+ prog = Anf.Anf_conv.run prog in
+  let restored_ast = Anf.Restore_ast.convert_anf_prog prog in
+  Format.printf "Type inference after:\n";
+  let env = Inferencer.run_inference restored_ast in
+  match env with
+  | Error err ->
+    let err = Format.asprintf "Typecheck error: %a" Typing.pp_error err in
+    Error err
+  | Ok _ ->
+    let alpha_prog = Anf.Alpha_conv.alpha_convert_prog prog in
+    (match alpha_prog with
+     | Ok prog ->
+       Inferencer.print_env env;
+       Format.printf "\n";
+       Format.printf "%a\n" Anf.Pp_anf_ast.pp_anf_prog prog;
+       Ok ()
+     | Error err ->
+       let err = Format.asprintf "Alpha conf error: %s" err in
+       Error err)
+;;
+
+let () =
+  let s = Stdio.In_channel.input_all Stdlib.stdin in
+  match alpha_demo s with
+  | Ok _ -> ()
+  | Error err -> Format.printf "%s\n" err
+;;
