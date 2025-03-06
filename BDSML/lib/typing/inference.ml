75f5a353ef7ab4436d89cc9f0ec54553fc20f805
LeonidElkin <yolkinleonwork@gmail.com>
2025-03-06 13:04:54 +0300

    test: added manytests support for inference

--- BDSML/lib/typing/inference.ml
+++ BDSML/lib/typing/inference.ml
@@ -53,6 +53,7 @@ let typexpr_to_type typexpr =
     | "char" -> Some TChar
     | "string" -> Some TString
     | "bool" -> Some TBool
+    | "unit" -> Some TUnit
     | _ -> None
   in
   let rec helper (type_vars : type_val TypeVars.t) = function
@@ -357,7 +358,8 @@ let predefine_operators =
   ; "( <= )", "'a -> 'a -> bool"
   ; "( = )", "'a -> 'a -> bool"
   ; "( <> )", "'a -> 'a -> bool"
-  ; "( || )", "bool -> bool -> bool"
+  ; "( == )", "'a -> 'a -> bool"
+  ; "print_int", "int -> unit"
   ]
 ;;
 
