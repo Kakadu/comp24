ebdd3cb57e913212839efe0e0e862867d533dbb5
Kirill Shishin <shishin_ka@mail.ru>
2025-03-07 14:37:08 +0300

    chore: Delete extra code

--- ObaML/demo_tests/llvm_compiler_tests.ml
+++ ObaML/demo_tests/llvm_compiler_tests.ml
@@ -27,24 +27,3 @@ let () =
      | Error e -> Format.printf "Infer: %a" Typedtree.pp_error e)
   | Error err -> Format.printf "Parser: %s\n" err
 ;;
-
-(* let () =
-   let s = Stdio.In_channel.input_all Stdlib.stdin in
-   match Parser.structure_from_string s with
-   | Ok structure ->
-   let simple_structure = To_simple_ast.convert structure in
-   let simple_structure, varSet =
-   Alpha_conversion.run_alpha_conversion simple_structure Inner
-   in
-   let simple_structure = Closure_conversion.run_closure_conversion simple_structure in
-   let simple_structure, varSet =
-   Lambda_lifting.run_lambda_lifting simple_structure varSet
-   in
-   let anf_res = To_anf.convert simple_structure varSet in
-   (match anf_res with
-   | Ok anf ->
-   let compiled_program = Compiler.generate_ir anf in
-   Format.printf "%s" compiled_program
-   | Error e -> Format.printf "Anf conversion error: %s" e)
-   | Error err -> Format.printf "Parser: %s\n" err
-   ;; *)
