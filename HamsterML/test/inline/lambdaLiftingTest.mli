326ec121f069ce5907b8fbdc091aefc94d46094c
d-zaytsev <dzaytsev308@gmail.com>
2025-03-07 21:12:05 +0300

    fmr & add interfaces

--- HamsterML/test/inline/lambdaLiftingTest.mli
+++ HamsterML/test/inline/lambdaLiftingTest.mli
@@ -1,4 +1,5 @@
 (* Apply lambda lifting to string *)
 val lambda_lift_prog : string -> HamsterML.LL.ll_prog
-(* Print lambda lifting result  *)
+
+(* Print lambda lifting result *)
 val pp_lambda_lift_prog : string -> unit
