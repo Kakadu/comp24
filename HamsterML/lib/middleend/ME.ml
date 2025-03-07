c782035a6daf4862f8ce2ea72f92ac6b20c456fe
d-zaytsev <dzaytsev308@gmail.com>
2025-03-07 17:03:28 +0300

    Add basic list&tuple operations

--- HamsterML/lib/middleend/ME.ml
+++ HamsterML/lib/middleend/ME.ml
@@ -19,6 +19,7 @@ and me_prog = me_expr list
 module ListConverter : sig
   val tail : me_expr -> me_expr
   val len : me_expr -> me_expr
+  val head : me_expr -> me_expr
   val get : me_expr -> int -> me_expr
 end = struct
   let ensure_list (lst : me_expr) =
@@ -27,12 +28,41 @@ end = struct
     | _ -> failwith "Attempt to perform a list operation on a non-list"
   ;;
 
-  let len lst = MEApplication (MEVar "length", ensure_list lst)
+  let len lst = MEApplication (MEVar "list_length", ensure_list lst)
 
   let get lst i =
-    MEApplication (MEApplication (MEVar "get", ensure_list lst), MEConst (Int i))
+    MEApplication (MEApplication (MEVar "list_get", ensure_list lst), MEConst (Int i))
   ;;
 
-  let head lst = MEApplication (MEVar "head", ensure_list lst)
-  let tail lst = MEApplication (MEVar "tail", ensure_list lst)
+  let head lst = MEApplication (MEVar "list_head", ensure_list lst)
+  let tail lst = MEApplication (MEVar "list_tail", ensure_list lst)
 end
+
+module TupleConverter : sig
+  val len : me_expr -> me_expr
+  val get : me_expr -> int -> me_expr
+end = struct
+  let ensure_tuple (tpl : me_expr) =
+    match tpl with
+    | METuple _ -> tpl
+    | _ -> failwith "Attempt to perform a tuple operation on a non-tuple"
+  ;;
+
+  let len lst = MEApplication (MEVar "tuple_length", ensure_tuple lst)
+
+  let get lst i =
+    MEApplication (MEApplication (MEVar "tuple_get", ensure_tuple lst), MEConst (Int i))
+  ;;
+end
+
+(* AND for conditions *)
+let ( <|> ) (a : me_expr) (b : me_expr) =
+  match a, b with
+  | MEConst (Bool true), b -> b
+  | a, MEConst (Bool true) -> a
+  | a, b -> MEApplication (MEApplication (MEOperation (Binary AND), a), b)
+;;
+
+
+
+
