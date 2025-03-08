b4419f834b38007ee598b95a845d16fd5092b147
AlexShmak <lexnder9@gmail.com>
2025-03-08 22:30:37 +0300

    Implement basic ImmExpr llvm code generation

--- HamsterML/lib/llvm/compiler.ml
+++ HamsterML/lib/llvm/compiler.ml
@@ -1,4 +1,5 @@
 open Llvm
+open Anf
 
 (* LLVM setup *)
 let global_context = global_context ()
@@ -8,6 +9,11 @@ let i64 = i16_type global_context
 let lookup_function_exception id llmodule = Option.get @@ lookup_function id llmodule
 
 (* TODO: codegen functions *)
+let codegen_immexpr = function
+  | ImmInt i -> const_int i64 i
+  | ImmBool b -> const_int i64 (Base.Bool.to_int b)
+  | _ -> failwith "not yet implemented"
+;;
 
 (* let codegen_immexpr = failwith "not yet implemented"
    let codegen_cexpr = failwith "not yet implemented"
