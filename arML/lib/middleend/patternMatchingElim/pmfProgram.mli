4eafa5ba33abec65afa2d79d5e90f6cbd9db0145
raf-nr <nurmuhametovrafik04@gmail.com>
2025-03-08 21:06:26 +0300

    Implement pattern matcghing elim for program

---
+++ arML/lib/middleend/patternMatchingElim/pmfProgram.mli
@@ -0,0 +1,8 @@
+(** Copyright 2024-2025, raf-nr and ksenmel *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+val eliminate_pm_program : 
+  Common.IdentifierStructs.IdentifierSet.t -> 
+  LambdaLifting.Llast.ll_program -> 
+  (Pmfast.pmf_program * Common.IdentifierStructs.IdentifierSet.t) Common.StateMonad.t
