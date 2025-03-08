ce93478217762955ab0c4d2ebde8b303089f5b22
Dmitry Pilyuk <dmitriipiliuk@gmail.com>
2025-03-09 01:42:25 +0300

    Fix format

--- FML/lib/anf/pe_ast.mli
+++ FML/lib/anf/pe_ast.mli
@@ -3,28 +3,28 @@
 (** SPDX-License-Identifier: LGPL-2.1 *)
 
 type rec_flag =
-  | Rec
-  | NoRec
+  | Rec (** rec *)
+  | NoRec (** norec*)
 
 type pe_const =
-  | Pe_Cint of int
-  | Pe_CBool of bool
+  | Pe_Cint of int (** 1 *)
+  | Pe_CBool of bool (** true *)
 
 type pe_expr =
-  | Pe_EUnit
-  | Pe_ENill
-  | Pe_EIdentifier of string
-  | Pe_EConst of pe_const
-  | Pe_EIf of pe_expr * pe_expr * pe_expr
-  | Pe_EFun of string list * pe_expr
-  | Pe_EApp of pe_expr * pe_expr
-  | Pe_ELet of rec_flag * string * pe_expr * pe_expr
-  | Pe_ECons of pe_expr * pe_expr
-  | Pe_ETuple of pe_expr list
+  | Pe_EUnit (** () *)
+  | Pe_ENill (** [] *)
+  | Pe_EIdentifier of string (** x *)
+  | Pe_EConst of pe_const (** 1 || true *)
+  | Pe_EIf of pe_expr * pe_expr * pe_expr (** if E1 then E2 else E3*)
+  | Pe_EFun of string list * pe_expr (** fun x y -> E *)
+  | Pe_EApp of pe_expr * pe_expr (** E1 E2 *)
+  | Pe_ELet of rec_flag * string * pe_expr * pe_expr (** let (rec) f = E1 in E2 *)
+  | Pe_ECons of pe_expr * pe_expr (** E1 :: E2 *)
+  | Pe_ETuple of pe_expr list (** (E1, E2, E3) *)
 
 type pe_declaration =
-  | Pe_Nonrec of (string * pe_expr) list
-  | Pe_Rec of (string * pe_expr) list
+  | Pe_Nonrec of (string * pe_expr) list (** (let f1 = E1 (and f2 = E2) *)
+  | Pe_Rec of (string * pe_expr) list (** (let rec f1 = E1 (and f2 = E2) *)
 
 type pe_program = pe_declaration list
 
