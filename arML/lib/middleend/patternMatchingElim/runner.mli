b4a996f390f82df648c96037d54cb63ee50ee3a1
raf-nr <nurmuhametovrafik04@gmail.com>
2025-03-08 21:06:51 +0300

    Implement pattern matching elim runner

---
+++ arML/lib/middleend/patternMatchingElim/runner.mli
@@ -0,0 +1,7 @@
+(** Copyright 2024-2025, raf-nr and ksenmel *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+val run_pmf_program : 
+  LambdaLifting.Llast.ll_program -> 
+  Pmfast.pmf_program
