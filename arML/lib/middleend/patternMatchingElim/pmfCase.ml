5c18d013d355c6cf9d10f83a024c802c3e740912
raf-nr <nurmuhametovrafik04@gmail.com>
2025-03-08 18:20:18 +0300

    Implement pattern eliminate in pm case

---
+++ arML/lib/middleend/patternMatchingElim/pmfCase.ml
@@ -0,0 +1,14 @@
+(** Copyright 2024-2025, raf-nr and ksenmel *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+let eliminate_pm_case (p, expr) (matched_branch, not_matched_branch) =
+  let decls = GenerateDeclarationsFromCase.generate_declarations_from_case (p, expr) in
+  let rec let_expr decls = match decls with
+    | [] -> matched_branch
+    | [lst] -> Pmfast.PMFLetIn (lst, matched_branch)
+    | hd :: tl -> Pmfast.PMFLetIn (hd, let_expr tl)
+  in
+  let matched_branch = let_expr decls in
+  GenerateConditionsFromCase.generate_condition_if_needed (p, expr) matched_branch not_matched_branch
+;;
