1f945376257491f911273323cc276998f1cbdb49
d-zaytsev <dzaytsev308@gmail.com>
2025-03-09 12:47:45 +0300

    Update anf

--- HamsterML/lib/middleend/anf.ml
+++ HamsterML/lib/middleend/anf.ml
@@ -1,5 +1,6 @@
 open Ast
 
+(* Immediate Expressions, don't require further computation *)
 type imm_expr =
   | ImmInt of int
   | ImmString of string
@@ -8,22 +9,26 @@ type imm_expr =
   | ImmList of imm_expr list
   | ImmTuple of imm_expr list
   | ImmUnit
+  | ImmConstraint of imm_expr * dataType
 
+(* Computed Expressions *)
 type cexpr =
+  | COperation of op
   | CApplication of cexpr * cexpr
   | CIf of imm_expr * aexpr * aexpr
   | CConstructList of imm_expr * imm_expr
-  | CMatch of imm_expr * (pattern * aexpr) list
   | CImm of imm_expr
 
+(* Atomic Expressions *)
 and aexpr =
   | ALetIn of pattern * cexpr * aexpr
   | ACExpr of cexpr
 
-type single_anf_binding = ALet of pattern * pattern list * aexpr
+(* Let binding in ANF form *)
+type single_anf_binding = ALet of pattern * id list * aexpr
 
 type anf_decl =
   | ADSingleLet of funType * single_anf_binding
-  | ADMutualRecDecl of funType * single_anf_binding
+  | ADMutualRecDecl of single_anf_binding list
 
 type anf_prog = anf_decl list
