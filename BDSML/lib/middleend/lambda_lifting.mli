c73538dc765dac4350b8b77ac209bdfb9db3f254
Kuarni <kuarni22@gmail.com>
2025-03-09 23:41:47 +0300

    feat(middleend): Result.monads instead of cringy exception

---
+++ BDSML/lib/middleend/lambda_lifting.mli
@@ -0,0 +1,5 @@
+(** Copyright 2025, Kuarni and LeonidElkin *)
+
+(** SPDX-License-Identifier: LGPL-2.1-or-later *)
+
+val ll : Reduced_ast.rstuct_item list -> (Reduced_ast.rstruct, Reduced_ast.error) result
--- BDSML/test/middleend/run_remove_patterns.ml
+++ BDSML/test/middleend/run_remove_patterns.ml
@@ -2,6 +2,8 @@
 
 (** SPDX-License-Identifier: LGPL-2.1-or-later *)
 
+open Middleend
+
 let test str =
   match Parser.Main_parser.parse str with
   | Result.Error e -> Format.eprintf "Parser error: %s" e
@@ -9,10 +11,17 @@ let test str =
     (match Typing.Inference.infer_program ast with
      | Result.Error s -> Format.eprintf "Inference error: %s" s
      | Result.Ok _ ->
-       print_string
-         (Middleend.Pattern_remover.remove_patterns ast
-          |> Middleend.Converter.rast_to_ast
-          |> Quickcheck.Main_unparser.unparse_structure))
+       let res =
+         Pattern_remover.remove_patterns ast
+         |> Result.map_error Middleend_utils.exp_to_string
+       in
+       let res =
+         match res with
+         | Result.Ok s ->
+           s |> Converter.rast_to_ast |> Quickcheck.Main_unparser.unparse_structure
+         | Result.Error s -> s
+       in
+       print_string res)
 ;;
 
 let () = test @@ Stdio.In_channel.input_all Stdlib.stdin
