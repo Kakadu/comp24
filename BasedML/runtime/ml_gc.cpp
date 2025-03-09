3bab775e5d0122413955bbf384a0e093d2fbd19e
Osogi <dragonmail135159@gmail.com>
2025-03-09 03:11:51 +0300

    fix: gc fixes first itteration

--- BasedML/runtime/ml_gc.cpp
+++ BasedML/runtime/ml_gc.cpp
@@ -1,5 +1,6 @@
 #include "ml_gc.h"
-#include "stdarg.h"
+#include <cstdint>
+#include <set>
 
 #ifdef GC
     #define START_POOL_SIZE 0x100
@@ -7,13 +8,14 @@
 box_t** stack_bottom = NULL;
 box_t** stack_top;
 
-box_t*** global_vars;
-box_t*** global_vars_end;
+box_t*** global_vars = NULL;
+box_t*** global_vars_end = NULL;
 
 typedef struct _pool_st {
-    void* pool_bottom;
-    void* pool_pointer;
-    void* pool_top;
+    uint8_t* pool_bottom;
+    uint8_t* pool_pointer;
+    uint8_t* pool_top;
+    std::set<box_t*> used_addresses;
 } pool_t;
 
 pool_t* the_pool;
@@ -22,15 +24,19 @@ pool_t* the_pool;
         ({                                                                                                             \
             void* res = pool->pool_pointer;                                                                            \
             pool->pool_pointer += sz;                                                                                  \
+            DEBUG_RUN(printf("[CR]: Added something with address %lx\n", (int64_t)res););                              \
+            pool->used_addresses.insert((box_t*)res);                                                                  \
             res;                                                                                                       \
         })
 
 pool_t* create_pool_t(size_t sz) {
-    void* start = malloc(sz);
-    pool_t* pool = malloc(sizeof(pool));
+    uint8_t* start = (uint8_t*)malloc(sz);
+    pool_t* pool = (pool_t*)malloc(sizeof(*pool));
     pool->pool_bottom = start;
     pool->pool_pointer = start;
     pool->pool_top = start + sz;
+    pool->used_addresses = std::set<box_t*>();
+
     return pool;
 }
 
@@ -48,33 +54,48 @@ uint8_t is_inside_pool(pool_t* pool, box_t* some_box) {
 }
 
 box_t* process_node(box_t* old_box, pool_t* old_pool) {
+    DEBUG_RUN(printf("[PROC]: Try process %lx: ", old_box); fflush(stdout););
+
     if (!is_inside_pool(old_pool, old_box)) {
         return old_box;
     } else {
-        if (old_box->header.color == COLOR_PROCESSED) {
-            return (box_t*)old_box->values[0]; // new box address
-        } else {
-            box_t* res = POOL_MALLOC(the_pool, old_box->header.size * 8);
-            res->header = old_box->header;
+        uint64_t orig_address = (uint64_t)old_box;
+        old_box = *(--(old_pool->used_addresses.upper_bound(old_box)));
+        uint64_t offset = orig_address - (uint64_t)old_box;
 
+        if (old_box->header.color == COLOR_PROCESSED) {
+            return (box_t*)old_box->values[0] + offset; // new box address
+        } else {
+
+            box_t* res = (box_t*)POOL_MALLOC(the_pool, old_box->header.size * 8);
+            res->header = old_box->header;
             // number of elems in box = header.size-1
             for (int i = 0; i < old_box->header.size - 1; i++) {
                 res->values[i] = (int64_t)process_node((box_t*)old_box->values[i], old_pool);
             }
+
+            DEBUG_RUN(if (old_box->header.tag == (int8_t)T_CLOSURE) {
+                printf("Was | Became \n");
+                printf("func: %lx  | %lx\n", old_box->values[0], res->values[0]);
+                printf("num: %lx  | %lx\n", old_box->values[1], res->values[1]);
+                printf("applied: %lx  | %lx\n", old_box->values[2], res->values[2]);
+            });
             old_box->header.color = COLOR_PROCESSED;
             old_box->values[0] = (int64_t)res; // set new box address
-            return res;
+            return (box_t*)((uint64_t)res + offset);
         }
     }
 }
 
 void realloc_the_pool(size_t sz) {
+    DEBUG_RUN(printf("[GC] ---- REALOC!!\n"); fflush(stdout););
     save_callee_saved_registers();
-    stack_bottom = (box_t**)get_stack_pointer();
+    stack_top = (box_t**)get_stack_pointer();
     pool_t* old_pool = the_pool;
     the_pool = create_pool_t(sz);
 
-    for (box_t** iter = stack_bottom; iter < stack_top; iter++) {
+    fflush(stdout);
+    for (box_t** iter = stack_top; iter < stack_bottom; iter++) {
         *iter = process_node(*iter, old_pool);
     }
     for (box_t*** iter = global_vars; iter < global_vars_end; iter++) {
@@ -85,7 +106,9 @@ void realloc_the_pool(size_t sz) {
 }
 
 void* safe_malloc(size_t sz) {
-    if (the_pool->pool_pointer + sz <= the_pool->pool_top) {
+    DEBUG_RUN(printf("[GC] ---- Safe malloc!! sz = %lx\n", sz); fflush(stdout););
+
+    if ((the_pool->pool_pointer + sz) <= the_pool->pool_top) {
         return POOL_MALLOC(the_pool, sz);
     } else {
         size_t cur_sz = (size_t)(the_pool->pool_top - the_pool->pool_bottom);
@@ -98,6 +121,7 @@ void* safe_malloc(size_t sz) {
     }
 }
 void* _init_gc_malloc(size_t sz) {
+    DEBUG_RUN(printf("[GC] ---- GC IS ALIVE\n"););
     stack_bottom = (box_t**)get_stack_pointer();
     the_pool = create_pool_t(START_POOL_SIZE);
     gc_malloc = (malloc_f_t)safe_malloc;
@@ -106,15 +130,12 @@ void* _init_gc_malloc(size_t sz) {
 
 malloc_f_t gc_malloc = _init_gc_malloc;
 
-void add_global_vars_to_gc(size_t n, ...) {
-    va_list globs;
-    va_start(globs, n);
-    global_vars = calloc(n, sizeof(*global_vars));
+void add_global_vars_to_gc(size_t n, va_list globs) {
+    global_vars = (box_t***)calloc(n, sizeof(*global_vars));
     global_vars_end = global_vars + n;
     for (int i = 0; i < n; i++) {
-        global_vars[i] = va_arg(globs, void*);
+        global_vars[i] = (box_t**)va_arg(globs, void*);
     }
-    va_end(globs);
 }
 
 void compact() {
@@ -125,7 +146,7 @@ void compact() {
 void print_gc_info() {
     size_t used = the_pool->pool_pointer - the_pool->pool_bottom;
     size_t all = the_pool->pool_top - the_pool->pool_bottom;
-    printf("Memory used: %8lx/%8lx", used, all);
+    printf("Memory used: %8lx/%8lx\n", used, all);
 }
 
 #endif
