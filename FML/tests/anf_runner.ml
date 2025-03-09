4e504021958ea80e78a2a4dc93b175370588d250
Dmitry Pilyuk <dmitriipiliuk@gmail.com>
2025-03-09 17:59:20 +0300

    Anf runner

--- FML/tests/anf_runner.ml
+++ FML/tests/anf_runner.ml
@@ -1,11 +1,9 @@
-
 (** Copyright 2024-2025, Dmitry Pilyuk, Aleksandr Rozhkov *)
 
 (** SPDX-License-Identifier: LGPL-2.1 *)
 
 open Fml_lib.Parser
 open Fml_lib.Inferencer
-open Fml_lib.Pe_ast
 open Fml_lib.Pattern_elim
 open Fml_lib.Alpha_conv
 open Fml_lib.Lambda_lifting
@@ -28,9 +26,8 @@ let () =
     let bind, cnt, ast = run_pe ast in
     let bind, cnt, ast = run_alpha_conv bind cnt ast in
     let ast = run_cc ast in
-    let bind, cnt, ast  = run_ll bind cnt ast in
+    let bind, cnt, ast = run_ll bind cnt ast in
     let _, _, ast = run_anf bind cnt ast in
     Format.printf "%a" pp_anf_structure ast
-    Format.printf "%a" pp_pe_structure ast
   | Error message -> Format.printf "%s" message
 ;;
