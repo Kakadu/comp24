c7eef137af2eed20f977ac1ce35b8adccc765084
LeonidElkin <yolkinleonwork@gmail.com>
2025-03-10 21:20:04 +0300

    feat: llvm untested version

--- BDSML/lib/llvm/codegen.ml
+++ BDSML/lib/llvm/codegen.ml
@@ -3,7 +3,13 @@ open Llvm_init
 open Middleend.Anf_ast
 open Parser.Ast
 
-let rec compile_aexpr = function
+let rec compile_aexpr =
+  let rec build_list = function
+    | AExp_construct ("[]", None) -> []
+    | AExp_construct ("::", Some (AExp_tuple [ head; tail ])) -> head :: build_list tail
+    | _ -> failwith "Not a list"
+  in
+  function
   | AExp_constant c ->
     (match c with
      | Const_int i -> const_int int_t i
@@ -21,13 +27,17 @@ let rec compile_aexpr = function
     let types = List.map compile_aexpr l' in
     const_struct context (Array.of_list types)
   | AExp_construct ("[]", _) -> const_struct context (Array.of_list [])
-  | AExp_construct ("None", _) -> build_none
+  | AExp_construct ("None", _) ->
+    const_struct context [| const_int bool_t 0; const_null ptr_t |]
   | AExp_construct ("Some", x) ->
     (match x with
      | None -> failwith "Impossible"
-     | Some x -> build_some (compile_aexpr x))
+     | Some x -> const_struct context [| const_int bool_t 1; compile_aexpr x |])
   | AExp_construct (_, _) -> failwith "Not implemented"
-  | AExp_ident id -> build_ident id
+  | AExp_ident id ->
+    (match Hashtbl.find_opt variable_value_table id with
+     | Some value -> build_load ptr_t value id builder
+     | None -> failwith ("Unknown variable: " ^ id))
 ;;
 
 let rec compile_cexpr = function
@@ -67,4 +77,81 @@ and compile_lexpr = function
     Hashtbl.remove variable_value_table s;
     body_val
   | LComplex c -> compile_cexpr c
+;;
+
+let compile_func f =
+  let name, args, body = f in
+  let param_types = Array.make (List.length args) int_t in
+  let func_type = function_type int_t param_types in
+  let func = define_function name func_type my_module in
+  let entry_block = append_block context "entry" func in
+  let builder = builder_at_end context entry_block in
+  List.iteri
+    (fun i arg_name ->
+      let param = Llvm.param func i in
+      let param_alloca = Llvm.build_alloca (Llvm.type_of param) arg_name builder in
+      let _ = Llvm.build_store param param_alloca builder in
+      Hashtbl.add variable_value_table arg_name param_alloca)
+    args;
+  let body_value = compile_lexpr body in
+  let _ = Llvm.build_ret body_value builder in
+  func
+;;
+
+let compile_func f =
+  let name, args, body = f in
+  let param_types = Array.make (List.length args) int_t in
+  let func_type = function_type int_t param_types in
+  let func = define_function name func_type my_module in
+  let entry_block = append_block context "entry" func in
+  let builder = builder_at_end context entry_block in
+  List.iteri
+    (fun i arg_name ->
+      let param = Llvm.param func i in
+      let param_alloca = Llvm.build_alloca (Llvm.type_of param) arg_name builder in
+      let _ = Llvm.build_store param param_alloca builder in
+      Hashtbl.add variable_value_table arg_name param_alloca)
+    args;
+  let body_value = compile_lexpr body in
+  let _ = Llvm.build_ret body_value builder in
+  func
+;;
+
+let compile_absexpr = function
+  | AbsStr_eval a -> ignore @@ compile_lexpr a
+  | AbsStr_func f -> ignore @@ compile_func f
+  | AbsStr_value (s, l) ->
+    let value = compile_lexpr l in
+    let var_alloca = build_alloca (type_of value) s builder in
+    let _ = build_store value var_alloca builder in
+    let () = Hashtbl.add variable_value_table s var_alloca in
+    ignore @@ value
+  | AbsStr_value_rec fl ->
+    let () =
+      List.iter
+        (fun (name, args, _) ->
+          let param_types = Array.make (List.length args) int_t in
+          let func_type = function_type int_t param_types in
+          ignore (declare_function name func_type my_module))
+        fl
+    in
+    ignore @@ List.map (fun func -> compile_func func) fl
+;;
+
+let compile_anf anf = List.iter (fun expr -> compile_absexpr expr) anf
+
+let compile_main =
+  let main_type = function_type int_t [||] in
+  let main_func = declare_function "main" main_type my_module in
+  let entry = append_block context "entry" main_func in
+  position_at_end entry builder;
+  ignore (build_ret (const_int int_t 0) builder)
+;;
+
+let compile_program ?(verbose = false) program =
+  let () = predefined_init () in
+  let () = compile_anf program in
+  let () = compile_main in
+  print_module "out.ll" my_module;
+  if verbose then dump_module my_module
 ;;
