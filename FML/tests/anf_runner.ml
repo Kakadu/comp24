08ff5a53f064739b5c120fb8d74aa5393ddfeeb4
Dmitry Pilyuk <dmitriipiliuk@gmail.com>
2025-03-10 23:59:39 +0300

    Improve anf runner

--- FML/tests/anf_runner.ml
+++ FML/tests/anf_runner.ml
@@ -4,6 +4,7 @@
 
 open Fml_lib.Parser
 open Fml_lib.Inferencer
+open Fml_lib.Inf_pprint
 open Fml_lib.Pattern_elim
 open Fml_lib.Alpha_conv
 open Fml_lib.Lambda_lifting
@@ -17,17 +18,22 @@ let () =
     match parse input with
     | Ok parsed ->
       (match run_program_inferencer parsed with
-       | Ok _ -> Ok parsed
+       | Ok types -> Ok (parsed, types)
        | Error _ -> Error (Format.asprintf "Infer error:"))
     | Error e -> Error (Format.sprintf "Parsing error: %s" e)
   in
   match parse_and_infer input with
-  | Ok ast ->
+  | Ok (ast, _) ->
     let bind, cnt, ast = run_pe ast in
     let bind, cnt, ast = run_alpha_conv bind cnt ast in
     let ast = run_cc ast in
     let bind, cnt, ast = run_ll bind cnt ast in
     let _, _, ast = run_anf bind cnt ast in
-    Format.printf "%a" pp_anf_program ast
+    let result = Format.asprintf "%a" pp_anf_program ast in
+    let () = Format.printf "%a" pp_anf_program ast in
+    let () = Format.printf "\nТипы после приведения в ANF:\n" in
+    (match parse_and_infer result with
+     | Ok (_, (env, names_list)) -> print_program_type env names_list
+     | Error e -> Format.printf "%s" e)
   | Error message -> Format.printf "%s" message
 ;;
