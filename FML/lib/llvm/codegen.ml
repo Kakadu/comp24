9debdaca2fa833d7c3c4eef2904f15ab9ca0150a
RozhkovAleksandr <rozhkov_a@outlook.com>
2025-03-10 23:28:19 +0300

    add llvm dune and binops

--- FML/lib/llvm/codegen.ml
+++ FML/lib/llvm/codegen.ml
@@ -3,3 +3,16 @@
 (** SPDX-License-Identifier: LGPL-2.1 *)
 
 open Llvm
+open Anf_ast
+
+let ctx = global_context ()
+let builder = builder ctx
+
+let compile_binop op x y =
+  match op with
+  | "( + )" -> build_add x y "add" builder
+  | "( - )" -> build_sub x y "sub" builder
+  | "( * )" -> build_mul x y "mul" builder
+  | "( / )" -> build_sdiv x y "div" builder
+  | _ -> failwith ("Invalid operator: " ^ op)
+;;
