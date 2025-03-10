3856e3ee9b0c5a4bad5303acd33380dd92a65857
LeonidElkin <yolkinleonwork@gmail.com>
2025-03-10 21:20:04 +0300

    feat: llvm untested version

--- BDSML/lib/llvm/llvm_init.ml
+++ BDSML/lib/llvm/llvm_init.ml
@@ -1,19 +1,18 @@
 open Llvm
 open Llvm_target
-open Middleend.Anf_ast
 
 (*Env init*)
 let context = global_context ()
 let my_module = create_module context "BDSML"
-let target_triple = Llvm_target.Target.default_triple ()
+let target_triple = Target.default_triple ()
 let () = set_target_triple target_triple my_module
 let () = Llvm_all_backends.initialize ()
 
 let () =
-  let target = Llvm_target.Target.by_triple target_triple in
-  let machine = Llvm_target.TargetMachine.create ~triple:target_triple target in
-  let data_layout = Llvm_target.TargetMachine.data_layout machine in
-  set_data_layout (Llvm_target.DataLayout.as_string data_layout) my_module
+  let target = Target.by_triple target_triple in
+  let machine = TargetMachine.create ~triple:target_triple target in
+  let data_layout = TargetMachine.data_layout machine in
+  set_data_layout (DataLayout.as_string data_layout) my_module
 ;;
 
 (*Types init*)
@@ -24,29 +23,33 @@ let bool_t = i1_type context
 let void_t = void_type context
 let ptr_t = pointer_type context
 
-(*Hashtables*)
+(*Hashtable*)
 
 let variable_value_table : (string, Llvm.llvalue) Hashtbl.t = Hashtbl.create 10
 
-(*Builders functions*)
+(*Predefined*)
 
-let rec build_list = function
-  | AExp_construct ("[]", None) -> []
-  | AExp_construct ("::", Some (AExp_tuple [ head; tail ])) -> head :: build_list tail
-  | _ -> failwith "Not a list"
+let predefined =
+  [ "__op_plus", function_type int_t [| int_t; int_t |]
+  ; "__op_minus", function_type int_t [| int_t; int_t |]
+  ; "__op_mult", function_type int_t [| int_t; int_t |]
+  ; "__op_div", function_type int_t [| int_t; int_t |]
+  ; "__op_neg", function_type int_t [| int_t |]
+  ; "__op_pos", function_type int_t [| int_t |]
+  ; "not", function_type bool_t [| bool_t |]
+  ; "__op_gt", function_type bool_t [| ptr_t; ptr_t |]
+  ; "__op_ge", function_type bool_t [| ptr_t; ptr_t |]
+  ; "__op_lt", function_type bool_t [| ptr_t; ptr_t |]
+  ; "__op_le", function_type bool_t [| ptr_t; ptr_t |]
+  ; "__op_eq", function_type bool_t [| ptr_t; ptr_t |]
+  ; "__op_neq", function_type bool_t [| ptr_t; ptr_t |]
+  ; "__op_or", function_type bool_t [| bool_t; bool_t |]
+  ; "__op_and", function_type bool_t [| bool_t; bool_t |]
+  ; "__op_phys_eq", function_type bool_t [| ptr_t; ptr_t |]
+  ; "print_int", function_type void_t [| int_t |]
+  ]
 ;;
 
-let build_none = const_struct context [| const_int bool_t 0; const_null ptr_t |]
-let build_some value = const_struct context [| const_int bool_t 1; value |]
-
-let rec build_construct = function
-  | AExp_construct ("[]", None) -> []
-  | AExp_construct ("::", Some (AExp_tuple [ head; tail ])) -> head :: build_list tail
-  | _ -> failwith "Not a list"
-;;
-
-let build_ident name =
-  match Hashtbl.find_opt variable_value_table name with
-  | Some value -> build_load ptr_t value name builder
-  | None -> failwith ("Unknown variable: " ^ name)
+let predefined_init () =
+  ignore @@ List.map (fun (str, t) -> declare_function str t my_module) predefined
 ;;
