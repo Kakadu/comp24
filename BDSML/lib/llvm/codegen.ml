49f49be7437f669697c484236427ba4d0e1148e1
LeonidElkin <yolkinleonwork@gmail.com>
2025-03-10 18:22:09 +0300

    feat: codegen for cexpr and lexpr

--- BDSML/lib/llvm/codegen.ml
+++ BDSML/lib/llvm/codegen.ml
@@ -3,7 +3,7 @@ open Llvm_init
 open Middleend.Anf_ast
 open Parser.Ast
 
-let rec compile_aexp = function
+let rec compile_aexpr = function
   | AExp_constant c ->
     (match c with
      | Const_int i -> const_int int_t i
@@ -14,18 +14,57 @@ let rec compile_aexp = function
        let fn_t = function_type void_t [||] in
        define_function "unit_t" fn_t my_module)
   | AExp_tuple elems ->
-    let types = List.map compile_aexp elems in
+    let types = List.map compile_aexpr elems in
     const_struct context (Array.of_list types)
   | AExp_construct ("::", _) as l ->
     let l' = build_list l in
-    let types = List.map compile_aexp l' in
+    let types = List.map compile_aexpr l' in
     const_struct context (Array.of_list types)
   | AExp_construct ("[]", _) -> const_struct context (Array.of_list [])
   | AExp_construct ("None", _) -> build_none
   | AExp_construct ("Some", x) ->
     (match x with
      | None -> failwith "Impossible"
-     | Some x -> build_some (compile_aexp x))
+     | Some x -> build_some (compile_aexpr x))
   | AExp_construct (_, _) -> failwith "Not implemented"
   | AExp_ident id -> build_ident id
+;;
+
+let rec compile_cexpr = function
+  | CExp_if (ae, l1, l2) ->
+    let ae' = compile_aexpr ae in
+    let func = block_parent (insertion_block builder) in
+    let then_block = append_block context "then" func in
+    let else_block = append_block context "else" func in
+    let merge_block = append_block context "merge" func in
+    let () = ignore (build_cond_br ae' then_block else_block builder) in
+    let () = position_at_end then_block builder in
+    let then_val = compile_lexpr l1 in
+    let () = ignore (build_br merge_block builder) in
+    let then_bb = insertion_block builder in
+    let () = position_at_end else_block builder in
+    let else_val = compile_lexpr l2 in
+    let () = ignore (build_br merge_block builder) in
+    let else_bb = insertion_block builder in
+    let () = position_at_end merge_block builder in
+    build_phi [ then_val, then_bb; else_val, else_bb ] "ite_result" builder
+  | CExp_apply (s, ael) ->
+    let func = lookup_function s my_module in
+    (match func with
+     | None -> failwith ("Undefined function: " ^ s)
+     | Some fn ->
+       let compiled_args = List.map compile_aexpr ael in
+       build_call (type_of fn) fn (Array.of_list compiled_args) "call_tmp" builder)
+  | CExp_atom ae -> compile_aexpr ae
+
+and compile_lexpr = function
+  | LLet_in (s, c, l) ->
+    let value = compile_cexpr c in
+    let var_alloca = build_alloca (type_of value) s builder in
+    let _ = build_store value var_alloca builder in
+    Hashtbl.add variable_value_table s var_alloca;
+    let body_val = compile_lexpr l in
+    Hashtbl.remove variable_value_table s;
+    body_val
+  | LComplex c -> compile_cexpr c
 ;;
--- BDSML/test/llvm/test_codegen.ml
+++ BDSML/test/llvm/test_codegen.ml
@@ -3,26 +3,26 @@ open My_llvm
 open Llvm
 
 let () =
-  let compiled1 =
-    Codegen.compile_aexp
-      (AExp_tuple
-         [ AExp_constant (Const_int 42)
+  (* let compiled1 =
+     Codegen.compile_aexpr
+     (AExp_tuple
+     [ AExp_constant (Const_int 42)
          ; AExp_constant (Const_char 'c')
          ; AExp_constant (Const_bool true)
          ; AExp_constant (Const_string "BDSM")
          ; AExp_constant Const_unit
          ])
-  in
-  let compiled2 =
-    Codegen.compile_aexp (AExp_construct ("Some", Some (AExp_constant (Const_int 1))))
-  in
-  let compiled3 =
-    Codegen.compile_aexp
-      (AExp_construct
-         ( "::"
-         , Some
-             (AExp_tuple
-                [ AExp_constant (Const_int 1)
+     in
+     let compiled2 =
+     Codegen.compile_aexpr (AExp_construct ("Some", Some (AExp_constant (Const_int 1))))
+     in
+     let compiled3 =
+     Codegen.compile_aexpr
+     (AExp_construct
+     ( "::"
+     , Some
+     (AExp_tuple
+     [ AExp_constant (Const_int 1)
                 ; AExp_construct
                     ( "::"
                     , Some
@@ -30,9 +30,14 @@ let () =
                            [ AExp_constant (Const_int 2); AExp_construct ("[]", None) ])
                     )
                 ]) ))
+     in *)
+  let compiled4 =
+    Codegen.compile_lexpr
+      (LLet_in
+         ( "x"
+         , CExp_atom (AExp_constant (Const_int 42))
+         , LComplex (CExp_atom (AExp_ident "x")) ))
   in
-  dump_value compiled1;
-  dump_value compiled2;
-  dump_value compiled3;
+  dump_value compiled4;
   dump_module Llvm_init.my_module
 ;;
