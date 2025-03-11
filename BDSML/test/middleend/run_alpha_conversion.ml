974b07f81c772bf04e90454eda8b3fbb39e35467
Kuarni <kuarni22@gmail.com>
2025-03-11 16:54:54 +0300

    refactor(middleend): remove commented code

--- BDSML/test/middleend/run_alpha_conversion.ml
+++ BDSML/test/middleend/run_alpha_conversion.ml
@@ -26,23 +26,4 @@ let test str =
        print_string res)
 ;;
 
-(* let __disassemble : 'a -> 'b = ()
-   let __get_from_tuple : 'a -> int -> 'c = ()
-   let __same_cons : 'a -> 'b -> bool = ()
-   let __get_cons_param : 'a -> 'b = ()
-   let __exception : string -> 'a = ()
-
-   let rec __var_helper __var_a0 __reserved_1 __reserved_2 = (let __anf_0 = ((__same_cons __reserved_2) "[]") in
-   (if __anf_0 then (let __nothing = ((__same_cons __reserved_2) (([]))) in
-   __reserved_1) else (let __anf_1 = ((__same_cons __reserved_2) "::") in
-   (let __anf_2 = ((( && ) true) true) in
-   (let __anf_3 = ((( && ) __anf_2) true) in
-   (let __anf_4 = ((( && ) __anf_1) __anf_3) in
-   (if __anf_4 then (let __reserved_4 = ((__disassemble "::") __reserved_2) in
-   (let __var_h = ((__get_from_tuple __reserved_4) 0) in
-   (let __var_tl = ((__get_from_tuple __reserved_4) 1) in
-   (let __anf_5 = ((( + ) __reserved_1) __var_a0) in
-   ((__var_helper __anf_5) __var_tl))))) else (__exception "Match_failure"))))))));;
-   let __var_f __reserved_0 = ((__var_helper __reserved_0) 0);; *)
-(**)
 let () = test @@ Stdio.In_channel.input_all Stdlib.stdin
