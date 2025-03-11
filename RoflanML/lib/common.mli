f5764fb2a89cebb68229bd48d5389a1558151bb7
Ilya Syresenkov <110498410+maybenotilya@users.noreply.github.com>
2025-03-11 22:06:03 +0300

    [D] RoflanML: QCheck & ANF (#66)
    * Update pairing.md
    
    * Feat: added AST and parser
    
    * Added ocamlformat
    
    * fix: change names in copyrights
    
    * tests: move tests to separate library
    
    * feat(parser): fix let and app parsing
    
    * Feat: added type anotation for function
    
    * feat(parser): change args to option type
    
    * Feat: added anotations for list and tuple
    
    * tests: add typechecker tests
    
    * Refactor: changed the tuple type as type_id * type_id * type_id list
    
    * feat(parser): change arguments list to curruing
    
    * feat(typechecker): add typechecker
    
    * feat(parser): change EBinop to EApp
    
    * feat(typechecker): add typechecker
    
    * refactor(typechecker): remove unused code
    
    * Refactor: added name for every tests
    
    * Fix linter error for inference
    
    * Fix linter error for parser
    
    * fix linter error for typing
    
    * fix: add .mli interfaces
    
    * Feat: add QCheck, Unparser, and fix small fix parser
    
    * add stdlib and closure conversion
    
    * fix CC and add some tests
    
    * add even more tests
    
    * add common modules and program closure
    
    * Feat(parser): add tuple pattern
    
    * fix ast
    
    * fix typo in tests
    
    * add tuple pattern to typechecker and closure conversion
    
    * add lambda lifting and more closure conversion tests
    
    * Feat(parser): mutual recursion
    
    * add ast to string converter
    
    * some fix
    
    * add mutual recursion to typechecker
    
    * add more tests on mutual rec typechecker
    
    * add cons operator and fix bugo in typechecker type_to_schema
    
    * add mutual recursion to CC
    
    * add anf
    
    * add .mli interfaces
    
    * fix: change decl to declt list in test
    
    * fix PVar _
    
    * fix parse_decl
    
    * move tests to cram
    
    * fix ocamlformat
    
    * switch to ocamlformat 0.26.0 version
    
    * add qcheck to deps and fix ocamlformat
    
    * change qcheck test
    
    * add ANF types tests
    
    * fix bugo wit hlet poly
    
    * fix new naming generation in anf and ll
    
    * add cram test for qcheck
    
    * add support for mutual recursion
    
    * fix cram tests
    
    * add anf parse tests
    
    * rename anf parse and typing tests and add message about anf parse failure
    
    ---------
    
    Co-authored-by: Lesh79 <lesh79.zaebal@gmail.com>
    Co-authored-by: Temerlan Akhmetov <110246409+Lesh79@users.noreply.github.com>
    Co-authored-by: Lesh79 <Temerlan.axmetov.75@mail.ru>

---
+++ RoflanML/lib/common.mli
@@ -0,0 +1,19 @@
+module Counter_Monad : sig
+  type ('a, 'err) t = int -> int * ('a, 'err) result
+
+  val return : 'a -> ('a, 'err) t
+  val fail : 'err -> ('a, 'err) t
+  val ( >>= ) : ('a, 'err) t -> ('a -> ('b, 'err) t) -> ('b, 'err) t
+  val ( let* ) : ('a, 'b) t -> ('a -> ('c, 'b) t) -> ('c, 'b) t
+  val fresh : int -> int * (int, 'a) result
+  val run : (int -> 'a * 'b) -> 'b
+end
+
+val gen_name : string -> (string, 'a) Base.Set.t -> (string, 'b) Counter_Monad.t
+
+open Ast
+
+module Middleend_Common : sig
+  val uncurry : expr -> typed_arg list * expr
+  val curry : typed_arg list -> expr -> expr
+end
---
+++ RoflanML/lib/middleend/lambda_lifting.ml
@@ -0,0 +1,137 @@
+(** Copyright 2024, Ilya Syresenkov, Akhmetov Tamerlan *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+open Base
+open Ast
+open Ll_ast
+open Common
+open Common.Counter_Monad
+open Common.Middleend_Common
+
+let gen_name = gen_name "ll"
+
+let get_bindings prog =
+  List.fold
+    prog
+    ~init:(Set.empty (module String))
+    ~f:(fun acc decl ->
+      match decl with
+      | DLet (_, id, _) -> Set.add acc id
+      | DMutualLet (_, decls) ->
+        List.fold decls ~init:acc ~f:(fun acc (id, _) -> Set.add acc id))
+;;
+
+let lift decl bindings =
+  let gen_name = gen_name bindings in
+  let rec lift_expr e lifted renames =
+    match e with
+    | EConst c -> return (LLConst c, lifted)
+    | EVar x ->
+      (match Map.find renames x with
+       | Some id -> return (LLVar id, lifted)
+       | None -> return (LLVar x, lifted))
+    | ETuple (e1, e2, es) ->
+      let* e1, lifted = lift_expr e1 lifted renames in
+      let* e2, lifted = lift_expr e2 lifted renames in
+      let* es, lifted =
+        List.fold_left
+          es
+          ~init:(return ([], lifted))
+          ~f:(fun acc e ->
+            let* es, lifted = acc in
+            let* e, lifted = lift_expr e lifted renames in
+            return (e :: es, lifted))
+      in
+      return (LLTuple (e1, e2, List.rev es), lifted)
+    | EList es ->
+      let* es, lifted =
+        List.fold_left
+          es
+          ~init:(return ([], lifted))
+          ~f:(fun acc e ->
+            let* es, lifted = acc in
+            let* e, lifted = lift_expr e lifted renames in
+            return (e :: es, lifted))
+      in
+      return (LLList (List.rev es), lifted)
+    | EBranch (cond, t, f) ->
+      let* cond, lifted = lift_expr cond lifted renames in
+      let* t, lifted = lift_expr t lifted renames in
+      let* f, lifted = lift_expr f lifted renames in
+      return (LLBranch (cond, t, f), lifted)
+    | EMatch (e, cases) ->
+      let* e, lifted = lift_expr e lifted renames in
+      let* cases, lifted =
+        List.fold_left
+          cases
+          ~init:(return ([], lifted))
+          ~f:(fun acc (p, e) ->
+            let* cases, lifted = acc in
+            let* e, lifted = lift_expr e lifted renames in
+            return ((p, e) :: cases, lifted))
+      in
+      return (LLMatch (e, List.rev cases), lifted)
+    | ELetIn (is_rec, id, (EFun _ as e1), e2) ->
+      let args, e = uncurry e1 in
+      let* new_name = gen_name in
+      let renames =
+        match is_rec with
+        | Rec -> Map.update renames id ~f:(fun _ -> new_name)
+        | NonRec -> renames
+      in
+      let* e, lifted = lift_expr e lifted renames in
+      let lifted = LLDLet (is_rec, new_name, args, e) :: lifted in
+      let renames = Map.update renames id ~f:(fun _ -> new_name) in
+      let* e2, lifted = lift_expr e2 lifted renames in
+      return (e2, lifted)
+    | ELetIn (_, id, e1, e2) ->
+      let* e1, lifted = lift_expr e1 lifted renames in
+      let* e2, lifted = lift_expr e2 lifted renames in
+      return (LLLetIn (id, e1, e2), lifted)
+    | EFun _ as f ->
+      let args, e = uncurry f in
+      let* new_name = gen_name in
+      let* e, lifted = lift_expr e lifted renames in
+      let lifted = LLDLet (NonRec, new_name, args, e) :: lifted in
+      return (LLVar new_name, lifted)
+    | EApp (e1, e2) ->
+      let* e1, lifted = lift_expr e1 lifted renames in
+      let* e2, lifted = lift_expr e2 lifted renames in
+      return (LLApp (e1, e2), lifted)
+  in
+  let rec lift_decl decl lifted renames =
+    match decl with
+    | DLet (is_rec, id, (EFun _ as f)) ->
+      let args, e = uncurry f in
+      let* e, lifted = lift_expr e lifted renames in
+      return (LLDLet (is_rec, id, args, e), lifted)
+    | DLet (is_rec, id, e) ->
+      let* e, lifted = lift_expr e lifted renames in
+      return (LLDLet (is_rec, id, [], e), lifted)
+    | DMutualLet (_, decls) ->
+      let* decls, lifted =
+        List.fold_right
+          decls
+          ~init:(return ([], []))
+          ~f:(fun (id, e) acc ->
+            let* decls, lifted = acc in
+            let* lifted = lift_decl (DLet (Rec, id, e)) lifted renames in
+            match lifted with
+            | LLDLet (_, id, args, e), lifted -> return ((id, args, e) :: decls, lifted)
+            | _ -> fail "Unreachable")
+      in
+      return (LLDMutualLet decls, lifted)
+  in
+  lift_decl decl [] (Map.empty (module String))
+;;
+
+let lift_program prog =
+  let helper prog =
+    List.fold_left prog ~init:(return []) ~f:(fun acc decl ->
+      let* acc = acc in
+      let* decl, lifted = lift decl (get_bindings prog) in
+      return (List.concat [ acc; List.rev (decl :: lifted) ]))
+  in
+  run (helper prog)
+;;
