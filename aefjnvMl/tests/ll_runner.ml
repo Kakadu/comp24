88836bdff229be5e6465c81655ac9ccfd13a6083
Artem-Rzhankoff <root@LAPTOP-URNC0QQR>
2025-03-11 14:21:08 +0300

    fix ll with tests proof

--- aefjnvMl/tests/ll_runner.ml
+++ aefjnvMl/tests/ll_runner.ml
@@ -16,7 +16,7 @@ let () =
     let*! ast' = Alpha_converter.rename_ast_with_uniq Common.Naming.alpha_prefix ast in
      let ast' = Middleend.Closure_conversion.convert_program ast' in
      let*! ast' = Alpha_converter.rename_ast_with_uniq Common.Naming.cc_prefix ast' in
-     let ast' =
+     let* ast' =
        let open Match_elimination in
        let*! m_ast = Match_elim.eliminate_match_in_program ast' in
        let m_ast' = Optimizations.optimize m_ast in
@@ -24,6 +24,8 @@ let () =
        let+! ll_ast = Ll.lift_lambdas m_ast' in
        Ll_converter.convert_ll_program ll_ast
     in
+    let+! env_a = Inferencer.check_program ast' in
+    let () = Pp_env.pp_env_after_modification env_a in
     ast'
   in
   let ast_printer ast_ = Stdlib.Format.printf "%a\n" Common.Ast_pp.pp_program ast_ in
