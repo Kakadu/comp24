d39a50f38f3750520d7d90b98ec0202e5adbad2e
Urtix <es.perevalov@mail.ru>
2025-03-11 21:47:52 +0300

    refactor: Fix ocamlformat
    Signed-off-by: Urtix <es.perevalov@mail.ru>

--- MEML/lib/pprinterANF.ml
+++ MEML/lib/pprinterANF.ml
@@ -68,25 +68,25 @@ let rec pp_aexpression formatter e =
 let pp_abindings formatter bindings =
   List.iter
     (fun bind ->
-       match bind with
-       | ALet (r, n_list, p, e) ->
-         Format.fprintf
-           formatter
-           "let %a %s %s = %a"
-           pp_rec
-           r
-           n_list
-           (String.concat " " p)
-           pp_aexpression
-           e;
-         Format.fprintf formatter "\n"
-       | ALetPat (n_list, e) ->
-         Format.fprintf formatter "let %a = %a" pp_pattern n_list pp_aexpression e;
-         Format.fprintf formatter "\n"
-       | AExpression e ->
-         pp_aexpression formatter e;
-         Format.fprintf formatter "\n")
-    bindings
-;;
+      match bind with
+      | ALet (r, n_list, p, e) ->
+        Format.fprintf
+          formatter
+          "let %a %s %s = %a"
+          pp_rec
+          r
+          n_list
+         (String.concat " " p)
+          pp_aexpression
+          e;
+        Format.fprintf formatter "\n"
+      | ALetPat (n_list, e) ->
+        Format.fprintf formatter "let %a = %a" pp_pattern n_list pp_aexpression e;
+        Format.fprintf formatter "\n"
+      | AExpression e ->
+        pp_aexpression formatter e;
+        Format.fprintf formatter "\n")
+     bindings
+ ;;
 
 let pp_anf bindings = Format.asprintf "%a" pp_abindings bindings
